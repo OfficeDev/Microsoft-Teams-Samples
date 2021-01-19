941acf4f5d094aa28bd600305ef4af88b51b7876
Alexander Katrukhin <a.katrukhin@gmail.com>
2021-01-19 10:17:54 -0800

    UI Template App Sample added

---
+++ samples/ui-templates/ts/src/app/about.tsx
@@ -0,0 +1,16 @@
+/**
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License.
+ *
+ * This component is used to display the required
+ * privacy statement which can be found in a link in the
+ * about tab.
+ */
+
+export function About() {
+  return (
+    <div>
+      <h1>About</h1>
+    </div>
+  );
+}
