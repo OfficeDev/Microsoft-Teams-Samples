f72c948cde51273f11c5f7d9f49957f1dd3148a9
Bob German <bgerman1@hotmail.com>
2021-01-22 14:00:42 -0500

    Fix tab config issue - JavaScript (bug 1479238)

--- samples/tab-channel-group-sso-quickstart/js/src/components/TabConfig.js
+++ samples/tab-channel-group-sso-quickstart/js/src/components/TabConfig.js
@@ -12,15 +12,25 @@ import * as microsoftTeams from "@microsoft/teams-js";
  * their choices and communicate that to Teams to enable the save button.
  */
 class TabConfig extends React.Component {
+
     render() {
       /**
-       * The content url for the tab is a required value that must be set.
-       * The url value is the source url for your configured tab.
+       * When the user clicks "Save", save the url for your configured tab.
        * This allows for the addition of query string parameters based on
        * the settings selected by the user.
        */
-      let contentURL = `${process.env.REACT_APP_BASE_URL}/tab`;
-      microsoftTeams.settings.setSettings({"contentUrl": contentURL});
+      microsoftTeams.settings.registerOnSaveHandler((saveEvent) => {
+
+        const baseUrl = `https://${window.location.hostname}:${window.location.port}`;
+        console.log (baseUrl);
+        microsoftTeams.settings.setSettings({
+          "suggestedDisplayName": "My Tab",
+          "entityId": "Test",
+          "contentUrl": baseUrl + "/tab",
+          "websiteUrl": baseUrl + "/tab"
+        });
+        saveEvent.notifySuccess();
+       });
   
       /**
        * After verifying that the settings for your tab are correctly
