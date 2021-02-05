81ab8cbe9517ce5d2df3867a1df4496e95f829b9
Trinetra Kumar <v-trkum@microsoft.com>
2021-02-05 08:36:46 +0530

    Config Tab Authentication Review Fixes

---
+++ samples/tab-channel-group-config-page-auth/csharp/ConfigTabAuthentication/Controllers/AuthenticationController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+using TabAuthentication.Models;
+
+namespace TabAuthentication.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+
+        private readonly ILogger<AuthenticationController> _logger;
+
+        private readonly IConfiguration Configuration;
+
+        public AuthenticationController(ILogger<AuthenticationController> logger, IConfiguration configuration)
+        {
+            _logger = logger;
+            Configuration = configuration;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("AuthStart")]
+        public ActionResult AuthStart()
+        {
+            ViewBag.ClientId = Configuration["ClientId"].ToString();
+            ViewBag.ResponseType = Configuration["ResponseType"].ToString();
+            ViewBag.ResponseMode = Configuration["ResponseMode"].ToString();
+            ViewBag.Resource = Configuration["Resource"].ToString();
+            return View();
+        }
+
+        [Route("AuthEnd")]
+        public ActionResult AuthEnd()
+        {
+            return View();
+        }
+
+        [Route("Success")]
+        public ActionResult Success()
+        {
+            return View();
+        }
+
+        [Route("Failed")]
+        public ActionResult Failed()
+        {
+            return View();
+        }
+
+        [Route("LogOut")]
+        public IActionResult LogOut()
+        {
+            return View();
+        }
+    }
+}
--- samples/tab-channel-group-config-page-auth/csharp/ConfigTabAuthentication/Controllers/HomeController.cs
+++ samples/tab-channel-group-config-page-auth/csharp/ConfigTabAuthentication/Controllers/HomeController.cs
@@ -1,7 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
 using TabAuthentication.Models;
 
 namespace TabAuthentication.Controllers
@@ -23,42 +28,14 @@ namespace TabAuthentication.Controllers
             return View();
         }
 
+        [Route("SetUp")]
+        public IActionResult SetUp()
+        {
+            return View();
+        }
+
         [Route("ConfigureTab")]
         public IActionResult ConfigureTab()
-        {
-            return View();
-        }
-
-        [Route("AuthStart")]
-        public ActionResult AuthStart()
-        {
-            ViewBag.ClientId = Configuration["ClientId"].ToString();
-            ViewBag.ResponseType = Configuration["ResponseType"].ToString();
-            ViewBag.ResponseMode = Configuration["ResponseMode"].ToString();
-            ViewBag.Resource = Configuration["Resource"].ToString();
-            return View();
-        }
-
-        [Route("AuthEnd")]
-        public ActionResult AuthEnd()
-        {
-            return View();
-        }
-
-        [Route("Success")]
-        public ActionResult Success()
-        {
-            return View();
-        }
-
-        [Route("Failed")]
-        public ActionResult Failed()
-        {
-            return View();
-        }
-
-        [Route("LogOut")]
-        public IActionResult LogOut()
         {
             return View();
         }
