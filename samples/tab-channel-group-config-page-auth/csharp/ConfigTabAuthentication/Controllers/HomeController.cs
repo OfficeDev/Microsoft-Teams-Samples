bd586fe2ce06b5852b4b9f7f24e91c85c02c53da
Trinetra Kumar <v-trkum@microsoft.com>
2021-02-17 10:36:02 +0530

    Config tab silent Auth implementation
    Updated sample coder for silent auth

--- samples/tab-channel-group-config-page-auth/csharp/ConfigTabAuthentication/Controllers/HomeController.cs
+++ samples/tab-channel-group-config-page-auth/csharp/ConfigTabAuthentication/Controllers/HomeController.cs
@@ -8,6 +8,9 @@ using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using TabAuthentication.Models;
+using System.IdentityModel.Tokens.Jwt;
+using Newtonsoft.Json.Linq;
+using System.Security.Claims;
 
 namespace TabAuthentication.Controllers
 {
@@ -37,6 +40,7 @@ namespace TabAuthentication.Controllers
         [Route("ConfigureTab")]
         public IActionResult ConfigureTab()
         {
+            ViewBag.ClientId = Configuration["ClientId"].ToString();
             return View();
         }
 
@@ -49,6 +53,42 @@ namespace TabAuthentication.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        public string ValidateToken(string validToken)
+        {
+            string decode = "";
+            string value = "";
+
+            var jwtHandler = new JwtSecurityTokenHandler();
+            var jwtInput = validToken;
+
+            //Check if readable token (string is in a JWT format)
+            var readableToken = jwtHandler.CanReadToken(jwtInput);
+
+            if (readableToken != true)
+            {
+                decode = "The token doesn't seem to be in a proper JWT format.";
+            }
+
+            if (readableToken == true)
+            {
+                var token = jwtHandler.ReadJwtToken(jwtInput);
+
+                //Extract the payload of the JWT
+                var claims = token.Claims;
+                var jwtPayload = "{";
+                foreach (Claim c in claims)
+                {
+                    jwtPayload += '"' + c.Type + "\":\"" + c.Value + "\",";
+                }
+                jwtPayload += "}";
+                decode += jwtPayload;
+
+                value = JToken.Parse(jwtPayload).ToString(Formatting.Indented);
+            }
+
+            return value;
         }
     }
 }
