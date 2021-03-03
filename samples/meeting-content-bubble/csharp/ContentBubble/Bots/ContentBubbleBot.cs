d58205423e8dd25871066aeb6aa663c0b40ac1a7
Trinetra Kumar <v-trkum@microsoft.com>
2021-03-03 09:09:09 +0530

    Content Bubble Review Fixes

---
+++ samples/meeting-content-bubble/csharp/ContentBubble/Bots/ContentBubbleBot.cs
@@ -0,0 +1,185 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using AdaptiveCards;
+using AdaptiveCards.Templating;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Teams;
+using Microsoft.Bot.Connector.Authentication;
+using Microsoft.Bot.Schema;
+using Microsoft.Bot.Schema.Teams;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+
+namespace Content_Bubble_Bot
+{
+    public class ContentBubbleBot : TeamsActivityHandler
+    {
+        private readonly IConfiguration _configuration;
+
+        public ContentBubbleBot(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+
+            if (turnContext.Activity.Value == null)
+            {
+                var path = "./Cards/adaptive.json";
+                var adaptiveCardJson = File.ReadAllText(path);
+                var adaptiveCardAttachment = new Attachment
+                {
+                    ContentType = AdaptiveCard.ContentType,
+                    Content = JsonConvert.DeserializeObject(adaptiveCardJson),
+                };
+
+                await turnContext.SendActivityAsync(MessageFactory.Attachment(adaptiveCardAttachment));
+            }
+
+            else
+            {               
+                string selectedoption = string.Empty;
+                JToken commandToken = JToken.Parse(turnContext.Activity.Value.ToString());
+                string command = commandToken["action"].Value<string>();
+
+                selectedoption = commandToken["myReview"].Value<string>();               
+                
+                if (selectedoption == "yes")
+                {
+                    await turnContext.SendActivityAsync(turnContext.Activity.From.Name + " : " + "**Yes** for " + "'" + command + "'");
+                }
+
+                else if (selectedoption == "no")
+                {
+                    await turnContext.SendActivityAsync(turnContext.Activity.From.Name + " : " + "**No** for " + "'" + command + "'");
+                }
+
+                else
+                {
+                    Titles.Title = selectedoption;
+
+                    await FetchTask(turnContext);
+
+                    var templateJson = @"
+                    {
+                        ""type"": ""AdaptiveCard"",
+                         ""version"": ""1.2"",
+                         ""body"": [
+                            {
+                                
+                               ""type"": ""TextBlock"",
+                                ""text"": ""Provide your Feedback!""
+                            },
+                            {
+
+                               ""type"": ""TextBlock"",
+                                ""text"": ""${option}?""
+                            },
+                    
+                            {
+                                 ""type"": ""Input.ChoiceSet"",
+                                  ""id"": ""myReview"",
+                                  ""style"": ""expanded"",
+                                  ""isMultiSelect"": false,
+                                ""wrap"": true,
+                                ""value"": ""1"",
+                                ""choices"": [
+                                       {
+                                          ""title"": ""Yes"",
+                                         ""value"": ""yes""
+                                         },
+                                          {
+                                          ""title"": ""No"",
+                                         ""value"": ""no""
+                                          }
+
+                                       ]
+                                  }
+                             ],
+
+                                ""actions"": [
+                                         {
+                                             ""type"": ""Action.Submit"",
+                                             ""id"": ""submit"",
+                                              ""title"": ""Submit"",
+                                               ""data"": {
+                                                             ""action"": ""${option}""
+                                                       }
+                                               } ]                                 
+                                        }
+                                  ";
+
+                    AdaptiveCardTemplate template = new AdaptiveCardTemplate(templateJson);
+                   
+                    var myData = new
+                    {
+                        Option = selectedoption
+                    };
+                                       
+                    string cardJson = template.Expand(myData);
+                   
+                    var adaptiveCardAttachment = new Attachment
+                    {
+                        ContentType = AdaptiveCard.ContentType,
+                        Content = JsonConvert.DeserializeObject(cardJson),
+                    };
+
+                    await turnContext.SendActivityAsync(MessageFactory.Attachment(adaptiveCardAttachment));
+                }
+            }
+        }
+
+        protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var welcomeText = "Hello and welcome to Content Bubble Bot !";
+            foreach (var member in membersAdded)
+            {
+                if (member.Id != turnContext.Activity.Recipient.Id)
+                {
+                    await turnContext.SendActivityAsync(MessageFactory.Text(welcomeText, welcomeText), cancellationToken);
+                }
+            }
+        }
+
+        protected override async Task<TaskModuleResponse> OnTeamsTaskModuleSubmitAsync(ITurnContext<IInvokeActivity> turnContext, TaskModuleRequest taskModuleRequest, CancellationToken cancellationToken)
+        {
+
+            var replyJson = JObject.Parse(taskModuleRequest.Data.ToString());
+            var reply = Convert.ToString(replyJson["userValue"]);
+            var title = Convert.ToString(replyJson["title"]);
+            var selectedQuestion = title.Split('?');
+            if (reply == "yes")
+            {
+                await turnContext.SendActivityAsync(turnContext.Activity.From.Name + " : " + "**Yes** for " + "'" + selectedQuestion[0] + "'");
+            }
+            else
+            {
+                await turnContext.SendActivityAsync(turnContext.Activity.From.Name + " : " + "**No** for " + "'" + selectedQuestion[0] + "'");
+            }
+            return null;
+        }
+
+        public async static Task FetchTask(ITurnContext turnContext)
+        {
+            Activity activity = MessageFactory.Text("**Please provide your valuable feedback**");
+
+            activity.ChannelData = new TeamsChannelData
+            {
+                Notification = new NotificationInfo()
+                {
+                    AlertInMeeting = true,
+                    ExternalResourceUrl = "https://teams.microsoft.com/l/bubble/"+_configuration["MicrosoftAppId"]+"?url="+_configuration["BaseUrl"]+"/ContentBubble&height=270&width=250&title=ContentBubble&completionBotId="++configuration["MicrosoftAppId"]+""
+                }
+            };
+            await turnContext.SendActivityAsync(activity).ConfigureAwait(false);
+        }
+    }
+}
---
+++ samples/meeting-content-bubble/csharp/README.md
@@ -0,0 +1,53 @@
+# ContentBubbleSample
+This sample illustrates how to implement Content Bubble In-Meeting Experience.
+
+## Prerequisites
+
+- Microsoft Teams is installed and you have an account
+- [.NET Core SDK](https://dotnet.microsoft.com/download) version 3.1
+- [ngrok](https://ngrok.com/) or equivalent tunnelling solution
+
+1) Clone the repository
+
+    ```bash
+    git clone https://github.com/OfficeDev/Microsoft-Teams-Samples.git
+    ```
+
+2) If you are using Visual Studio
+  - Launch Visual Studio
+  - File -> Open -> Project/Solution
+  - Navigate to `meeting-content-bubble\csharp` folder
+  - Select `ContentBubbleBot.sln` file
+
+3) Run ngrok - point to port 3978
+
+    ```bash
+    ngrok http -host-header=rewrite 3978
+    ```
+### Create a bot with Azure Bot Service 
+1. This bot has been created using [Bot Framework](https://dev.botframework.com).
+2. Navigate to app registration page, in the overview section, copy and save the **Application (client) ID**. You’ll need those later when updating your Teams application manifest and in the appsettings.json.
+3. Under **Manage**, select **Expose an API**. 
+4. Select the **Set** link to generate the Application ID URI in the form of `api://{AppID}`. Insert your fully qualified domain name (with a forward slash "/" appended to the end) between the double forward slashes and the GUID. The entire ID should have the form of: `api://fully-qualified-domain-name/{AppID}`
+    * ex: `api://%ngrokDomain%.ngrok.io/00000000-0000-0000-0000-000000000000`.
+5. "ContentBubbleUrl ": Content bubble iframe url (default. https://[BaseUrl]/contentBubble.html). Remember that [BaseUrl] will be your ngrok domain, so the content bubble URL will be similar to
+
+6. Navigate to the **Certificates & secrets**. In the Client secrets section, click on "+ New client secret". Add a description      (Name of the secret) for the secret and select “Never” for Expires. Click "Add". Once the client secret is created, copy its value, it need to be placed in the appsettings.json.
+ 7. __*This step is specific to Teams.*__
+    - **Edit** the `manifest.json` contained in the `Manifest` folder to replace your Microsoft App Id (that was created when you registered your bot earlier) *everywhere* you         see the place holder string `<<YOUR-MICROSOFT-APP-ID>>` (depending on the scenario the Microsoft App Id may occur multiple times in the `manifest.json`)
+    - **Add** the ngrok domain to the valid domains array in the manifest. 
+    - Fill-in the following `webApplicationInfo` section, using `MicrosoftAppId` and `BaseUrl` values from the previous section.
+        ```json
+        "webApplicationInfo": {  
+          "id": "[MicrosoftAppId]",  
+          "resource": "api://[BaseUrl]/[MicrosoftAppId]"  
+          }
+        ```
+    - **Zip** up the contents of the `teamsAppManifest` folder to create a `manifest.zip`
+    - **Upload** the `manifest.zip` to Teams (in the Apps view click "Upload a custom app")
+ 8. __*Update External resource URL*__ in `ContentBubbleBot.cs` file. **Replace** AppID, BotID with MicrosoftAppID and Url with ngrok Url.
+ 9. Run your app, either from Visual Studio with `F5` or using `dotnet run` in the appropriate folder. 
+
+## Interacting with the app in Teams Meeting
+
+Install the App in Teams Meeting, message the Bot by @ mentioning to interact with the content bubble.
