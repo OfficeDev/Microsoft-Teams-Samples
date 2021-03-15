e948977424b1b5064eee8ee3c152a7ff12ff092e
Yitong Feng <yife@microsoft.com>
2021-03-15 14:45:05 +0800

    Tab SSO C#

---
+++ samples/tab-personal-sso-quickstart/csharp_dotnetcore/Controllers/AuthController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace TeamsTabSSO.Controllers
+{
+    public class AuthController : Controller
+    {
+
+        private readonly IConfiguration _configuration;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HomeController"/> class.
+        /// </summary>
+        /// <param name="configuration">IConfiguration instance.</param>
+        public AuthController(
+            IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        /// <summary>
+        /// Authentication start
+        /// </summary>
+        /// <returns>current View</returns>
+        public IActionResult Start()
+        {
+            ViewBag.AzureClientId = _configuration["AzureAd:ClientId"];
+            return View();
+        }
+
+        /// <summary>
+        /// Authentication End
+        /// </summary>
+        /// <returns>current View</returns>
+        public IActionResult End()
+        {
+            return View();
+        }
+    }
+}
