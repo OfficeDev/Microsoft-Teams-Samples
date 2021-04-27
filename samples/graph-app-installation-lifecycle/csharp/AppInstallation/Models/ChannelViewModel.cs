0bb48a4b2d97d72364f26e60e5205a009156c290
Trinetra-MSFT <v-trkum@microsoft.com>
2021-04-27 22:26:59 +0530

    Graph App Installation Life Cycle using Tab csharp (#72)
    * Graph App Installation Life Cycle using Tab csharp
    
    1. Covered List apps
    2. Update app
    3. Delete app
    4. Upgrade app
    
    This reverts commit a60e31e337ee66a43b4effc41407ccc8a64dbe94.
    Co-authored-by: Wajeed-msft <31851992+Wajeed-msft@users.noreply.github.com>

---
+++ samples/graph-app-installation-lifecycle/csharp/AppInstallation/Models/ChannelViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Graph;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppInstallation.Models
+{
+    public class AppViewModel
+    {
+        public List<AppModel> appList { get; set; }
+        public List<string> TabsList { get; set; }
+        public List<string> Members { get; set; }
+        public string TenantId { get; set; }
+        public string GroupId { get; set; }
+        public bool IsUserList { get; set; }
+    }
+
+    public class AppModel
+    {     
+        public string AppId { get; set; }
+        public string AppName { get; set; }
+        public string AppDesc { get; set; }
+        public string AppDistributionMethod { get; set; }
+
+    }
+}
