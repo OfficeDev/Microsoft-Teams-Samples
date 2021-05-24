67539b0abf250316b7e305c28275b2690eb1568f
dipja <61967816+dipja@users.noreply.github.com>
2021-05-24 21:01:14 +0530

    Adding Teams catering bot sample  (#95)
    * adding sp upload app which uses universal bots feature
    
    * adding catering bot app sample code
    
    * adding configuration injection to CateringBot
    
    * cleaan up - 1
    
    * cleaning up startup.cs file
    
    * renaming bot folder
    
    * adding README file to catering bot code
    
    * fixing image links in read me
    
    * adding images to README.md
    
    * addressing feedback
    
    * addressing feedback - 2
    
    * removing sp-uploader from this branch
    
    Co-authored-by: dipja <dipja@Dipeshs-MacBook-Pro.local>

---
+++ samples/bot-teams-catering/csharp/Catering/Models/Card.cs
@@ -0,0 +1,11 @@
+﻿namespace Catering.Models
+{
+    public enum Card : int
+    {
+        Entre = 0,
+        Drink = 1,
+        Review = 2,
+        ReviewAll = 3,
+        Confirmation = 4
+    }
+}
---
+++ samples/bot-teams-catering/csharp/Microsoft.Bot.AdaptiveCards/AdaptiveCardInvokeResponse.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace Microsoft.Bot.AdaptiveCards
+{
+    public class AdaptiveCardInvokeResponse
+    {
+        [JsonProperty("statusCode")]
+        public int StatusCode { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("value")]
+        public object Value { get; set; }
+    }
+}
