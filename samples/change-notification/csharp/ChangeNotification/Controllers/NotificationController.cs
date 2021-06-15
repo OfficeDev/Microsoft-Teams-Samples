2420aed2c56b3782baaa117bf7886810add06f98
Sudhir Sharma (ZEN3 INFOSOLUTIONS PRIVATE LIM) <v-sharmasu@microsoft.com>
2021-06-15 11:57:40 +0530

    Change-Notification csharp sample.
    Change-Notification csharp sample.

---
+++ samples/change-notification/csharp/ChangeNotification/Controllers/NotificationController.cs
@@ -0,0 +1,104 @@
+﻿using ChangeNotification.Helper;
+using ChangeNotification.Helper;
+using ChangeNotification.Model;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ChangeNotification.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotificationsController : Controller
+    {
+        private readonly IConfiguration _config;
+       // private readonly string _rootPath;
+        private readonly ILogger _logger;
+        public static  ITurnContext stepContext;
+
+        private readonly static ConcurrentDictionary<string, bool> _processedMessage = new ConcurrentDictionary<string, bool>(2, 64);
+
+        public NotificationsController(IConfiguration config,
+            ILogger<NotificationsController> logger,
+            UserState userState)
+        {
+            _config = config;
+            _logger = logger;
+            ;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Get()
+        {
+            return  Json(SubscriptionManager.Subscriptions.Select(s => new
+            {
+                Resource = s.Value.Resource,
+                ExpirationDateTime = TimeZoneInfo.ConvertTimeBySystemTimeZoneId(s.Value.ExpirationDateTime.Value.DateTime, "UTC", "India Standard Time").ToString("MM/dd/yyyy h:mm tt"),
+                Expired = s.Value.ExpirationDateTime < DateTime.UtcNow
+            }));
+        }
+
+        // Callback
+        [Route("/api/webhookLifecyle")]
+        [HttpPost]
+        public ActionResult WebhookLifecyle([FromQuery] string validationToken = null)
+        {
+            if (validationToken != null)
+            {
+                // Ack the webhook subscription
+                return Ok(validationToken);
+            }
+            else
+            {
+                _logger.LogCritical("To do -- handle authorization challenge");
+                return null;
+            }
+        }
+
+        public async Task<ActionResult<string>> Post([FromQuery] string validationToken = null)
+        {
+            // handle validation
+            if (!string.IsNullOrEmpty(validationToken))
+            {
+                _logger.LogInformation($"Received Token: '{validationToken}'");
+
+                return Ok(validationToken);
+            }
+
+            // handle notifications
+            using (StreamReader reader = new StreamReader(Request.Body))
+            {
+                string content = await reader.ReadToEndAsync();
+
+                var notifications = JsonConvert.DeserializeObject<Notifications>(content);
+
+                Console.WriteLine(content);
+                ResourceData resourceData = new ResourceData();
+                foreach (var notification in notifications.Items)
+                {
+                    resourceData = notification.ResourceData;
+                }
+                if (stepContext != null)
+                {
+                   
+                    ChangeNotificationHelper changeNotificationHelper = new ChangeNotificationHelper();
+                    var attachData = changeNotificationHelper.ShowAdaptiveCard("User Presence", resourceData);
+                    await stepContext.SendActivityAsync(MessageFactory.Attachment(attachData));
+                }
+
+                return Ok();
+            }
+
+        }
+    }
+}
---
+++ samples/change-notification/csharp/ChangeNotification/Helper/ChangeNotification.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.IO;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Graph;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+using AdaptiveCards;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace ChangeNotification.Helper
+{
+    public class ChangeNotificationHelper
+    {
+
+        public Microsoft.Bot.Schema.Attachment ShowAdaptiveCard(string Header, ChangeNotification.Model.ResourceData InfoData)
+        {
+            AdaptiveCard adaptiveCard = new AdaptiveCard(new AdaptiveSchemaVersion(1, 0));
+            adaptiveCard.Body = new List<AdaptiveElement>()
+            {
+                new AdaptiveTextBlock(){Text=Header, Weight=AdaptiveTextWeight.Bolder}
+            };
+
+            AdaptiveTextBlock textBlock = new AdaptiveTextBlock()
+            {
+                Text = "- " + "Availability : " + InfoData.Availability + " \r" +
+                        "- " + "Activity : " + InfoData.Activity + " \r",
+
+            };
+            adaptiveCard.Body.Add(textBlock);
+
+
+            return new Microsoft.Bot.Schema.Attachment()
+            {
+                ContentType = AdaptiveCard.ContentType,
+                Content = adaptiveCard
+            };
+        }
+    }
+
+}
