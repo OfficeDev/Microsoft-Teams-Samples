da6bc21849b079cfa4631ad3e04ed41875975c91
Wajeed-msft <31851992+Wajeed-msft@users.noreply.github.com>
2021-06-23 00:06:09 +0530

    Fixed indentations

--- samples/graph-chat-lifecycle/nodejs/src/static/scripts/auth.js
+++ samples/graph-chat-lifecycle/nodejs/src/static/scripts/auth.js
@@ -1,42 +1,42 @@
 microsoftTeams.initialize();
 
 getClientSideToken()
-.then((clientSideToken) => {
-return getServerSideToken(clientSideToken);
-})
-.then((serverSideToken) => {
-return useServerSideToken(serverSideToken);
-})
-.catch((error) => {
-if (error === "invalid_grant") {
-    display(`Error: ${error} - user or admin consent required`);
-    // Display in-line button so user can consent
-    let button = display("Consent", "button");
-    button.onclick = (() => {
-        requestConsent()
-            .then((result) => {
-                // Consent succeeded - use the token we got back
-                let accessToken = JSON.parse(result).accessToken;
-                useServerSideToken(accessToken);
-            })
-            .catch((error) => {
-                display(`ERROR ${error}`);
-                // Consent failed - offer to refresh the page
-                button.disabled = true;
-                let refreshButton = display("Refresh page", "button");
-                refreshButton.onclick = (() => { window.location.reload(); });
+    .then((clientSideToken) => {
+        return getServerSideToken(clientSideToken);
+    })
+    .then((serverSideToken) => {
+        return useServerSideToken(serverSideToken);
+    })
+    .catch((error) => {
+        if (error === "invalid_grant") {
+            display(`Error: ${error} - user or admin consent required`);
+            // Display in-line button so user can consent
+            let button = display("Consent", "button");
+            button.onclick = (() => {
+                requestConsent()
+                    .then((result) => {
+                        // Consent succeeded - use the token we got back
+                        let accessToken = JSON.parse(result).accessToken;
+                        useServerSideToken(accessToken);
+                    })
+                    .catch((error) => {
+                        display(`ERROR ${error}`);
+                        // Consent failed - offer to refresh the page
+                        button.disabled = true;
+                        let refreshButton = display("Refresh page", "button");
+                        refreshButton.onclick = (() => { window.location.reload(); });
+                    });
             });
+        } else {
+            // Something else went wrong
+            display(`Error from web service: ${error}`);
+        }
     });
-} else {
-    // Something else went wrong
-    display(`Error from web service: ${error}`);
-}
-});
 
 function getClientSideToken() {
 
     return new Promise((resolve, reject) => {
-    
+
         microsoftTeams.authentication.getAuthToken({
             successCallback: (result) => {
                 console.log(result);
@@ -46,19 +46,19 @@ function getClientSideToken() {
                 reject("Error getting token: " + error);
             }
         });
-    
+
     });
-    
-    }
-    
-    // 2. Exchange that token for a token with the required permissions
-    //    using the web service (see /auth/token handler in app.js)
-    function getServerSideToken(clientSideToken) {
-        
+
+}
+
+// 2. Exchange that token for a token with the required permissions
+//    using the web service (see /auth/token handler in app.js)
+function getServerSideToken(clientSideToken) {
+
     return new Promise((resolve, reject) => {
-    
+
         microsoftTeams.getContext((context) => {
-    
+
             fetch('/auth/token', {
                 method: 'post',
                 headers: {
@@ -66,36 +66,36 @@ function getClientSideToken() {
                 },
                 body: JSON.stringify({
                     'tid': context.tid,
-                    'token': clientSideToken 
+                    'token': clientSideToken
                 }),
                 mode: 'cors',
                 cache: 'default'
             })
-            .then((response) => {
-                console.log(response);
-                if (response.ok) {
-                    return response.json();
-                } else {
-                    reject(response.error);
-                }
-            })
-            .then((responseJson) => {
-                if (responseJson.error) {
-                    reject(responseJson.error);
-                } else {
-                     serverSideToken = responseJson;
-                     localStorage.setItem("accessToken",serverSideToken);
-                     $("#createGroupChat").show();
-                    resolve(serverSideToken);
-                }
-            });
+                .then((response) => {
+                    console.log(response);
+                    if (response.ok) {
+                        return response.json();
+                    } else {
+                        reject(response.error);
+                    }
+                })
+                .then((responseJson) => {
+                    if (responseJson.error) {
+                        reject(responseJson.error);
+                    } else {
+                        serverSideToken = responseJson;
+                        localStorage.setItem("accessToken", serverSideToken);
+                        $("#createGroupChat").show();
+                        resolve(serverSideToken);
+                    }
+                });
         });
     });
-    }
-    
-    // 3. Get the server side token and use it to call the Graph API
-    function useServerSideToken(data) {
-        
+}
+
+// 3. Get the server side token and use it to call the Graph API
+function useServerSideToken(data) {
+
     return fetch("https://graph.microsoft.com/v1.0/me/",
         {
             method: 'GET',
@@ -107,20 +107,20 @@ function getClientSideToken() {
             cache: 'default'
         })
         .then((response) => {
-            if (response.ok) {          
-                return response.json();                    
+            if (response.ok) {
+                return response.json();
             } else {
                 throw (`Error ${response.status}: ${response.statusText}`);
             }
         })
         .then((profile) => {
-           // display(JSON.stringify(profile, undefined, 4), 'pre');
+            // display(JSON.stringify(profile, undefined, 4), 'pre');
         });
-    
-    }
-    
-    // Show the consent pop-up
-    function requestConsent() {
+
+}
+
+// Show the consent pop-up
+function requestConsent() {
     return new Promise((resolve, reject) => {
         microsoftTeams.authentication.authenticate({
             url: window.location.origin + "/auth/auth-start",
@@ -136,15 +136,14 @@ function getClientSideToken() {
             }
         });
     });
-    }
-    
-    // Add text to the display in a <p> or other HTML element
-    function display(text, elementTag) {
+}
+
+// Add text to the display in a <p> or other HTML element
+function display(text, elementTag) {
     var logDiv = document.getElementById('logs');
     var p = document.createElement(elementTag ? elementTag : "p");
     p.innerText = text;
     logDiv.append(p);
     console.log("ssoDemo: " + text);
     return p;
-    }
-    
+}
