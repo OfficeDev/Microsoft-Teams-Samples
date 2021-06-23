57ec3f9a1fc15dd5845af2a6930e228fcaa489da
Sudhir Sharma (ZEN3 INFOSOLUTIONS PRIVATE LIM) <v-sharmasu@microsoft.com>
2021-06-23 18:34:02 +0530

    Change-Notification nodejs sample.
    Change-Notification nodejs sample.

---
+++ samples/graph-change-notification/nodejs/constant.js
@@ -0,0 +1,6 @@
+exports.subscriptionConfiguration = {
+    changeType: 'updated',
+    notificationUrl: 'https://*******.ngrok.io/api/notifications',
+    clientState: 'ClientState',
+    latestSupportedTlsVersion: "v1_2"
+  };
