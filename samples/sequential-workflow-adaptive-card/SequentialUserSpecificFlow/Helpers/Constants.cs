9178e7ecb96add97aca6f1f8b90040992a4ea7b1
Madhuri-MSFT <v-merraboina@microsoft.com>
2021-06-28 20:47:23 +0530

    initial commit for sequential flow, user specific bot c#

---
+++ samples/sequential-workflow-adaptive-card/SequentialUserSpecificFlow/Helpers/Constants.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace SequentialUserSpecificFlow.Helpers
+{
+    public class Constants
+    {
+        public static List<string> Hardware = new List<string>() { "Display Issue", "Keyboard Issue" };
+
+        public static List<string> Software = new List<string>() {"Software Installation", "Software Un-Installation" };
+    }
+}
---
+++ samples/sequential-workflow-adaptive-card/SequentialUserSpecificFlow/Models/InitialSequentialCard.cs
@@ -0,0 +1,25 @@
+﻿namespace SequentialUserSpecificFlow.Models
+{
+    public class InitialSequentialCard
+    {
+        public Action action { get; set; }
+        public string trigger { get; set; }
+    }
+    public class Action
+    {
+        public string type { get; set; }
+        public string title { get; set; }
+        public Data data { get; set; }
+        public string verb { get; set; }
+    }
+    public class Data
+    {
+        public string CreatedBy { get; set; }
+        public string IncidentTitle { get; set; }
+        public string AssignedTo { get; set; }
+        public string Category { get; set; }
+        public string SubCategory { get; set; }
+        public string AssignedToName { get; set; }
+        public string UserMRI { get; set; }
+    }
+}
