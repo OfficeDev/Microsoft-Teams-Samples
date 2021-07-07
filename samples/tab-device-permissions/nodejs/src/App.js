b3610876406ce3dc99dc474a158a353a35de5c6d
AbhijeetThakur-msft <v-abt@microsoft.com>
2021-07-07 15:34:35 +0530

    code setup for device permissions check in tab

---
+++ samples/tab-device-permissions/nodejs/src/App.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import Tab from './components/Tab'
+
+function App() {
+  return (
+    <div className="App">
+      <Tab></Tab>
+    </div>
+  );
+}
+
+export default App;
