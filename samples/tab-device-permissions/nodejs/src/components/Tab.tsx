e5c094f531d8eebbd5a2842370204d2dc1ad2b00
AbhijeetThakur-msft <v-abt@microsoft.com>
2021-07-08 01:21:11 +0530

    updating code for testing device permissions and adding cards for respective action items

--- samples/tab-device-permissions/nodejs/src/components/Tab.tsx
+++ samples/tab-device-permissions/nodejs/src/components/Tab.tsx
@@ -3,6 +3,7 @@
 
 import React, { useEffect } from 'react';
 import * as microsoftTeams from "@microsoft/teams-js";
+import { Card, Flex, Text, Button, CardHeader, CardBody } from '@fluentui/react-northstar'
 
 /**
  * The 'PersonalTab' component renders the main tab content
@@ -18,59 +19,207 @@ const Tab = () => {
   });
  })
 
-  //method to validate before capturing media.
-  function captureMedia() {
-      navigator.permissions.query({name:'microphone'}).then(function(result) {
-        console.log(result)
-      if (result.state == 'granted') {
-        console.log(result)
-        let mediaInput: microsoftTeams.media.MediaInputs = {
-          mediaType: microsoftTeams.media.MediaType.Audio,
-          maxMediaCount: 1,
-      };
-      microsoftTeams.media.selectMedia(mediaInput, (error: microsoftTeams.SdkError, attachments: microsoftTeams.media.Media[]) => {
-        debugger
-          if (error) {
-              if (error.message) {
-                  alert(" ErrorCode: " + error.errorCode + error.message);
-              } else {
-                  alert(" ErrorCode: " + error.errorCode);
-              }
-          }
-          // If you want to directly use the audio file (for smaller file sizes (~4MB))    if (attachments) {
-          let audioResult = attachments[0];
-          var videoElement = document.createElement("video");
-          videoElement.setAttribute("src", ("data:" + audioResult.mimeType + ";base64," + audioResult.preview));
-          //To use the audio file via get Media API for bigger audio file sizes greater than 4MB       
-          audioResult.getMedia((error: microsoftTeams.SdkError, blob: Blob) => {
-          if (blob) {
-              if (blob.type.includes("video")) {
-                  videoElement.setAttribute("src", URL.createObjectURL(blob));
-              }
-          }
-          if (error) {
-              if (error.message) {
-                  alert(" ErrorCode: " + error.errorCode + error.message);
-              } else {
-                  alert(" ErrorCode: " + error.errorCode);
-              }
-          }
-      });
-      }); 
-    } 
-    else if (result.state == 'prompt') {
-        // Access has not been granted
-        console.log(result)
-        navigator.mediaDevices.getUserMedia({ audio: true, video: true });
+ function scanBarCode()
+ {
+  const config: microsoftTeams.media.BarCodeConfig = {
+    timeOutIntervalInSec: 30};
+  microsoftTeams.media.scanBarCode((error: microsoftTeams.SdkError, decodedText: string) => {
+    if (error) {
+      if (error.message) {
+        alert(" ErrorCode: " + error.errorCode + error.message);
+      } else {
+        alert(" ErrorCode: " + error.errorCode);
       }
+    } else if (decodedText) {
+      alert(decodedText);
+    }
+  }, config);
+ }
+
+ function captureImage(){
+  microsoftTeams.media.captureImage((error: microsoftTeams.SdkError, files: microsoftTeams.media.File[]) => {
+   
+  });
+ }
+
+   //method to validate before capturing media.
+  function captureMultipleImages() {
+    microsoftTeams.media.selectMedia({ maxMediaCount: 10, mediaType: microsoftTeams.media.MediaType.Image }, (error: microsoftTeams.SdkError, attachments: microsoftTeams.media.Media[]) => {
+      /* ... */
+    });
+    //   navigator.permissions.query({name:'microphone'}).then(function(result) {
+    //     console.log(result)
+    //   if (result.state == 'granted') {
+    //     console.log(result)
+    //     let mediaInput: microsoftTeams.media.MediaInputs = {
+    //       mediaType: microsoftTeams.media.MediaType.Audio,
+    //       maxMediaCount: 1,
+    //   };
+    //   microsoftTeams.media.selectMedia(mediaInput, (error: microsoftTeams.SdkError, attachments: microsoftTeams.media.Media[]) => {
+    //     console.log(microsoftTeams)
+    //       if (error) {
+    //           if (error.message) {
+    //               alert(" ErrorCode: " + error.errorCode + error.message);
+    //           } else {
+    //               alert(" ErrorCode: " + error.errorCode);
+    //           }
+    //       }
+    //       // If you want to directly use the audio file (for smaller file sizes (~4MB))    
+    //       if (attachments) {
+    //       let audioResult = attachments[0];
+    //       var videoElement = document.createElement("video");
+    //       videoElement.setAttribute("src", ("data:" + audioResult.mimeType + ";base64," + audioResult.preview));
+    //       //To use the audio file via get Media API for bigger audio file sizes greater than 4MB       
+    //       audioResult.getMedia((error: microsoftTeams.SdkError, blob: Blob) => {
+    //       if (blob) {
+    //           if (blob.type.includes("video")) {
+    //               videoElement.setAttribute("src", URL.createObjectURL(blob));
+    //           }
+    //       }
+    //       if (error) {
+    //           if (error.message) {
+    //               alert(" ErrorCode: " + error.errorCode + error.message);
+    //           } else {
+    //               alert(" ErrorCode: " + error.errorCode);
+    //           }
+    //       }
+          
+    //   });
+    // }
+    //   }); 
+    // } 
+    // else if (result.state == 'prompt') {
+    //     // Access has not been granted
+    //     console.log(result)
+    //     navigator.mediaDevices.getUserMedia({ audio: true, video: true });
+    //   }
+    // });
+  }
+
+  function captureAudio() {
+    microsoftTeams.media.selectMedia({ maxMediaCount: 1, mediaType: microsoftTeams.media.MediaType.Audio }, (error: microsoftTeams.SdkError, attachments: microsoftTeams.media.Media[]) => {
+      /* ... */
+    });
+  }
+  
+  function getLocation () {
+      microsoftTeams.location.getLocation({ allowChooseLocation: false, showMap: false }, (error: microsoftTeams.SdkError, location: microsoftTeams.location.Location) => {
+        alert(error.errorCode)
+       });
+  }
+
+  // Method to show geo location for given latitude and longitude values.
+  function showLocation () {
+  let location = {"latitude":28.704059,"longitude":77.10249};
+  microsoftTeams.location.showLocation(location, (err: microsoftTeams.SdkError, result: boolean) => {
+          if (err) {
+            alert(err);
+            return;
+          }
     });
   }
 
+  function selectPeople()
+ {
+  microsoftTeams.people.selectPeople((error: microsoftTeams.SdkError, people: microsoftTeams.people.PeoplePickerResult[]) => 
+ {
+    if (error) 
+    {
+        if (error.message) 
+           {
+             alert(" ErrorCode: " + error.errorCode + error.message);
+           }
+            else 
+            {
+              alert(" ErrorCode: " + error.errorCode);
+            }
+      }
+    if (people)
+     {
+            alert("People length: " + people.length);
+      }
+  });
+ }
+ 
     return(
-      <>
-       <div>Tab content</div>
-       <button onClick={captureMedia}>Capture Video</button>
-      </>
+      <Flex column gap="gap.large">
+       {/* Card for capturing single image */}
+      <Card>
+        <CardHeader>
+          <Text content="Capture Image (Mobile Only)" weight="bold" />
+        </CardHeader>
+        <CardBody>
+        <Flex column gap="gap.small">
+        <Text content="Checks for permission before capturing image" />
+          <Button content="Capture Image" onClick={captureImage}/>
+        </Flex>
+        </CardBody>
+      </Card>
+
+      {/* Card for showing multiple images */}
+      <Card>
+        <CardHeader>
+          <Text content="Capture Multiple Image (Mobile Only)" weight="bold" />
+        </CardHeader>
+        <CardBody>
+        <Flex column gap="gap.small">
+        <Text content="Checks for permission before capturing image. You can capture multiple images or select them grom gallery." />
+          <Button content="Capture multiple images" onClick={captureMultipleImages}/>
+        </Flex>
+        </CardBody>
+      </Card>
+
+       {/* Card for showing Video/Audio */}
+       <Card>
+        <CardHeader>
+          <Text content="Media (Mobile Only)" weight="bold" />
+        </CardHeader>
+        <CardBody>
+        <Flex column gap="gap.small">
+        <Text content="Checks for permission to use media input" />
+          <Button content="Capture audio" onClick={captureAudio}/>
+        </Flex>
+        </CardBody>
+      </Card>
+
+      {/* Card for Barcode Scanner */}
+      <Card>
+        <CardHeader>
+          <Text content="Scan Barcode (Mobile Only)" weight="bold" />
+        </CardHeader>
+        <CardBody>
+        <Flex column gap="gap.small">
+        <Text content="Scan any barcode to get information related to it" />
+           <Button content="Scan Barcode" onClick={scanBarCode}/>
+        </Flex>
+        </CardBody>
+      </Card>
+
+      {/* Card for People Picker */}
+      <Card>
+        <CardHeader>
+          <Text content="People Picker (Mobile Only)" weight="bold" />
+        </CardHeader>
+        <CardBody>
+        <Flex column gap="gap.small">
+          <Button content="People Picker" onClick={selectPeople}/>
+        </Flex>
+        </CardBody>
+      </Card>
+
+      {/* Card for Get/Show Geo-Location */}
+      <Card>
+        <CardHeader>
+          <Text content="Get Geo Location (Mobile Only)" weight="bold" />
+        </CardHeader>
+        <CardBody>
+        <Flex column gap="gap.small">
+          <Button content="Get Location" onClick={getLocation}/>
+          <Button content="Show Location" onClick={showLocation}/>
+        </Flex>
+        </CardBody>
+      </Card>
+      </Flex>
     );
 }
 export default Tab;
