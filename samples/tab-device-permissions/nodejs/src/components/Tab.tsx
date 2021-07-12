b9548160eea6fbbe2a4579b57c7e6a39e75565bb
Kalyan Chimmiri <v-kalc@microsoft.com>
2021-07-12 17:14:56 +0530

    Added grid for tab

--- samples/tab-device-permissions/nodejs/src/components/Tab.tsx
+++ samples/tab-device-permissions/nodejs/src/components/Tab.tsx
@@ -1,12 +1,39 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
-import { Flex } from '@fluentui/react-northstar'
+import { Grid, Segment } from '@fluentui/react-northstar'
 import ScanBarCode from './ScanBarCode';
 import GetGeoLocation from './GetGeoLocation';
 import CaptureImage from './CaptureImage';
 import PeoplePicker from './PeoplePicker';
 import CaptureAudio from './CaptureAudio';
+
+const getTabContent = () => {
+  return (
+    <Grid columns={window.innerWidth < 720 ? 1 : 5}>
+      <Segment
+        /* Component to capture image(s) */
+        content={<CaptureImage />}
+      />
+      <Segment
+        /* Component to capture audio */
+        content={<CaptureAudio />}
+      />
+      <Segment
+        /* Component to scan barcode */
+        content={<ScanBarCode />}
+      />
+      <Segment
+        /* Component to show selected people */
+        content={<PeoplePicker />}
+      />
+      <Segment
+        /* Component to Get/Show geo-Location */
+        content={<GetGeoLocation />}
+      />
+    </Grid>
+  )
+}
 
 /**
  * The 'Tab' contains all the components
@@ -14,22 +41,7 @@ import CaptureAudio from './CaptureAudio';
  */
 const Tab = () => {
   return (
-    <Flex column gap="gap.large">
-      {/* Component to capture image(s) */}
-      <CaptureImage />
-
-      {/* Component to capture audio */}
-      <CaptureAudio />
-
-      {/* Component to scan barcode */}
-      <ScanBarCode />
-
-      {/* Component to show selected people */}
-      <PeoplePicker />
-
-      {/* Component to Get/Show geo-Location */}
-      <GetGeoLocation />
-    </Flex>
+    getTabContent()
   );
 }
 
