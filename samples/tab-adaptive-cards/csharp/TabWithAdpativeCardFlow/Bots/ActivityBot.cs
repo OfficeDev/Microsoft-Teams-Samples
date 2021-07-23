1188b6f07b41a28643255b9025a124487bbf2f23
Kalyan Chimmiri <v-kalc@microsoft.com>
2021-07-23 23:29:23 +0530

    Added entire flow as per node js and changed Adaptive cards

--- samples/tab-adaptive-cards/csharp/TabWithAdpativeCardFlow/Bots/ActivityBot.cs
+++ samples/tab-adaptive-cards/csharp/TabWithAdpativeCardFlow/Bots/ActivityBot.cs
@@ -6,7 +6,6 @@ using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
-using AdaptiveCards;
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Builder.Teams;
 using Microsoft.Bot.Connector;
@@ -14,7 +13,7 @@ using Microsoft.Bot.Connector.Authentication;
 using Microsoft.Bot.Schema;
 using Microsoft.Bot.Schema.Teams;
 using Microsoft.Extensions.Configuration;
-using TabWithAdpativeCardFlow.Models;
+using TabWithAdpativeCardFlow.Helpers;
 
 namespace TabWithAdpativeCardFlow.Bots
 {
@@ -41,43 +40,45 @@ namespace TabWithAdpativeCardFlow.Bots
         /// <returns></returns>
         protected override async Task<InvokeResponse> OnInvokeActivityAsync(ITurnContext<IInvokeActivity> turnContext, CancellationToken cancellationToken)
         {
-            var state = turnContext.TurnState.ToString(); // Check the state value
-
-            var token = await this.oAuthClient.UserToken.GetAadTokensAsync(turnContext.Activity.From.Id, _connectionName, new Microsoft.Bot.Schema.AadResourceUrls { ResourceUrls = new string[] { "https://graph.microsoft.com/" } });
-            var tokenResponse = await GetTokenResponse(turnContext, state, cancellationToken);
-            if (tokenResponse == null || string.IsNullOrEmpty(tokenResponse.Token))
-            {
-                // There is no token, so the user has not signed in yet.
-
-                // Retrieve the OAuth Sign in Link to use in the MessagingExtensionResult Suggested Actions
-                var signInLink = await GetSignInLinkAsync(turnContext, cancellationToken);
-
-                return CreateInvokeResponse(new TabResponse
-                {
-                    Tab = new TabResponsePayload
-                    {
-                        Type = "auth",
-                        SuggestedActions = new TabSuggestedActions
-                        {
-                            Actions = new List<CardAction>
-                            {
-                                new CardAction
-                                {
-                                    Type = ActionTypes.OpenUrl,
-                                    Value = signInLink,
-                                    Title = "Sign in to this app",
-                                },
-                            },
-                        },
-                    },
-                });
-            }
-
-            var client = new SimpleGraphClient(tokenResponse.Token);
-            var profile = await client.GetMyProfile();
+            var userTokenClient = turnContext.TurnState.Get<UserTokenClient>();
 
             if (turnContext.Activity.Name == "tab/fetch")
             {
+                var state = turnContext.TurnState.ToString(); // Check the state value
+                var token = await this.oAuthClient.UserToken.GetAadTokensAsync(turnContext.Activity.From.Id, _connectionName, new Microsoft.Bot.Schema.AadResourceUrls { ResourceUrls = new string[] { "https://graph.microsoft.com/" } });
+                var tokenResponse = await GetTokenResponse(turnContext, state, cancellationToken);
+                var client = new SimpleGraphClient(tokenResponse.Token);
+                var profile = await client.GetMyProfile();
+
+                if (tokenResponse == null || string.IsNullOrEmpty(tokenResponse.Token))
+                {
+                    // There is no token, so the user has not signed in yet.
+
+                    var resource = await userTokenClient.GetSignInResourceAsync(_connectionName, turnContext.Activity as Activity, null, cancellationToken);
+                    // Retrieve the OAuth Sign in Link to use in the MessagingExtensionResult Suggested Actions
+                    var signInLink = resource.SignInLink;
+
+                    return CreateInvokeResponse(new TabResponse
+                    {
+                        Tab = new TabResponsePayload
+                        {
+                            Type = "auth",
+                            SuggestedActions = new TabSuggestedActions
+                            {
+                                Actions = new List<CardAction>
+                                {
+                                    new CardAction
+                                    {
+                                        Type = ActionTypes.OpenUrl,
+                                        Value = signInLink,
+                                        Title = "Sign in to this app",
+                                    },
+                                },
+                            },
+                        },
+                    });
+                }
+
                 return CreateInvokeResponse(new TabResponse
                 {
                     Tab = new TabResponsePayload
@@ -89,11 +90,11 @@ namespace TabWithAdpativeCardFlow.Bots
                             {
                                 new TabResponseCard
                                 {
-                                    Card = GetAdaptiveCard1()
+                                    Card = CardHelper.GetAdaptiveCard1()
                                 },
                                 new TabResponseCard
                                 {
-                                    Card = GetAdaptiveCard2()
+                                    Card = CardHelper.GetAdaptiveCard2()
                                 },
                             },
                         },
@@ -102,6 +103,8 @@ namespace TabWithAdpativeCardFlow.Bots
             }
             else if (turnContext.Activity.Name == "tab/submit")
             {
+                await userTokenClient.SignOutUserAsync(turnContext.Activity.From.Id, _connectionName, turnContext.Activity.ChannelId, cancellationToken);
+
                 return CreateInvokeResponse(new TabResponse
                 {
                     Tab = new TabResponsePayload
@@ -113,7 +116,7 @@ namespace TabWithAdpativeCardFlow.Bots
                             {
                                 new TabResponseCard
                                 {
-                                    Card = GetAdaptiveCard2()
+                                    Card = CardHelper.GetSignOutCard()
                                 },
                             },
                         },
@@ -129,7 +132,7 @@ namespace TabWithAdpativeCardFlow.Bots
                         Type = "continue",
                         Value = new TaskModuleTaskInfo()
                         {
-                            Card = GetAdaptiveCardForTaskModule(),
+                            Card = CardHelper.GetAdaptiveCardForTaskModule(),
                             Height = 250,
                             Width = 400,
                             Title = "Sample Adaptive Card",
@@ -137,135 +140,28 @@ namespace TabWithAdpativeCardFlow.Bots
                     },
                 });
             }
-
-            return null;
-        }
-
-        /// <summary>
-        /// Sample Adaptive card
-        /// </summary>
-        /// <returns></returns>
-        private AdaptiveCard GetAdaptiveCard1()
-        {
-            AdaptiveCard card = new AdaptiveCard(new AdaptiveSchemaVersion("1.2"))
+            else if (turnContext.Activity.Name == "task/submit")
             {
-                Body = new List<AdaptiveElement>
+                return CreateInvokeResponse(new TabResponse
                 {
-                    new AdaptiveTextBlock
+                    Tab = new TabResponsePayload
                     {
-                        Text = "Hello:",
-                        Size = AdaptiveTextSize.Medium,
-                        Weight = AdaptiveTextWeight.Bolder,
-                    },
-                    new AdaptiveImage
-                    {
-                        Url = new Uri("https://cdn.vox-cdn.com/thumbor/Ndb49Uk3hjiquS041NDD0tPDPAs=/0x169:1423x914/fit-in/1200x630/cdn.vox-cdn.com/uploads/chorus_asset/file/7342855/microsoftteams.0.jpg"),
-                        AltText = "AlternativeText",
-                        PixelHeight = 50,
-                        PixelWidth = 50,
-                        Spacing = AdaptiveSpacing.Medium,
-                    },
-                },
-                Actions = new List<AdaptiveAction>
-                {
-                    new AdaptiveSubmitAction
-                    {
-                        Title = "Hide Action card",
-                    },
-
-                    new AdaptiveSubmitAction
-                    {
-                        Title = "Show Task Module",
-                        Data = new AdaptiveCardAction
+                        Type = "continue",
+                        Value = new TabResponseCards
                         {
-                            MsteamsCardAction = new CardAction
+                            Cards = new List<TabResponseCard>
                             {
-                                Type = "task/fetch",
+                                new TabResponseCard
+                                {
+                                    Card = CardHelper.GetTaskModuleSubmitCard()
+                                },
                             },
                         },
                     },
-                },
-            };
+                });
+            }
 
-            return card;
-        }
-
-        /// <summary>
-        /// Sample Adaptive card
-        /// </summary>
-        /// <returns></returns>
-        private AdaptiveCard GetAdaptiveCard2()
-        {
-            AdaptiveCard card = new AdaptiveCard(new AdaptiveSchemaVersion("1.2"))
-            {
-                Body = new List<AdaptiveElement>
-                {
-                    new AdaptiveImage
-                    {
-                        Url = new Uri("https://cdn.vox-cdn.com/thumbor/Ndb49Uk3hjiquS041NDD0tPDPAs=/0x169:1423x914/fit-in/1200x630/cdn.vox-cdn.com/uploads/chorus_asset/file/7342855/microsoftteams.0.jpg"),
-                        AltText = "AlternativeText",
-                        PixelHeight = 300,
-                        PixelWidth = 400,
-                        Spacing = AdaptiveSpacing.Medium,
-                    },
-                    new AdaptiveTextBlock
-                    {
-                        Text = "tab/fetch is the first invoke request that your bot receives when a user opens an Adaptive Card tab. When your bot receives the request, it either sends a tab continue response or a tab auth response",
-                        Wrap = true,
-                        Spacing = AdaptiveSpacing.Medium,
-                    },
-                    new AdaptiveTextBlock
-                    {
-                        Text = "tab/submit request is triggered to your bot with the corresponding data through the Action.Submit function of Adaptive Card",
-                        Spacing = AdaptiveSpacing.Medium,
-                        Wrap = true,
-                    },
-                },
-
-                Actions = new List<AdaptiveAction>
-                {
-                    new AdaptiveSubmitAction
-                    {
-                        Title = "Click to submit",
-                    },
-                },
-            };
-
-            return card;
-        }
-
-        /// <summary>
-        /// Sample Adaptive card for Task module
-        /// </summary>
-        /// <returns></returns>
-        private Attachment GetAdaptiveCardForTaskModule()
-        {
-            AdaptiveCard card = new AdaptiveCard(new AdaptiveSchemaVersion("1.2"))
-            {
-                Body = new List<AdaptiveElement>
-                {
-                    new AdaptiveTextBlock
-                    {
-                        Text = "Hello:",
-                        Weight = AdaptiveTextWeight.Bolder,
-                        Spacing = AdaptiveSpacing.Medium,
-                    },
-                    new AdaptiveImage
-                    {
-                        Url = new Uri("https://cdn.vox-cdn.com/thumbor/Ndb49Uk3hjiquS041NDD0tPDPAs=/0x169:1423x914/fit-in/1200x630/cdn.vox-cdn.com/uploads/chorus_asset/file/7342855/microsoftteams.0.jpg"),
-                        AltText = "AlternativeText",
-                        PixelHeight = 50,
-                        PixelWidth = 50,
-                        Spacing = AdaptiveSpacing.Medium,
-                    },
-                },
-            };
-
-            return new Attachment()
-            {
-                ContentType = AdaptiveCard.ContentType,
-                Content = card,
-            };
+            return null;
         }
 
         private async Task<string> GetSignInLinkAsync(ITurnContext turnContext, CancellationToken cancellationToken)
