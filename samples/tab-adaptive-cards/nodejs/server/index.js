ac407a84e997938143fc22b6e05bb90f18372a0c
AbhijeetThakur-msft <v-abt@microsoft.com>
2021-07-26 01:10:36 +0530

    adding code to serve static file

--- samples/tab-adaptive-cards/nodejs/server/index.js
+++ samples/tab-adaptive-cards/nodejs/server/index.js
@@ -7,17 +7,17 @@ require('dotenv').config({ path: ENV_FILE });
 const PORT = process.env.PORT || 3978;
 const server = express();
 
+server.use("/Images", express.static(path.resolve(__dirname, '../Images')));
+
 server.use(cors());
 server.use(express.json());
 server.use(express.urlencoded({
     extended: true
 }));
 server.use('/api', require('./api'));
-
 server.get('*', (req, res) => {
     res.json({ error: 'Route not found' });
 });
-
 server.listen(PORT, () => {
     console.log(`Server listening on http://localhost:${PORT}`);
 });
