c55cc078f06f87dc3fcdc778df2ab6e828b8ebaf
NikitaJain-MSFT <v-nikija@microsoft.com>
2021-07-26 20:44:12 +0530

    graph-activity-feed nodejs setup

---
+++ samples/graph-activity-feed/nodejs/server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+const bodyparser = require('body-parser');
+const env = require('dotenv')
+const path = require('path');
+const auth = require('./auth');
+
+const app = express();
+
+app.use(express.static(__dirname + '/Styles'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+app.set('views', __dirname);
+
+const ENV_FILE = path.join(__dirname, '.env');
+require('dotenv').config({ path: ENV_FILE });
+
+app.get('/configure', function (req, res) {
+  res.render('./views/configure');
+});
+
+app.get('/GroupChatNotification', function (req, res) {
+  var tenantId = req.url.split('=')[1];
+  auth.getAccessToken(tenantId).then(async function (token) {
+    console.log("token from js file : " + token);
+    res.render('./views/GroupChatNotification', { token: JSON.stringify(token) });
+  });
+
+});
+
+app.get('/TeamNotification', function (req, res) {
+  var tenantId = req.url.split('=')[1];
+  console.log(tenantId);
+  auth.getAccessToken(tenantId).then(async function (token) {
+    console.log("token from js file : " + token);
+    res.render('./views/TeamNotification', { token: JSON.stringify(token) });
+  });
+
+});
+app.listen(3333, function () {
+  console.log('app listening on port 3333!');
+});
