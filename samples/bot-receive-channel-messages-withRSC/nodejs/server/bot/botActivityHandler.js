a7bbf92e51fedc27318c0c4f3fd94c810a93bf9f
AbhijeetThakur-msft <v-abt@microsoft.com>
2021-08-14 00:58:46 +0530

    adding method comments

--- samples/bot-receive-channel-messages-withRSC/nodejs/server/bot/botActivityHandler.js
+++ samples/bot-receive-channel-messages-withRSC/nodejs/server/bot/botActivityHandler.js
@@ -11,7 +11,8 @@ class BotActivityHandler extends TeamsActivityHandler  {
         var options = "Press 1 to know about the permissions required,  Press 2 for documentation link"
         var permissionRequired = "This capability is enabled by specifying the ChannelMessage.Read.Group permission in the manifest of an RSC enabled Teams app";
         var docLink = "https://docs.microsoft.com/en-us/microsoftteams/platform/bots/how-to/conversations/channel-messages-with-rsc";
-        
+
+        // Activity called when there's a message in channel
         this.onMessage(async (context, next) => {
             var replyText = context.activity.text;
             if(replyText == 1)
@@ -28,6 +29,7 @@ class BotActivityHandler extends TeamsActivityHandler  {
             await next();
         });
 
+        // Called when the bot is added to a team.
         this.onMembersAdded(async (context, next) => {
           var welcomeText = "Hello and welcome! With this sample your bot can receive user messages across standard channels in a team without being @mentioned";
           await context.sendActivity(MessageFactory.text(welcomeText));
