7651abb6210ca43e67a73ba5e0bff10801ff7fef
Kalyan chimmiri-MSFT <59079664+v-kalc@users.noreply.github.com>
2021-08-17 14:48:26 +0530

    To show tab in stage View- C# with Razor (#128)
    * Added stage view for cards
    
    * Added appsettings
    
    * Updated stage view code and added deep linking
    
    * Removed React for front end and placed razor instead and finished the flow with static pages
    
    * Update README.md
    
    * Update README.md
    
    * Resolved comments
    
    * Update README.md
    
    * Changes for deep link in bot and tab
    
    * adding images
    
    * updating the readme
    
    * updating images
    
    * Update README.md
    
    Correcting image names
    
    * updating readme file path and manifest version info
    
    Co-authored-by: AbhijeetThakur-msft <v-abt@microsoft.com>
    Co-authored-by: AbhijeetThakur-msft <86800431+AbhijeetThakur-msft@users.noreply.github.com>

---
+++ samples/tab-stage-view/csharp/TabInStageView/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿namespace TabInStageView.Controllers
+{
+    using System;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Configuration;
+
+    public class HomeController : Controller
+    {
+        private readonly string _appId;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HomeController"/> class.
+        /// </summary>
+        /// <param name="configuration">configuration of application.</param>
+        public HomeController(IConfiguration configuration)
+        {
+            _appId = configuration["MicrosoftAppId"] ?? throw new NullReferenceException("MicrosoftAppId");
+        }
+
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("sampleTab")]
+        public ActionResult SampleTab()
+        {
+            // Setting client id to be used in deep link in tab
+            ViewBag.AppId = _appId;
+            return View();
+        }
+    }
+}
