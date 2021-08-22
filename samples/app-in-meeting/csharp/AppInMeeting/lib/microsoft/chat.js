62c328129b740f2b244348484caa402ad2a96d14
AbhijeetThakur-msft <v-abt@microsoft.com>
2021-08-22 16:45:16 +0530

    Adding style changes, task module to capture user's details and updating readme.

--- samples/app-in-meeting/csharp/AppInMeeting/lib/microsoft/chat.js
+++ samples/app-in-meeting/csharp/AppInMeeting/lib/microsoft/chat.js
@@ -1,79 +1,20 @@
-﻿const connection = new signalR.HubConnectionBuilder()
+﻿// Creating a SignalR connection for real time updates.
+const connection = new signalR.HubConnectionBuilder()
     .withUrl("/chatHub")
     .build();
 
-//This method receive the message and Append to our list  
+// This method receives the messages and appends it to containers.  
 connection.on("ReceiveMessage", (user, description, status) => {
     var detailsClass = window.innerWidth > 600 ? "details" : "details-sidepanel";
     document.getElementById(status).innerHTML += (`<div class=${detailsClass}>
-                Description: ${description}</br>
-                Assigned To:${user}</div>
+                <div class="description" title="${description}">${description}</div>
+                <div class="userName">--${user}</div>
           `);
 });
 
 connection.start().catch(err => console.error(err.toString()));
 
-const updateDetails = (data) => {
-    alert(data.userName)
-    connection.invoke("SendMessage", data.userName, data.taskDescription, data.taskStatus).catch(err => console.log(err.toString()));
-}
-
-const openTaskModule1 = () => {
-    let taskInfo = {
-        title: "Custom Form",
-        height: 510,
-        width: 430,
-        url: "https://8489-122-175-234-83.ngrok.io/Index",
-    };
-  
-    microsoftTeams.tasks.startTask(taskInfo, submitHandler);
-    //microsoftTeams.tasks.startTask(taskInfo, updateDetails);
-};
-
-submitHandler1 = (err, result) => {
-    console.log(`Submit handler - err: ${err}`);
-    console.log(`Submit handler - result\rName: ${result.name}\rEmail: ${result.email}\rFavorite book: ${result.favoriteBook}`);
-};
-
-// Send task details.
-const sendTaskDetails1 = () => {
-    
-    microsoftTeams.initialize();
-    var taskStatus = document.querySelector(".task-status");
-    var taskDescription = document.querySelector(".task-description")
-    var userName = document.querySelector(".user-name")
-    var isValid = true;
-    $('.task-status,.task-description,.user-name').each(function (e) {
-        if ($.trim($(this).val()) == '') {
-            isValid = false;
-            $(this).css({
-                "border": "1px solid red"
-            });
-        }
-        else {
-            $(this).css({
-                "border": "",
-                "background": ""
-            });
-        }
-    });
-    if (isValid == false) {
-        e.preventDefault();
-        return false;
-    }
-    const taskDetails = {
-        "taskStatus": taskStatus.value,
-        "taskDescription": taskDescription.value,
-        "userName": userName.value
-    };
-    var customerInfo = {
-        name: "name",
-        email: "email",
-        favoriteBook: "favouriteBook"
-    }
-   // alert("sendDetails end");
-   // microsoftTeams.tasks.submitTask(customerInfo, "1234");
-    //microsoftTeams.tasks.submitTask(taskDetails);
-    updateDetails(taskDetails);
-    return true;
+// Method to send message to update entered details.
+const updateDetails = (data, status) => {
+    connection.invoke("SendMessage", data.userName, data.taskDescription, status).catch(err => console.log(err.toString()));
 }
---
+++ samples/app-in-meeting/csharp/README.md
@@ -0,0 +1,96 @@
+﻿﻿This App helps to enable and configure your apps for Teams meetings. This app covers Shared meeting stage.
+For reference please check [Enable and configure your apps for Teams meetings](https://docs.microsoft.com/en-us/microsoftteams/platform/apps-in-teams-meetings/enable-and-configure-your-app-for-teams-meetings)
+
+This capability is currently available in developer preview only.
+
+## Prerequisites
+
+- [.NET Core SDK](https://dotnet.microsoft.com/download) version 3.1
+
+  ```bash
+  # determine dotnet version
+  dotnet --version
+  ```
+- Publicly addressable https url or tunnel such as [ngrok](https://ngrok.com/) or [Tunnel Relay](https://github.com/OfficeDev/microsoft-teams-tunnelrelay) 
+
+## Setup
+
+1. Run ngrok - point to port 3978
+
+```bash
+# ngrok http -host-header=rewrite 3978
+```
+
+2. Modify the `manifest.json` in the `/AppPackage` folder and replace the following details
+   - `<<MANIFEST-ID>>` with some unique GUID   
+   - `<<BASE-URL>>` with your application's base url, e.g. https://1234.ngrok.io
+   - `<<VALID DOMAIN>>` with your app domain e.g. *.ngrok.io
+
+3. Zip the contents of `AppPackage` folder into a `manifest.zip`, and use the `manifest.zip` to deploy in app store or add to Teams as in step 4.
+
+4. Upload the manifest.zip to Teams (in the Apps view click "Upload a custom app")
+   - Go to Microsoft Teams. From the lower left corner, select Apps
+   - From the lower left corner, choose Upload a custom App
+   - Go to your project directory, the ./appPackage folder, select the zip folder, and choose Open.
+   - Select Add in the pop-up dialog box. Your tab is uploaded to Teams.
+    
+## To try this sample
+
+- In a terminal, navigate to `AppInMeeting`
+
+    ```bash
+    # change into project folder
+    cd # AppInMeeting
+    ```
+
+- Run the app from a terminal or from Visual Studio, choose option A or B.
+
+  A) From a terminal
+
+  ```bash
+  # run the app
+  dotnet run
+  ```
+
+  B) Or from Visual Studio
+
+  - Launch Visual Studio
+  - File -> Open -> Project/Solution
+  - Navigate to `AppInMeeting` folder
+  - Select `AppInMeeting.csproj` file
+  - Press `F5` to run the project
+
+## Interacting with the app in Teams
+    You can use this app by following the below steps:
+    - Edit a meeting and select `+` icon at the top right corner.
+
+![Add icon in meeting](AppInMeeting/Images/add_icon.png)
+
+    - Search for your app `App in meeting` and add it.
+
+![Select App](AppInMeeting/Images/select_app.png)
+
+    - Join the meeting and click on the app icon at the top, this will open a sidepanel with `Share` icon at top to share the app for collaboration.
+
+![App icon](AppInMeeting/Images/app_icon.png)
+
+![Share Icon](AppInMeeting/Images/share_icon.png)
+
+    - You can now interact with the app.
+
+
+## Features of this sample
+
+- App in stage view.
+
+![Stage View Screen](AppInMeeting/Images/stage_view.png)
+
+- Add Details for collaboration.
+
+![Add Button](AppInMeeting/Images/add_button.png)
+
+![Add Details](AppInMeeting/Images/add_details.png)
+
+- App in sidepanel.
+
+![App in sidepanel](AppInMeeting/Images/side_panel.png)
