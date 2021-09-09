ae33d37778c354c0cb2fd1ea5803cc0038e833ff
AbhijeetThakur-msft <v-abt@microsoft.com>
2021-09-10 01:48:09 +0530

    react components setup

--- samples/meeting-app/MeetingApp/MeetingApp/ClientApp/src/App.js
+++ samples/meeting-app/MeetingApp/MeetingApp/ClientApp/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch
+} from 'react-router-dom';
+import Configuration from './components/configuration';
+import RecruitingDetails from './components/recruiting-details/recruiting-details';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path='/configure' component={Configuration}></Route>
+        <Route exact path='/details' component={RecruitingDetails}></Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ samples/meeting-app/MeetingApp/MeetingApp/ClientApp/src/components/recruiting-details/recruiting-details.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as microsoftTeams from "@microsoft/teams-js";
+
+const RecruitingDetails = () => {
+    return (
+        <div className="config-container">
+            Candidate details
+        </div>
+    )
+}
+
+export default (RecruitingDetails);
