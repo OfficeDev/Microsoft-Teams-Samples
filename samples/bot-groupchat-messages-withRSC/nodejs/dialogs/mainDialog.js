137a76c189fc8a04898718dba284d0c7d3ae6678
NikitaJain-MSFT <v-nikija@microsoft.com>
2021-09-27 23:58:09 +0530

    initial setup

---
+++ samples/bot-groupchat-messages-withRSC/nodejs/dialogs/mainDialog.js
@@ -0,0 +1,73 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+const { ConfirmPrompt, DialogSet, DialogTurnStatus, WaterfallDialog } = require('botbuilder-dialogs');
+const { LogoutDialog } = require('./logoutDialog');
+
+const CONFIRM_PROMPT = 'ConfirmPrompt';
+const MAIN_DIALOG = 'MainDialog';
+const MAIN_WATERFALL_DIALOG = 'MainWaterfallDialog';
+const OAUTH_PROMPT = 'OAuthPrompt';
+const { SsoOAuthPrompt } = require('./ssoOAuthPrompt');
+const { SimpleGraphClient } = require('../simpleGraphClient');
+const { polyfills } = require('isomorphic-fetch');
+const { CardFactory, MessageFactory } = require('botbuilder-core');
+
+class MainDialog extends LogoutDialog {
+    constructor() {
+        super(MAIN_DIALOG, process.env.connectionName);
+
+        this.addDialog(new SsoOAuthPrompt(OAUTH_PROMPT, {
+            connectionName: process.env.connectionName,
+            text: 'Please Sign In',
+            title: 'Sign In',
+            timeout: 300000
+        }));
+        this.addDialog(new ConfirmPrompt(CONFIRM_PROMPT));
+        this.addDialog(new WaterfallDialog(MAIN_WATERFALL_DIALOG, [
+            this.promptStep.bind(this),
+            this.loginStep.bind(this)
+        ]));
+
+        this.initialDialogId = MAIN_WATERFALL_DIALOG;
+    }
+
+    /**
+     * The run method handles the incoming activity (in the form of a DialogContext) and passes it through the dialog system.
+     * If no dialog is active, it will start the default dialog.
+     * @param {*} dialogContext
+     */
+    async run(context, accessor) {
+        const dialogSet = new DialogSet(accessor);
+        dialogSet.add(this);
+        const dialogContext = await dialogSet.createContext(context);
+        const results = await dialogContext.continueDialog();
+        if (results.status === DialogTurnStatus.empty) {
+            await dialogContext.beginDialog(this.id);
+        }
+    }
+
+    async promptStep(stepContext) {
+        try {
+            return await stepContext.beginDialog(OAUTH_PROMPT);
+        } catch (err) {
+            console.error(err);
+        }
+    }
+
+    async loginStep(stepContext) {
+        // Get the token from the previous step. Note that we could also have gotten the
+        // token directly from the prompt itself. There is an example of this in the next method.
+        const tokenResponse = stepContext.result;
+        if (!tokenResponse || !tokenResponse.token) {
+            await stepContext.context.sendActivity('Login was not successful please try again.');
+        } else {
+            const client = new SimpleGraphClient(tokenResponse.token);
+            const me = await client.getMessages(stepContext.context._activity.conversation.id);
+            console.log(me);
+        }
+        return await stepContext.endDialog();
+    }
+}
+
+module.exports.MainDialog = MainDialog;
