09199b30d78aadd06310c6fda9d47323f37669b9
NikitaJain-MSFT <v-nikija@microsoft.com>
2021-09-27 19:12:34 +0530

    added graph client code

--- samples/bot-groupchat-messages-withRSC/csharp/FetchGroupChatMessagesWithRSC/Bots/ActivityBot.cs
+++ samples/bot-groupchat-messages-withRSC/csharp/FetchGroupChatMessagesWithRSC/Bots/ActivityBot.cs
@@ -1,10 +1,22 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT License.
 
+using FetchGroupChatMessagesWithRSC.helper;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Builder.Teams;
+using Microsoft.Bot.Connector;
+using Microsoft.Bot.Connector.Authentication;
 using Microsoft.Bot.Schema;
+using Microsoft.Bot.Schema.Teams;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -12,25 +24,17 @@ namespace FetchGroupChatMessagesWithRSC.Bots
 {
     public class ActivityBot : TeamsActivityHandler
     {
-        /// <summary>
-        /// Sample description text.
-        /// </summary>
-        private const string SampleDescription = "With this sample your bot can receive user messages across standard channels in a team without being @mentioned";
+        public readonly IConfiguration _configuration;
+        private readonly GetChatHelper _helper = new();
+        private readonly IWebHostEnvironment _env;
+        private readonly IHttpClientFactory _clientFactory;
 
-        /// <summary>
-        /// Options text.
-        /// </summary>
-        private const string Option = "Type 1 to know about the permissions required,  Type 2 for documentation link";
-
-        /// <summary>
-        /// Permission required text.
-        /// </summary>
-        private const string PermissionRequired = "This capability is enabled by specifying the ChannelMessage.Read.Group permission in the manifest of an RSC enabled Teams app";
-
-        /// <summary>
-        /// Docs link.
-        /// </summary>
-        private const string DocLink = "https://docs.microsoft.com/en-us/microsoftteams/platform/bots/how-to/conversations/channel-messages-with-rsc";
+        public ActivityBot(IConfiguration configuration, IWebHostEnvironment env, IHttpClientFactory clientFactory)
+        {
+            _clientFactory = clientFactory;
+            _configuration = configuration;
+            _env = env;
+        }
 
         /// <summary>
         /// Handle when a message is addressed to the bot.
@@ -44,21 +48,67 @@ namespace FetchGroupChatMessagesWithRSC.Bots
         /// </remarks>
         protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
         {
-            var userCommand = turnContext.Activity.Text;
-            switch (userCommand)
+            var userTokenClient = turnContext.TurnState.Get<UserTokenClient>();
+            var activity = this.StripAtMentionText((Activity)turnContext.Activity);
+            var userCommand = activity.Text;
+            if(userCommand!= "getchat")
             {
-                case "1":
-                    await turnContext.SendActivityAsync(MessageFactory.Text(PermissionRequired));
-                    return;
+                this.createChatFile(activity);
+            }
+            else
+            {
+                //var resource = await userTokenClient.GetSignInResourceAsync(_configuration["ConnectionName"], turnContext.Activity as Activity, null, cancellationToken);
+                // Retrieve the OAuth Sign in Link to use in the MessagingExtensionResult Suggested Actions
+                //var signInLink = resource.SignInLink;
+                var attachment = new Attachment
+                {
+                    Content = new OAuthCard
+                    {
+                        ConnectionName = _configuration["ConnectionName"],
+                        Text = "Please login",
+                        
+                    },
+                    ContentType = OAuthCard.ContentType,
+                };
+                var activiti = MessageFactory.Attachment(attachment);
 
-                case "2":
-                    await turnContext.SendActivityAsync(MessageFactory.Text(DocLink));
-                    return;
+                // NOTE: This activity needs to be sent in the 1:1 conversation between the bot and the user. 
+                // If the bot supports group and channel scope, this code should be updated to send the request to the 1:1 chat. 
 
-                default:
-                    await turnContext.SendActivityAsync(MessageFactory.Text(SampleDescription));
-                    await turnContext.SendActivityAsync(MessageFactory.Text(Option));
-                    return;
+                await turnContext.SendActivityAsync(activiti, cancellationToken);
+                //var chat = await _helper.GetGroupChatMessage(turnContext.Activity.Conversation.TenantId, _configuration["MicrosoftAppId"], _configuration["MicrosoftAppPassword"], turnContext.Activity.Conversation.Id);
+
+                string filename = "chat.txt";
+                string filePath = Path.Combine(_env.ContentRootPath, $".\\public\\chat.txt");
+                long fileSize = new FileInfo(filePath).Length;
+                await SendFileCardAsync(turnContext, filename, fileSize, cancellationToken); 
+                await turnContext.SendActivityAsync(MessageFactory.Text("hello"));
+            }
+            return;
+        }
+
+        protected override async Task<InvokeResponse> OnInvokeActivityAsync
+    (ITurnContext<IInvokeActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var userTokenClient = turnContext.TurnState.Get<UserTokenClient>();
+            // Check the state value
+            var state = turnContext.Activity.Value.ToString();
+            var tokenResponse = await GetTokenResponse(turnContext, state, cancellationToken);
+            try
+            {
+                if (turnContext.Activity.Name == SignInConstants.TokenExchangeOperationName && turnContext.Activity.ChannelId == Channels.Msteams)
+                {
+                    await OnTokenResponseEventAsync((ITurnContext<IEventActivity>)turnContext, cancellationToken);
+                    return new InvokeResponse() { Status = 200 };
+                }
+                else
+                {
+                    return await base.OnInvokeActivityAsync(turnContext, cancellationToken);
+                }
+            }
+            catch (InvokeResponseException e)
+            {
+                return e.CreateInvokeResponse();
             }
         }
 
@@ -79,6 +129,163 @@ namespace FetchGroupChatMessagesWithRSC.Bots
                     await turnContext.SendActivityAsync(MessageFactory.Text(welcomeText, welcomeText), cancellationToken);
                 }
             }
+        }
+
+        private async Task SendFileCardAsync(ITurnContext turnContext, string filename, long filesize, CancellationToken cancellationToken)
+        {
+            var consentContext = new Dictionary<string, string>
+            {
+                { "filename", filename },
+            };
+
+            var fileCard = new FileConsentCard
+            {
+                Description = "This is the file I want to send you",
+                SizeInBytes = filesize,
+                AcceptContext = consentContext,
+                DeclineContext = consentContext,
+            };
+
+            var asAttachment = new Attachment
+            {
+                Content = fileCard,
+                ContentType = FileConsentCard.ContentType,
+                Name = filename,
+            };
+
+            var replyActivity = turnContext.Activity.CreateReply();
+            replyActivity.Attachments = new List<Attachment>() { asAttachment };
+            await turnContext.SendActivityAsync(replyActivity, cancellationToken);
+        }
+
+        protected override async Task OnTeamsFileConsentAcceptAsync(ITurnContext<IInvokeActivity> turnContext, FileConsentCardResponse fileConsentCardResponse, CancellationToken cancellationToken)
+        {
+            try
+            {
+                JToken context = JObject.FromObject(fileConsentCardResponse.Context);
+
+                string filePath = Path.Combine(_env.ContentRootPath, $".\\public\\chat.txt");
+                long fileSize = new FileInfo(filePath).Length;
+                var client = _clientFactory.CreateClient();
+                using (var fileStream = File.OpenRead(filePath))
+                {
+                    var fileContent = new StreamContent(fileStream);
+                    fileContent.Headers.ContentLength = fileSize;
+                    fileContent.Headers.ContentRange = new ContentRangeHeaderValue(0, fileSize - 1, fileSize);
+                    await client.PutAsync(fileConsentCardResponse.UploadInfo.UploadUrl, fileContent, cancellationToken);
+                }
+
+                await FileUploadCompletedAsync(turnContext, fileConsentCardResponse, cancellationToken);
+            }
+            catch (Exception e)
+            {
+                await FileUploadFailedAsync(turnContext, e.ToString(), cancellationToken);
+            }
+        }
+
+        protected override async Task OnTeamsFileConsentDeclineAsync(ITurnContext<IInvokeActivity> turnContext, FileConsentCardResponse fileConsentCardResponse, CancellationToken cancellationToken)
+        {
+            JToken context = JObject.FromObject(fileConsentCardResponse.Context);
+
+            var reply = MessageFactory.Text($"Declined. We won't upload file <b>{context["filename"]}</b>.");
+            reply.TextFormat = "xml";
+            await turnContext.SendActivityAsync(reply, cancellationToken);
+        }
+
+        private async Task FileUploadCompletedAsync(ITurnContext turnContext, FileConsentCardResponse fileConsentCardResponse, CancellationToken cancellationToken)
+        {
+            var downloadCard = new FileInfoCard
+            {
+                UniqueId = fileConsentCardResponse.UploadInfo.UniqueId,
+                FileType = fileConsentCardResponse.UploadInfo.FileType,
+            };
+
+            var asAttachment = new Attachment
+            {
+                Content = downloadCard,
+                ContentType = FileInfoCard.ContentType,
+                Name = fileConsentCardResponse.UploadInfo.Name,
+                ContentUrl = fileConsentCardResponse.UploadInfo.ContentUrl,
+            };
+
+            var reply = MessageFactory.Text($"<b>File uploaded.</b> Your file <b>{fileConsentCardResponse.UploadInfo.Name}</b> is ready to download");
+            reply.TextFormat = "xml";
+            reply.Attachments = new List<Attachment> { asAttachment };
+
+            await turnContext.SendActivityAsync(reply, cancellationToken);
+        }
+
+        private async Task FileUploadFailedAsync(ITurnContext turnContext, string error, CancellationToken cancellationToken)
+        {
+            var reply = MessageFactory.Text($"<b>File upload failed.</b> Error: <pre>{error}</pre>");
+            reply.TextFormat = "xml";
+            await turnContext.SendActivityAsync(reply, cancellationToken);
+        }
+        private Activity StripAtMentionText(Activity activity)
+        {
+            if (activity == null)
+            {
+                throw new ArgumentNullException(nameof(activity));
+            }
+
+            foreach (var m in activity.GetMentions())
+            {
+                if (m.Mentioned.Id == activity.Recipient.Id)
+                {
+                    //Bot is in the @mention list.  
+                    //The below example will strip the bot name out of the message, so you can parse it as if it wasn't included.
+                    //Note that the Text object will contain the full bot name, if applicable.
+                    if (m.Text != null)
+                        activity.Text = activity.Text.Replace(m.Text, "").Trim();
+                }
+            }
+
+            return activity;
+        }
+
+        private void createChatFile(Activity activity)
+        {
+            var fileName = Path.Combine(_env.ContentRootPath, $".\\public\\chat.txt");
+            // string fileName = "C:\\Users\\v-nikija\\source\\repos\\Microsoft-Teams-Samples\\samples\\bot-groupchat-messages-withRSC\\csharp\\FetchGroupChatMessagesWithRSC\\public\\chat.txt";
+            FileInfo fi = new FileInfo(fileName);
+            try
+            {
+                // Check if file already exists. If yes, delete it.     
+                if (fi.Exists)
+                {
+                    using (StreamWriter sw = fi.AppendText())
+                    {
+                        sw.WriteLine("from: {0}", activity.From.Name);
+                        sw.WriteLine("text: {0}", activity.Text);
+                        sw.WriteLine("at: {0}", activity.LocalTimestamp);
+                    }
+                }
+            }
+            catch (Exception Ex)
+            {
+                Console.WriteLine(Ex.ToString());
+            }
+
+        }
+
+        /// <summary>
+        /// Get token response on basis of state.
+        /// </summary>
+        private async Task<TokenResponse> GetTokenResponse(ITurnContext<IInvokeActivity> turnContext, string state, CancellationToken cancellationToken)
+        {
+            var magicCode = string.Empty;
+
+            if (!string.IsNullOrEmpty(state))
+            {
+                if (int.TryParse(state, out var parsed))
+                {
+                    magicCode = parsed.ToString();
+                }
+            }
+
+            var userTokenClient = turnContext.TurnState.Get<UserTokenClient>();
+            var tokenResponse = await userTokenClient.GetUserTokenAsync(turnContext.Activity.From.Id, _configuration["ConnectionName"], turnContext.Activity.ChannelId, magicCode, cancellationToken).ConfigureAwait(false);
+            return tokenResponse;
         }
     }
 }
---
+++ samples/bot-groupchat-messages-withRSC/csharp/FetchGroupChatMessagesWithRSC/helper/GetChatHelper.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.Graph;
+using Microsoft.Identity.Client;
+using System;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace FetchGroupChatMessagesWithRSC.helper
+{
+    public class GetChatHelper
+    {
+        private async Task<GraphServiceClient> GetAuthenticatedClient(string tenantId, string MicrosoftAppId, string MicrosoftAppPassword)
+        {
+            var accessToken = await GetToken(tenantId, MicrosoftAppId, MicrosoftAppPassword);
+            var graphClient = new GraphServiceClient(
+                new DelegateAuthenticationProvider(
+                    requestMessage =>
+                    {
+                        // Append the access token to the request.
+                        requestMessage.Headers.Authorization = new AuthenticationHeaderValue("bearer", accessToken);
+
+                        // Get event times in the current time zone.
+                        requestMessage.Headers.Add("Prefer", "outlook.timezone=\"" + TimeZoneInfo.Local.Id + "\"");
+
+                        return Task.CompletedTask;
+                    }));
+
+            return graphClient;
+        }
+
+        public async Task<string> GetToken(string tenantId, string MicrosoftAppId, string MicrosoftAppPassword)
+        {
+            IConfidentialClientApplication app = ConfidentialClientApplicationBuilder.Create(MicrosoftAppId)
+                                                  .WithClientSecret(MicrosoftAppPassword)
+                                                  .WithAuthority($"https://login.microsoftonline.com/{tenantId}")
+                                                  .WithRedirectUri("https://daemon")
+                                                  .Build();
+
+            string[] scopes = new string[] { "https://graph.microsoft.com/.default" };
+
+            var result = await app.AcquireTokenForClient(scopes).ExecuteAsync();
+
+            return result.AccessToken;
+        }
+
+        public async Task<IChatMessagesCollectionPage> GetGroupChatMessage(string MicrosoftTenantId, string MicrosoftAppId, string MicrosoftAppPassword, string Chatid)
+        {
+            GraphServiceClient graphClient = await GetAuthenticatedClient(MicrosoftTenantId, MicrosoftAppId, MicrosoftAppPassword);
+
+            try
+            {
+                var messages = await graphClient.Chats[Chatid].Messages
+                        .Request()
+                        .GetAsync();
+                return messages;
+            }
+            catch (ServiceException ex)
+            {
+                // This is where app is already installed but we don't have conversation reference.
+                throw ex;
+            }
+        }
+    }
+}
