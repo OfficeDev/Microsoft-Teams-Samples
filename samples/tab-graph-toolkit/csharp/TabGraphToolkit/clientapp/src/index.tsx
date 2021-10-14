405a52fb037db445e318b7c78c5229c234dbc7d6
NikitaJain-MSFT <v-nikija@microsoft.com>
2021-10-14 17:21:41 +0530

    Adding UI changes

--- samples/tab-graph-toolkit/csharp/TabGraphToolkit/clientapp/src/index.tsx
+++ samples/tab-graph-toolkit/csharp/TabGraphToolkit/clientapp/src/index.tsx
@@ -6,25 +6,28 @@ import reportWebVitals from './reportWebVitals';
 import { Providers } from '@microsoft/mgt-element';
 import { TeamsMsal2Provider } from '@microsoft/mgt-teams-msal2-provider';
 import * as MicrosoftTeams from "@microsoft/teams-js";
-import { Route, BrowserRouter } from 'react-router-dom'
+import { Route, BrowserRouter } from 'react-router-dom';
+import { Provider, teamsTheme } from '@fluentui/react-northstar'
 import TabAuth from './components/TabAuth';
 
 TeamsMsal2Provider.microsoftTeamsLib = MicrosoftTeams;
 
 Providers.globalProvider = new TeamsMsal2Provider({
-    clientId: 'client_Id',
+    clientId: 'client_id',
   authPopupUrl: window.location.origin + '/tabauth',
   scopes: ['calendars.read', 'user.read', 'openid', 'profile', 'people.read', 'user.readbasic.all'],
 });
 
 ReactDOM.render(
   <React.StrictMode>
-    < BrowserRouter >
-      <div>
-        <Route exact path="/tab" component={App} />
-        <Route path="/tabauth" component={TabAuth} />
-      </div>
-    </ BrowserRouter >
+    <Provider theme={teamsTheme}>
+       <BrowserRouter >
+          <div>
+          <Route exact path="/tab" component={App} />
+          <Route path="/tabauth" component={TabAuth} />
+          </div>     
+       </BrowserRouter >
+    </Provider >
   </React.StrictMode>,
   document.getElementById('root')
 );
