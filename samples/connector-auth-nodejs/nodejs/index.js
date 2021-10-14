200c762696cee4a5396bc6d9d720eeb7f3c55e8d
Prithvi-MSFT <v-pritka@microsoft.com>
2021-10-14 18:14:54 +0530

    Changes for sending welcome card

--- samples/connector-auth-nodejs/nodejs/index.js
+++ samples/connector-auth-nodejs/nodejs/index.js
@@ -5,6 +5,7 @@ const ENV_FILE = path.join(__dirname, '.env');
 require('dotenv').config({ path: ENV_FILE });
 const PORT = process.env.PORT || 3978;
 const server = express();
+const axios = require('axios');
 
 server.use(cors());
 server.use(express.json());
@@ -35,4 +36,38 @@ server.get('/SimpleEnd', (req, res, next) => {
 
 server.get('*', (req, res) => {
     res.json({ error: 'Route not found' });
+});
+
+server.post('/Connector/Save', (req, res) => {
+    var responseMsg = JSON.stringify({
+        "type": "message",
+        "attachments": [
+            {
+                "contentType": "application/vnd.microsoft.card.adaptive",
+                "contentUrl": null,
+                "content": {
+                    "type": "AdaptiveCard",
+                    "version": "1.4",
+                    "body": [
+                        {
+                            "type": "TextBlock",
+                            "text": "Teams todo connector is setup we will notify you when new task is created [here](https://google.com)"
+                        }
+                    ]
+                },
+                "name": null,
+                "thumbnailUrl": null
+            }
+        ]
+    });
+
+    axios.post(req.body.webhookUrl, responseMsg)
+  .then(res => {
+    console.log(`statusCode: ${res.status}`)
+    console.log(res)
+  })
+  .catch(error => {
+    console.error(error)
+  })
+
 });
