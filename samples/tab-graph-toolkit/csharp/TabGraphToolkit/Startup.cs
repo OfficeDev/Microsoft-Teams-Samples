e7c9e435c138593f663cbe618612819185f64092
AbhijeetThakur-msft <v-abt@microsoft.com>
2021-10-17 15:58:28 +0530

    test commit to test workflow execution

--- samples/tab-graph-toolkit/csharp/TabGraphToolkit/Startup.cs
+++ samples/tab-graph-toolkit/csharp/TabGraphToolkit/Startup.cs
@@ -21,7 +21,7 @@ namespace TabGraphToolkit
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
