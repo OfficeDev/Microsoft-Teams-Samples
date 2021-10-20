e5de081451c6162537afb781419c037d2c4d2fb0
NikitaJain-MSFT <v-nikija@microsoft.com>
2021-10-20 15:31:54 +0530

    initial setup

---
+++ samples/bot-sharepoint-file-viewer/csharp/BotWithSharePointFileViewer/Dialogs/MainDialog.cs
@@ -0,0 +1,80 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using BotWithSharePointFileViewer.helper;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Schema;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.Graph;
+
+namespace BotWithSharePointFileViewer.Dialogs
+{
+    public class MainDialog : LogoutDialog
+    {
+        protected readonly ILogger _logger;
+        private readonly IWebHostEnvironment _env;
+        public readonly IConfiguration _configuration;
+
+        public MainDialog(IConfiguration configuration, ILogger<MainDialog> logger, IWebHostEnvironment env)
+            : base(nameof(MainDialog), configuration["ConnectionName"])
+        {
+            _logger = logger;
+            _env = env;
+            _configuration = configuration;
+
+            AddDialog(new OAuthPrompt(
+                nameof(OAuthPrompt),
+                new OAuthPromptSettings
+                {
+                    ConnectionName = ConnectionName,
+                    Text = "Please login",
+                    Title = "Login",
+                    Timeout = 300000, // User has 5 minutes to login
+                }));
+
+            AddDialog(new TextPrompt(nameof(TextPrompt)));
+
+            AddDialog(new WaterfallDialog(nameof(WaterfallDialog), new WaterfallStep[]
+            {
+                PromptStepAsync,
+                LoginStepAsync
+            }));
+
+            // The initial child Dialog to run.
+            InitialDialogId = nameof(WaterfallDialog);
+        }
+
+        private async Task<DialogTurnResult> PromptStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            return await stepContext.BeginDialogAsync(nameof(OAuthPrompt), null, cancellationToken);
+        }
+
+        private async Task<DialogTurnResult> LoginStepAsync(WaterfallStepContext stepContext, CancellationToken cancellationToken)
+        {
+            // Get the token from the previous step. Note that we could also have gotten the
+            // token directly from the prompt itself. There is an example of this in the next method.
+            var tokenResponse = (TokenResponse)stepContext.Result;
+
+            if (tokenResponse != null)
+            {
+                if (stepContext.Context.Activity.Conversation.ConversationType != "personal")
+                {
+                    GetSharePointFileHelper.GetSharePointFile(stepContext.Context, tokenResponse, stepContext.Context.Activity.Conversation.Id);
+
+                    return await stepContext.EndDialogAsync();
+                }
+
+                await stepContext.Context.SendActivityAsync(MessageFactory.Text("Login successful"), cancellationToken);
+                await stepContext.Context.SendActivityAsync(MessageFactory.Text("Please type 'getchat' command in the groupchat where the bot is added to fetch messages."), cancellationToken);
+            }
+
+            return await stepContext.EndDialogAsync();
+        }
+    }
+}
---
+++ samples/bot-sharepoint-file-viewer/csharp/BotWithSharePointFileViewer/SimpleGraphClient.cs
@@ -0,0 +1,87 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Microsoft.Graph;
+
+namespace BotWithSharePointFileViewer
+{
+    // This class is a wrapper for the Microsoft Graph API
+    // See: https://developer.microsoft.com/en-us/graph
+    public class SimpleGraphClient
+    {
+        private readonly string _token;
+
+        public SimpleGraphClient(string token)
+        {
+            if (string.IsNullOrWhiteSpace(token))
+            {
+                throw new ArgumentNullException(nameof(token));
+            }
+
+            _token = token;
+        }
+
+        // Get share point file.
+        public async Task<DriveItem> GetSharePointFile(string itemId)
+        {
+            var graphClient = GetAuthenticatedClient();
+            var fileInfo = await graphClient.Me.Drive.Items[itemId]
+                                .Request()
+                                .GetAsync();
+            return fileInfo;
+        }
+
+        // Send file to chat.
+        public async void SendFileToChat(string chatid, DriveItem driveItem)
+        {
+            var graphClient = GetAuthenticatedClient();
+            var weburl = driveItem.WebUrl.Substring(0, driveItem.WebUrl.IndexOf("&action"));
+
+            var chatMessage = new ChatMessage
+            {
+                Body = new ItemBody
+                {
+                    ContentType = BodyType.Html,
+                    Content = $"Here's the share point file. <attachment id=\"{driveItem.ETag.Substring(2,36)}\"></attachment>"
+                },
+                Attachments = new List<ChatMessageAttachment>()
+                {
+                    new ChatMessageAttachment
+                    {
+                        Id = driveItem.ETag.Substring(2,36),
+                        ContentType = "reference",
+                        ContentUrl = weburl,
+                        Name = driveItem.Name
+                    }
+                }
+            };
+
+            await graphClient.Chats[chatid].Messages
+                .Request()
+                .AddAsync(chatMessage);
+        }
+
+        // Get an Authenticated Microsoft Graph client using the token issued to the user.
+        private GraphServiceClient GetAuthenticatedClient()
+        {
+            var graphClient = new GraphServiceClient(
+                new DelegateAuthenticationProvider(
+                    requestMessage =>
+                    {
+                        // Append the access token to the request.
+                        requestMessage.Headers.Authorization = new AuthenticationHeaderValue("bearer", _token);
+
+                        // Get event times in the current time zone.
+                        requestMessage.Headers.Add("Prefer", "outlook.timezone=\"" + TimeZoneInfo.Local.Id + "\"");
+
+                        return Task.CompletedTask;
+                    }));
+
+            return graphClient;
+        }
+    }
+}
