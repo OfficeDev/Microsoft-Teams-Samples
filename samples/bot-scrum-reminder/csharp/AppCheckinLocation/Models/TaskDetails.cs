a5595cc97fefb0bb8ccd53f1107919a4995a02d7
NikitaJain-MSFT <v-nikija@microsoft.com>
2021-11-09 11:29:44 +0530

    adding task details

---
+++ samples/bot-scrum-reminder/csharp/AppCheckinLocation/Models/TaskDetails.cs
@@ -0,0 +1,23 @@
+﻿// <copyright file="LocationDetails.cs" company="Microsoft">
+// Copyright (c) Microsoft. All rights reserved.
+// </copyright>
+
+using Newtonsoft.Json;
+
+namespace AppCheckinLocation.Models
+{
+    /// <summary>
+    /// Location details model class.
+    /// </summary>
+    public class TaskDetails<T>
+    {
+        [JsonProperty("title")]
+        public object Title { get; set; }
+
+        [JsonProperty("description")]
+        public object Description { get; set; }
+
+        [JsonProperty("dateTime")]
+        public object DateTime { get; set; }
+    }
+}
