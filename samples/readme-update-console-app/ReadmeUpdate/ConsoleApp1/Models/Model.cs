d9705d179e3bcbb867b5fdb02b1311c6e5e85f2d
AbhijeetThakur-msft <v-abt@microsoft.com>
2021-11-12 19:01:23 +0530

    changes to get all samples from main readme

---
+++ samples/readme-update-console-app/ReadmeUpdate/ConsoleApp1/Models/Model.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1.Models
+{
+    public class DirectoryInfo
+    {
+        public List<FileInformation> FilesInfo { get; set; }
+    }
+
+    public class FileInformation
+    {
+        public String name;
+        public String type;
+        public String download_url;
+        public LinkFields _links;
+    }
+
+    public class LinkFields
+    {
+        public String self;
+    }
+
+    public class DirectoryInformation
+    {
+        public String name;
+        public List<DirectoryInformation> subDirs;
+        public List<FileData> files;
+        public List<Sample> samples;
+    }
+
+    public class Sample
+    {
+        public string SampleLinkKey { get; set; }
+
+        public string SampleFolder { get; set; }
+    }
+}
--- samples/readme-update-console-app/ReadmeUpdate/ConsoleApp1/Program.cs
+++ samples/readme-update-console-app/ReadmeUpdate/ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using ConsoleApp1;
+using ConsoleApp1.Models;
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
@@ -8,12 +9,6 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
-    public struct Directory
-    {
-        public String name;
-        public List<Directory> subDirs;
-        public List<FileData> files;
-    }
 
     //Structs used to hold file data
     public struct FileData
@@ -21,85 +16,136 @@ namespace ConsoleApp1
         public String name;
         public String contents;
     }
-
-    struct LinkFields
-    {
-        public String self;
-    }
-    struct FileInfo
-    {
-        public String name;
-        public String type;
-        public String download_url;
-        public LinkFields _links;
-    }
-   
+  
     class Program
     {
+        // Information about the respository
         static string owner = "OfficeDev";
         static string repoName = "Microsoft-Teams-Samples";
         static string access_token = "";
+        static string rootReadmeContent = "";
 
         //Get all files from a repo
-        public static async Task<Directory> getRepo()
+        public static async Task<DirectoryInformation> getRepo()
         {
             HttpClient client = new HttpClient();
-            Directory root = await readDirectory("root", client, String.Format("https://api.github.com/repos/{0}/{1}/contents/", owner, repoName), access_token);
+            DirectoryInformation root = await readRootDirectory("root", client, String.Format("https://api.github.com/repos/{0}/{1}/contents/", owner, repoName), access_token);
             client.Dispose();
             return root;
         }
 
-        //recursively get the contents of all files and subdirectories within a directory 
-        private static async Task<Directory> readDirectory(String name, HttpClient client, string uri, string access_token)
+        // Recursively get the contents of all files and subdirectories within a directory 
+        private static async Task<DirectoryInformation> readRootDirectory(String name, HttpClient client, string uri, string access_token)
         {
             //get the directory contents
             HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, uri);
-            //request.Headers.Add("Authorization",
-            //    "Basic " + Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(String.Format("{0}:{1}", access_token, "x-oauth-basic"))));
             request.Headers.Add("Authorization",
-               "token " + access_token);
+                "Basic " + Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(String.Format("{0}:{1}", access_token, "x-oauth-basic"))));
             request.Headers.Add("User-Agent", "lk-github-client");
 
             //parse result
             HttpResponseMessage response = await client.SendAsync(request);
             String jsonStr = await response.Content.ReadAsStringAsync(); ;
             response.Dispose();
-            FileInfo[] dirContents = JsonConvert.DeserializeObject<FileInfo[]>(jsonStr);
+
+            List<FileInformation> dirContents = JsonConvert.DeserializeObject<List<FileInformation>>(jsonStr);
+
+            // Getting only the root README file.
+            FileInformation rootReadme = dirContents.Find(x => x.name == "README.md");
+
+            // Getting the file contents
+            HttpRequestMessage downLoadUrl = new HttpRequestMessage(HttpMethod.Get, rootReadme.download_url);
+            downLoadUrl.Headers.Add("Authorization",
+                        "Basic " + Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(String.Format("{0}:{1}", access_token, "x-oauth-basic"))));
+            request.Headers.Add("User-Agent", "lk-github-client");
+
+            HttpResponseMessage contentResponse = await client.SendAsync(downLoadUrl);
+            rootReadmeContent = await contentResponse.Content.ReadAsStringAsync();
+            contentResponse.Dispose();
 
             //read in data
-            Directory result;
+            DirectoryInformation result = new DirectoryInformation();
             result.name = name;
-            result.subDirs = new List<Directory>();
+            result.subDirs = new List<DirectoryInformation>();
             result.files = new List<FileData>();
-            foreach (FileInfo file in dirContents)
+
+            FileData data;
+            data.name = rootReadme.name;
+            data.contents = rootReadmeContent;
+
+            result.files.Add(data);
+
+            List<string> currentSamples = new List<string>();
+            string line;
+            using (StreamReader file = new StreamReader(GenerateStreamFromString(rootReadmeContent)))
             {
-                if (file.type == "dir")
-                { //read in the subdirectory
-                    Directory sub = await readDirectory(file.name, client, file._links.self, access_token);
-                    result.subDirs.Add(sub);
-                }
-                else
-                { //get the file contents;
-                    HttpRequestMessage downLoadUrl = new HttpRequestMessage(HttpMethod.Get, file.download_url);
-                    downLoadUrl.Headers.Add("Authorization",
-                        "Basic " + Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(String.Format("{0}:{1}", access_token, "x-oauth-basic"))));
-                    request.Headers.Add("User-Agent", "lk-github-client");
-
-                    HttpResponseMessage contentResponse = await client.SendAsync(downLoadUrl);
-                    String content = await contentResponse.Content.ReadAsStringAsync();
-                    contentResponse.Dispose();
-
-                    FileData data;
-                    data.name = file.name;
-                    data.contents = content;
-
-                    result.files.Add(data);
+                while ((line = file.ReadLine()) != null)
+                {
+                    if (line.Contains(":samples/"))
+                    {
+                        currentSamples.Add(line);
+                    }
                 }
             }
-            return result;
+
+            // Iterating through all samples in README file.
+            foreach (string sampleString in currentSamples)
+            {
+                var splitSampleString = sampleString.Split(":");
+                var sampleKey = string.Join("", splitSampleString[0].Split('[', ']')).Trim();
+                var samplePath = splitSampleString[1];
+                //HttpClient tempClient = new HttpClient();
+                //DirectoryInformation sub = await readRootDirectory(samplePath, tempClient, file._links.self, access_token);
+            }
+
+                //foreach (FileInfo file in dirContents)
+                //{
+                //    if (file.type == "dir")
+                //    { //read in the subdirectory
+                //        Directory sub = await readRootDirectory(file.name, client, file._links.self, access_token);
+                //        result.subDirs.Add(sub);
+                //    }
+                //    else
+                //    {
+                //        //get the file contents;
+                //        HttpRequestMessage downLoadUrl = new HttpRequestMessage(HttpMethod.Get, file.download_url);
+                //        downLoadUrl.Headers.Add("Authorization",
+                //            "Basic " + Convert.ToBase64String(System.Text.Encoding.UTF8.GetBytes(String.Format("{0}:{1}", access_token, "x-oauth-basic"))));
+                //        request.Headers.Add("User-Agent", "lk-github-client");
+
+                //        HttpResponseMessage contentResponse = await client.SendAsync(downLoadUrl);
+                //        String content = await contentResponse.Content.ReadAsStringAsync();
+                //        contentResponse.Dispose();
+
+                //        FileData data;
+                //        data.name = file.name;
+                //        data.contents = content;
+
+                //        result.files.Add(data);
+                //    }
+                //}
+              return result;
+        }
+
+        // Method to generate Stream from string to search for a line.
+        public static Stream GenerateStreamFromString(string s)
+        {
+            var stream = new MemoryStream();
+            var writer = new StreamWriter(stream);
+            writer.Write(s);
+            writer.Flush();
+            stream.Position = 0;
+            return stream;
+        }
+
+        // Method to append information to readme file.
+        public static void UpdateReadme()
+        {
+
         }
     }
 }
+
 public class MainClass
 {
     public static void Main()
