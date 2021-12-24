4d61371bced036496e603201b1975bb3437487f4
AbhijeetThakur-msft <v-abt@microsoft.com>
2021-12-24 15:47:55 +0530

    initial changes

---
+++ samples/graph-activity-feed-broadcast/csharp/ActivityFeedNotification/Repository/FeedRepository.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using TabActivityFeed.Model;
+
+namespace TabActivityFeed.Repository
+{
+    public class FeedRepository
+    {
+        public static List<TaskInfo> Tasks { get; set; } = new List<TaskInfo>();
+        static FeedRepository()
+        {
+            Tasks.Add(new TaskInfo
+            {
+                title = "Get the bills",
+                DeployementTitle = "Get the bills",
+                description = "Get the travel and accommodation bills",
+                DeploymentDescription = "Get the travel and accommodation bills"
+            });
+
+            Tasks.Add(new TaskInfo
+            {
+                title = "Get the pay",
+                DeployementTitle = "Get the pay",
+                description = "Get the travel and accommodation slips",
+                DeploymentDescription = "Get the travel and accommodation slips"
+            });
+        }
+    }
+}
