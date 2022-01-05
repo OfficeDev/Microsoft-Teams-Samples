47b0320410d57fee203e0f0b91fe70527791ad9b
NikitaJain-MSFT <87964525+NikitaJain-MSFT@users.noreply.github.com>
2022-01-05 21:28:01 +0530

    Tab sso sample Obo flow update (#206)
    * updating graph call flow
    
    * Updating get userprofile for multi tenant
    
    * adding msal onbehalfof flow.
    
    * divided code into client and server folders
    
    * solving PR comments.
    
    Co-authored-by: AbhijeetThakur-msft <v-abt@microsoft.com>

--- samples/tab-sso/nodejs/src/client/scripts/ssoDemo.js
+++ samples/tab-sso/nodejs/src/client/scripts/ssoDemo.js
@@ -6,9 +6,7 @@
     function getClientSideToken() {
 
         return new Promise((resolve, reject) => {
-
             display("1. Get auth token from Microsoft Teams");
-
             microsoftTeams.authentication.getAuthToken({
                 successCallback: (result) => {
                     display(result)
@@ -18,29 +16,22 @@
                     reject("Error getting token: " + error);
                 }
             });
-
         });
-
     }
 
     // 2. Exchange that token for a token with the required permissions
     //    using the web service (see /auth/token handler in app.js)
     function getServerSideToken(clientSideToken) {
-
-        display("2. Exchange for server-side token");
-
         return new Promise((resolve, reject) => {
-
             microsoftTeams.getContext((context) => {
-
-                fetch('/auth/token', {
+                fetch('/getProfileOnBehalfOf', {
                     method: 'post',
                     headers: {
                         'Content-Type': 'application/json'
                     },
                     body: JSON.stringify({
                         'tid': context.tid,
-                        'token': clientSideToken 
+                        'token': clientSideToken
                     }),
                     mode: 'cors',
                     cache: 'default'
@@ -56,9 +47,8 @@
                     if (responseJson.error) {
                         reject(responseJson.error);
                     } else {
-                        const serverSideToken = responseJson;
-                        display(serverSideToken);
-                        resolve(serverSideToken);
+                        const profile = responseJson;
+                        resolve(profile);
                     }
                 });
             });
@@ -68,36 +58,16 @@
     // 3. Get the server side token and use it to call the Graph API
     function useServerSideToken(data) {
 
-        display("3. Call https://graph.microsoft.com/v1.0/me/ with the server side token");
+        display("2. Call https://graph.microsoft.com/v1.0/me/ with the server side token");
 
-        return fetch("https://graph.microsoft.com/v1.0/me/",
-            {
-                method: 'GET',
-                headers: {
-                    "accept": "application/json",
-                    "authorization": "bearer " + data
-                },
-                mode: 'cors',
-                cache: 'default'
-            })
-            .then((response) => {
-                if (response.ok) {
-                    return response.json();
-                } else {
-                    throw (`Error ${response.status}: ${response.statusText}`);
-                }
-            })
-            .then((profile) => {
-                display(JSON.stringify(profile, undefined, 4), 'pre');
-            });
-
+        return display(JSON.stringify(data, undefined, 4), 'pre');
     }
 
     // Show the consent pop-up
     function requestConsent() {
         return new Promise((resolve, reject) => {
             microsoftTeams.authentication.authenticate({
-                url: window.location.origin + "/auth/auth-start",
+                url: window.location.origin + "/auth-start",
                 width: 600,
                 height: 535,
                 successCallback: (result) => {
@@ -127,8 +97,8 @@
         .then((clientSideToken) => {
             return getServerSideToken(clientSideToken);
         })
-        .then((serverSideToken) => {
-            return useServerSideToken(serverSideToken);
+        .then((profile) => {
+            return useServerSideToken(profile);
         })
         .catch((error) => {
             if (error === "invalid_grant") {
@@ -138,10 +108,13 @@
                 button.onclick = (() => {
                     requestConsent()
                         .then((result) => {
-                            // Consent succeeded - use the token we got back
-                            let accessToken = JSON.parse(result).accessToken;
-                            display(`Received access token ${accessToken}`);
-                            useServerSideToken(accessToken);
+                             // Consent succeeded
+                             display(`Consent succeeded`);
+                            
+                             // offer to refresh the page
+                             button.disabled = true;
+                             let refreshButton = display("Refresh page", "button");
+                             refreshButton.onclick = (() => { window.location.reload(); });
                         })
                         .catch((error) => {
                             display(`ERROR ${error}`);
---
+++ samples/tab-sso/nodejs/src/server/tabs.js
@@ -0,0 +1,95 @@
+'use strict';
+const fetch = require("node-fetch");
+const querystring = require("querystring");
+var config = require('config');
+const msal = require('@azure/msal-node');
+
+module.exports.setup = function (app) {
+  var express = require('express')
+
+  // Configure the view engine, views folder and the statics path
+  // Use the JSON middleware
+  app.use(express.json());
+
+  // Setup home page
+  app.get('/', function (req, res) {
+    res.render('hello');
+  });
+
+    // Setup the configure tab, with first and second as content tabs
+  app.get('/configure', function (req, res) {
+    res.render('configure');
+  });
+
+  // ------------------
+  // SSO demo page
+  app.get('/ssodemo', function (req, res) {
+    res.render('ssoDemo');
+  });
+
+  // Pop-up dialog to ask for additional permissions, redirects to AAD page
+  app.get('/auth-start', function (req, res) {
+    var clientId = config.get("tab.appId");
+    res.render('auth-start', { clientId: clientId });
+  });
+
+  // End of the pop-up dialog auth flow, returns the results back to parent window
+  app.get('/auth-end', function (req, res) {
+    var clientId = config.get("tab.appId");
+    res.render('auth-end', { clientId: clientId });
+  });
+
+  // On-behalf-of token exchange
+  app.post('/getProfileOnBehalfOf', function (req, res) {
+    var tid = req.body.tid;
+    var token = req.body.token;
+    var scopes = ["https://graph.microsoft.com/User.Read"];
+
+    // Creating MSAL client
+    const msalClient = new msal.ConfidentialClientApplication({
+      auth: {
+        clientId: config.get("tab.appId"),
+        clientSecret: config.get("tab.appPassword")
+      }
+    });
+    
+    var oboPromise = new Promise((resolve, reject) => {
+      msalClient.acquireTokenOnBehalfOf({
+        authority: `https://login.microsoftonline.com/${tid}`,
+        oboAssertion: token,
+        scopes: scopes,
+        skipCache: true
+      }).then(result => {
+            fetch("https://graph.microsoft.com/v1.0/me/",
+              {
+                method: 'GET',
+                headers: {
+                  "accept": "application/json",
+                  "authorization": "bearer " + result.accessToken
+                },
+                mode: 'cors',
+                cache: 'default'
+              })
+              .then((response) => {
+                if (response.ok) {
+                  return response.json();
+                } else {
+                  throw (`Error ${response.status}: ${response.statusText}`);
+                }
+              })
+              .then((profile) => {
+                resolve(profile);
+              })
+      }).catch(error => {
+        reject({ "error": error.errorCode });
+      });
+    });
+
+    oboPromise.then(function (result) {
+      res.json(result);
+    }, function (err) {
+      console.log(err); // Error: "It broke"
+      res.json(err);
+    });
+  });
+};
