9c4bd079e7f3d2666d807ca5b54741ed23c5d296
Prithvi-MSFT <v-pritka@microsoft.com>
2022-01-05 15:01:10 +0530

    Added actions for webhook cards

--- samples/incoming-webhook/csharp/IncomingWebhook/ClientApp/src/components/card-constant.ts
+++ samples/incoming-webhook/csharp/IncomingWebhook/ClientApp/src/components/card-constant.ts
@@ -1,22 +1,51 @@
-﻿export const DEFAULT_CARD_PAYLOAD = `{
+﻿var baseUrl = window.location.origin;
+export const DEFAULT_CARD_PAYLOAD = `{
     "@type": "MessageCard",
-        "summary": "Task Created",
-            "sections": [
-                {
-                    "activityTitle": "Test task",
-                    "facts": [
-                        {
-                            "name": 'Title:',
-                            "value": 'Test title'
-                        },
-                        {
-                            "name": 'Description:',
-                            "value": 'Test desc'
-                        },
-                        {
-                            "name": 'Assigned To:',
-                            "value": 'Test ass to'
-                        }
-                    ]
-                }]
-}`;
+    "@context": "http://schema.org/extensions",
+    "themeColor": "0076D7",
+    "summary": "Larry Bryant created a new task",
+    "sections": [{
+        "activityTitle": "Larry Bryant created a new task",
+        "activitySubtitle": "On Project Tango",
+        "activityImage": "https://teamsnodesample.azurewebsites.net/static/img/image5.png",
+        "facts": [{
+            "name": "Assigned to",
+            "value": "Megan Bowen"
+        },
+        {
+            "name": "Due date",
+            "value": "Wed Jan 05 2022 17:07:18 GMT-0700 (Pacific Daylight Time)"
+        },
+        {
+            "name": "Status",
+            "value": "Not started"
+        }],
+        "markdown": true
+    }],
+    "potentialAction": [{
+        "@type": "ActionCard",
+        "name": "Add a comment",
+        "inputs": [{
+            "@type": "TextInput",
+            "id": "comment",
+            "isMultiline": false,
+            "value": null,
+            "title": "Add a comment here for this task"
+        }],
+        "actions": [{
+            "@type": "HttpPOST",
+            "name": "Add comment",
+            "body": "{\\"comment\\":\\"{{comment.value}}\\"}",
+            "bodyContentType": "application/json",
+            "target": "${baseUrl}/api/Save"
+        }]
+    }, 
+    {
+        "@type": "OpenUri",
+        "name": "Learn More",
+        "targets": [{
+            "os": "default",
+            "uri": "https://docs.microsoft.com/outlook/actionable-messages"
+        }]
+    }
+]}`;
--- samples/incoming-webhook/csharp/IncomingWebhook/Controllers/CardController.cs
+++ samples/incoming-webhook/csharp/IncomingWebhook/Controllers/CardController.cs
@@ -10,24 +10,52 @@ namespace IncomingWebhook.Controllers
     /// <summary>
     /// Controller class for sending card using incoming webhook.
     /// </summary>
-    [Route("api/Send")]
+    [Route("api")]
     [ApiController]
     public class CardController : ControllerBase
     {
+        public static string url = "";
 
         /// <summary>
         /// Method to send card to team using incoming webhook.
         /// </summary>
         /// <returns></returns>
-        [HttpPost]
+        [HttpPost("Send")]
         public async Task SendCardAsync([FromBody] CardEntity cardEntity)
         {
             try
             {
+                url = cardEntity.WebhookUrl;
                 HttpClient client = new HttpClient();
                 client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
                 var content = new StringContent(cardEntity.CardBody, System.Text.Encoding.UTF8, "application/json");
                 var response = await client.PostAsync(cardEntity.WebhookUrl, content);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        /// Method to save and send user response.
+        /// </summary>
+        /// <returns></returns>
+        [HttpPost("Save")]
+        public async Task SendResponse([FromBody] ResponseEntity entity)
+        {
+            try
+            {
+                string cardJson = @"{
+                ""@type"": ""MessageCard"",
+                ""summary"": ""Response Message"",
+                ""sections"": [{ 
+                ""activityTitle"": ""Welcome Message"",
+                ""text"": ""Submitted response: "+entity.Comment+ @"""}]}";
+                HttpClient client = new HttpClient();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                var content = new StringContent(cardJson, System.Text.Encoding.UTF8, "application/json");
+                var response = await client.PostAsync(url, content);
             }
             catch (Exception ex)
             {
