d25f008243d5c21cc4a078f908ff8d65e39aee38
NikitaJain-MSFT <v-nikija@microsoft.com>
2022-01-06 20:05:02 +0530

    updating obo flow for graph activity feed

--- samples/graph-activity-feed/nodejs/server.js
+++ samples/graph-activity-feed/nodejs/server.js
@@ -3,10 +3,12 @@ const bodyparser = require('body-parser');
 const env = require('dotenv')
 const path = require('path');
 const auth = require('./auth');
-const fetch = require("node-fetch");
-const querystring = require("querystring");
 const app = express();
+const msal = require('@azure/msal-node');
+const axios = require('axios');
 
+var delegatedToken = "";
+var applicationToken = "";
 app.use(express.static(path.join(__dirname, 'static')));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'ejs');
@@ -24,81 +26,433 @@ app.get('/configure', function (req, res) {
 app.get('/GroupChatNotification', function (req, res) {
   var tenantId = req.url.split('=')[1];
   auth.getAccessToken(tenantId).then(async function (token) {
-    res.render('./views/GroupChatNotification', { token: JSON.stringify(token) });
+    applicationToken = token;
+    res.render('./views/GroupChatNotification');
   });
 });
 
 app.get('/TeamNotification', function (req, res) {
   var tenantId = req.url.split('=')[1];
   auth.getAccessToken(tenantId).then(async function (token) {
-    res.render('./views/TeamNotification', { token: JSON.stringify(token) });
+    applicationToken = token;
+    res.render('./views/TeamNotification');
   });
 });
 
 app.get('/UserNotification', function (req, res) {
-  var tenantId = req.url.split('=')[1];
+  var tenantId = process.env.TenantId;
   auth.getAccessToken(tenantId).then(async function (token) {
-    res.render('./views/UserNotification', { token: JSON.stringify(token) });
+    applicationToken = token;
+    res.render('./views/UserNotification');
   });
 });
 
 // Pop-up dialog to ask for additional permissions, redirects to AAD page
-app.get('/auth/auth-start', function (req, res) {
-  var clientId = process.env.ClientId;
-  res.render('./views/auth-start', { clientId: clientId });
+app.get('/auth-start', function (req, res) {
+  res.render('./views/auth-start',{ clientId: JSON.stringify(process.env.ClientId) });
 });
 
 // End of the pop-up dialog auth flow, returns the results back to parent window
-app.get('/auth/auth-end', function (req, res) {
+app.get('/auth-end', function (req, res) {
   var clientId = process.env.ClientId;
   res.render('./views/auth-end', { clientId: clientId });
 });
 
 // On-behalf-of token exchange
-app.post('/auth/token', function(req, res) {
+app.post('/auth/token', function (req, res) {
   var tid = req.body.tid;
   var token = req.body.token;
   var scopes = ["https://graph.microsoft.com/User.Read"];
 
-  var oboPromise = new Promise((resolve, reject) => {
-      const url = "https://login.microsoftonline.com/" + tid + "/oauth2/v2.0/token";
-      const params = {
-          client_id: process.env.ClientId,
-          client_secret: process.env.ClientSecret,
-          grant_type: "urn:ietf:params:oauth:grant-type:jwt-bearer",
-          assertion: token,
-          requested_token_use: "on_behalf_of",
-          scope: scopes.join(" ")
-      };
-  
-      fetch(url, {
-        method: "POST",
-        body: querystring.stringify(params),
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/x-www-form-urlencoded"
-        }
-      }).then(result => {
-        if (result.status !== 200) {
-          result.json().then(json => {
-            // TODO: Check explicitly for invalid_grant or interaction_required
-            reject({"error":json.error});
-          });
-        } else {
-          result.json().then(json => {
-            resolve(json.access_token);
-          });
-        }
-      });
+  // Creating MSAL client
+  const msalClient = new msal.ConfidentialClientApplication({
+    auth: {
+      clientId: process.env.ClientId,
+      clientSecret: process.env.ClientSecret
+    }
   });
 
-  oboPromise.then(function(result) {
-      res.json(result);
-  }, function(err) {
-      console.log(err); // Error: "It broke"
-      res.json(err);
+  var oboPromise = new Promise((resolve, reject) => {
+    msalClient.acquireTokenOnBehalfOf({
+      authority: `https://login.microsoftonline.com/${tid}`,
+      oboAssertion: token,
+      scopes: scopes,
+      skipCache: true
+    }).then(result => {
+      delegatedToken = result.accessToken
+      resolve();
+    }).catch(error => {
+      reject({ "error": error.errorCode });
+    });
+  });
+
+  oboPromise.then(function (result) {
+    res.json(result);
+  }, function (err) {
+    console.log(err); // Error: "It broke"
+    res.json(err);
   });
 });
+
+// Send notification to group chat for task creation.
+app.post('/sendNotificationToGroupChat', (req, res) => {
+  const postData = {
+    "topic": {
+      "source": "0",
+      "value": "https://graph.microsoft.com/v1.0/chats/" + req.body.groupchatId
+    },
+    "activityType": "taskCreated",
+    "previewText": {
+      "content": "New Task Created"
+    },
+    "recipient": {
+      "@odata.type": "microsoft.graph.aadUserNotificationRecipient",
+      "userId": req.body.userId
+    },
+    "templateParameters": [
+      {
+        "name": "taskName",
+        "value": req.body.title
+      }
+    ]
+  };
+
+  var url = "https://graph.microsoft.com/v1.0/chats/" + req.body.groupchatId + "/sendActivityNotification"
+  axios.post(url, postData, {
+    headers: {
+      "accept": "application/json",
+      "contentType": 'application/json',
+      "authorization": "bearer " + applicationToken
+    }
+  })
+    .then(res => {
+      console.log(`statusCode: ${res.status}`)
+      console.log(res)
+    })
+    .catch(error => {
+      console.error(error)
+    })
+});
+
+// Send notification of custom topic in group chat.
+app.post('/sendCustomTopicNotificationGroupChat', (req, res) => {
+  const message = {
+    "body": {
+      "content": "New Deployment: " + req.body.DeployementTitle
+    }
+  }
+
+  var url = "https://graph.microsoft.com/v1.0/chats/" + req.body.groupchatId + "/messages"
+  axios.post(url, message, {
+    headers: {
+      "accept": "application/json",
+      "contentType": 'application/json',
+      "authorization": "bearer " + delegatedToken
+    }
+  })
+    .then(res => {
+      const postData = {
+        "topic": {
+          "source": "entityUrl",
+          "value": "https://graph.microsoft.com/v1.0/chats/" + req.body.groupchatId + "/messages/" + res.data.id
+        },
+        "activityType": "approvalRequired",
+        "previewText": {
+          "content": "Deployment requires your approval"
+        },
+        "recipient": {
+          "@odata.type": "microsoft.graph.aadUserNotificationRecipient",
+          "userId": req.body.userId
+        },
+        "templateParameters": [
+          {
+            "name": "approvalTaskId",
+            "value": req.body.DeployementTitle
+          }
+        ]
+      }
+
+      axios.post("https://graph.microsoft.com/v1.0/chats/" + req.body.groupchatId + "/sendActivityNotification", postData, {
+        headers: {
+          "accept": "application/json",
+          "contentType": 'application/json',
+          "authorization": "bearer " + applicationToken
+        }
+      }).then(response => {
+        console.log(`statusCode: ${response.status}`)
+      })
+    })
+    .catch(error => {
+      console.error(error)
+    })
+});
+
+// Send notification to channel in a team for task creation.
+app.post('/sendNotificationToChannel', (req, res) => {
+  const postData = {
+    "topic": {
+      "source": "entityUrl",
+      "value": "https://graph.microsoft.com/v1.0/teams/" + req.body.teamId
+    },
+    "activityType": "pendingFinanceApprovalRequests",
+    "previewText": {
+      "content": "Internal spending team has a pending finance approval requests"
+    },
+    "recipient": {
+      "@odata.type": "microsoft.graph.aadUserNotificationRecipient",
+      "userId": req.body.userId
+    },
+    "templateParameters": [
+      {
+        "name": "pendingRequestCount",
+        "value": req.body.title
+      }
+    ]
+  };
+
+  var url = "https://graph.microsoft.com/v1.0/teams/" + req.body.teamId + "/sendActivityNotification"
+  axios.post(url, postData, {
+    headers: {
+      "accept": "application/json",
+      "contentType": 'application/json',
+      "authorization": "bearer " + applicationToken
+    }
+  })
+    .then(res => {
+      console.log(`statusCode: ${res.status}`)
+      console.log(res)
+    })
+    .catch(error => {
+      console.error(error)
+    })
+});
+
+// Send notification for channel tab.
+app.post('/channelTabTeamNotification', (req, res) => {
+  var url = "https://graph.microsoft.com/v1.0/teams/" + req.body.teamId + "/channels/" + req.body.channelId + "/tabs?$expand=teamsApp"
+  
+  axios.get(url, {
+    headers: {
+      "accept": "application/json",
+      "contentType": 'application/json',
+      "authorization": "bearer " + applicationToken
+    }
+  })
+    .then(res => {
+      var tabUrl = getTabURL(res.data);
+      const postData = {
+        "topic": {
+          "source": "entityUrl",
+          "value": "https://graph.microsoft.com/v1.0/teams/" + req.body.teamId + "/channels/" + req.body.channelId + "/tabs/" + tabUrl
+        },
+        "activityType": "reservationUpdated",
+        "previewText": {
+          "content": "You have moved up the queue"
+        },
+        "recipient": {
+          "@odata.type": "microsoft.graph.aadUserNotificationRecipient",
+          "userId": req.body.userId
+        },
+        "templateParameters": [
+          {
+            "name": "reservationId",
+            "value": req.body.id
+          },
+          {
+            "name": "currentSlot",
+            "value": req.body.currentSlot
+          }
+        ]
+      };
+
+      axios.post("https://graph.microsoft.com/v1.0/teams/" + req.body.teamId + "/sendActivityNotification", postData, {
+        headers: {
+          "accept": "application/json",
+          "contentType": 'application/json',
+          "authorization": "bearer " + applicationToken
+        }
+      }).then(response => {
+        console.log(`statusCode: ${response.status}`)
+      })
+
+    })
+    .catch(error => {
+      console.error(error)
+    })
+});
+
+// Send notification for custom topic in team.
+app.post('/customTopicTeamNotification', (req, res) => {
+  const message = {
+    "body": {
+      "content": "New Deployment :" + req.body.title
+    }
+  };
+  var url = "https://graph.microsoft.com/v1.0/teams/" + req.body.teamId + "/channels/" + req.body.channelId + "/messages"
+  
+  axios.post(url, message, {
+    headers: {
+      "accept": "application/json",
+      "contentType": 'application/json',
+      "authorization": "bearer " + delegatedToken
+    }
+  })
+    .then(res => {
+      const postData = {
+        "topic": {
+          "source": "text",
+          "value": "Deployment Approvals Channel",
+          "webUrl": res.data.webUrl,
+        },
+        "activityType": "deploymentApprovalRequired",
+        "previewText": {
+          "content": "New deployment requires your approval"
+        },
+        "recipient": {
+          "@odata.type": "microsoft.graph.aadUserNotificationRecipient",
+          "userId": req.body.userId
+        },
+        "templateParameters": [
+          {
+            "name": "deploymentId",
+            "value": req.body.title
+          }
+        ]
+      };
+
+      axios.post("https://graph.microsoft.com/v1.0/teams/" + req.body.teamId + "/sendActivityNotification", postData, {
+        headers: {
+          "accept": "application/json",
+          "contentType": 'application/json',
+          "authorization": "bearer " + applicationToken
+        }
+      }).then(response => {
+        console.log(`statusCode: ${response.status}`)
+      })
+    })
+    .catch(error => {
+      console.error(error)
+    })
+});
+
+// Send notification to user.
+app.post('/sendNotificationToUser', (req, res) => {
+  console.log("token" + delegatedToken);
+  var url = "https://graph.microsoft.com/v1.0/users/" + req.body.userId + "/teamwork/installedApps/?$expand=teamsAppDefinition"
+ 
+  axios.get(url, {
+    headers: {
+      "accept": "application/json",
+      "contentType": 'application/json',
+      "authorization": "bearer " + delegatedToken
+    }
+  }).then(res => {
+    var appId = getAppId(res.data);
+    const postData = {
+      "topic": {
+        "source": "entityUrl",
+        "value": "https://graph.microsoft.com/v1.0/users/" + req.body.userId + "/teamwork/installedApps/" + appId
+      },
+      "activityType": "taskCreated",
+      "previewText": {
+        "content": "New Task Created"
+      },
+      "templateParameters": [
+        {
+          "name": "taskName",
+          "value": req.body.title
+        }
+      ]
+    };
+    
+    axios.post("https://graph.microsoft.com/v1.0/users/" + req.body.userId + "/teamwork/sendActivityNotification",
+      postData,
+      {
+        headers: {
+          "accept": "application/json",
+          "contentType": 'application/json',
+          "authorization": "bearer " + applicationToken
+        }
+      }).then(response => {
+        console.log(`statusCode: ${response.status}`)
+      })
+  }
+  ).catch(error => {
+    console.log("error:" + error)
+  })
+});
+
+// Send notification for custom topic to user.
+app.post('/customTopicNotificationToUser', (req, res) => {
+  const message = {
+    "body": {
+      "content": "New Deployment: " + req.body.DeployementTitle
+    }
+  };
+  var url = "https://graph.microsoft.com/v1.0/teams/" + req.body.teamId + "/channels/" + req.body.channelId + "/messages"
+  
+  axios.post(url, message, {
+    headers: {
+      "accept": "application/json",
+      "contentType": 'application/json',
+      "authorization": "bearer " + delegatedToken
+    }
+  })
+    .then(res => {
+      const postData = {
+        "topic": {
+          "source": "text",
+          "value": "Deployment Approvals Channel",
+          "webUrl": res.data.webUrl
+        },
+        "activityType": "deploymentApprovalRequired",
+        "previewText": {
+          "content": "New deployment requires your approval"
+        },
+        "templateParameters": [
+          {
+            "name": "deploymentId",
+            "value": req.body.DeployementTitle
+          }
+        ]
+      }
+
+      axios.post("https://graph.microsoft.com/v1.0/users/" + req.body.userId + "/teamwork/sendActivityNotification", postData, {
+        headers: {
+          "accept": "application/json",
+          "contentType": 'application/json',
+          "authorization": "bearer " + applicationToken
+        }
+      }).then(response => {
+        console.log(`statusCode: ${response.status}`)
+      })
+    })
+    .catch(error => {
+      console.error(error)
+    })
+})
+
+// Get tab Url.
+function getTabURL(appList) {
+  list = appList.value;
+  var i = 0;
+  for (i = 0; i < list.length; i++) {
+    if (list[i].teamsApp['displayName'] == "NotifyFeedApp") {
+      return list[i].id;
+    }
+  }
+}
+
+// Get app id.
+function getAppId(appList) {
+  var list = appList.value;
+  var i;
+  for (i = 0; i < list.length; i++) {
+    if (list[i].teamsAppDefinition['displayName'] == "NotifyFeedApp") {
+      return list[i].id;
+    }
+  }
+}
 
 app.listen(3978, function () {
   console.log('app listening on port 3978!');
