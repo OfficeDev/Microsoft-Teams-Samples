8d69e3375902fd38a37b9e4de0b98103be9f92fa
AbhijeetThakur-msft <v-abt@microsoft.com>
2022-03-30 01:31:20 +0530

    changes for maintaining state of meeting.

--- samples/meetings-stage-view/nodejs/server.js
+++ samples/meetings-stage-view/nodejs/server.js
@@ -4,7 +4,12 @@ const path = require('path');
 const app = express();
 
 const server = require('http').createServer(app);
-const io = require('socket.io')(server,{cors:{origin:"*"}});
+const io = require('socket.io')(server, { cors: { origin: "*" } });
+const todoData = {};
+const doneData = {};
+const doingData = {};
+
+
 app.use(express.static(__dirname + '/static'));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'ejs');
@@ -40,8 +45,26 @@ app.get('/doneView', function (req, res) {
 app.use("/Images", express.static(path.resolve(__dirname, './Images')));
 
 io.on("connection", (socket) => {
-  socket.on("message", (message, status) => {
-    io.emit("message", message, status)
+  socket.on("message", (message, status, meetingId) => {
+    io.emit("message", message, status);
+    if (status === "todo") {
+      if (todoData.hasOwnProperty(meetingId)) {
+        todoData[meetingId] = new Array();
+      }
+      todoData[meetingId].push({ taskName: message.taskDescription, assignedTo: message.userName });
+    }
+    if (status === "doing") {
+      if (doingData.hasOwnProperty(meetingId)) {
+        doingData[meetingId] = new Array();
+      }
+      doingData[meetingId].push({ taskName: message.taskDescription, assignedTo: message.userName });
+    }
+    if (status === "done") {
+      if (!doneData.hasOwnProperty(meetingId)) {
+        doneData[meetingId] = new Array();
+      }
+      doneData[meetingId].push({ taskName: message.taskDescription, assignedTo: message.userName });
+    }
   })
 });
 
--- samples/meetings-stage-view/nodejs/static/socket.js
+++
@@ -1,5 +0,0 @@
- // Creating socket instance for real time status updates in stage view.
- const socket = io();
- socket.on('connection')
- 
- exports.socketConn = socket;
