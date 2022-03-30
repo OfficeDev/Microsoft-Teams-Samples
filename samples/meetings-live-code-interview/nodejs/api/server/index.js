282db88d1f0debb215f91b5d333bdeccaf4ffb12
NikitaJain-MSFT <v-nikija@microsoft.com>
2022-03-30 20:29:13 +0530

    updating ui

--- samples/meetings-live-code-interview/nodejs/api/server/index.js
+++ samples/meetings-live-code-interview/nodejs/api/server/index.js
@@ -3,65 +3,65 @@ const PORT = process.env.PORT || 3978;
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({
-    extended: true
+  extended: true
 }));
 const server = require('http').createServer(app);
-const io = require('socket.io')(server,{cors:{origin: "*"}});
+const io = require('socket.io')(server, { cors: { origin: "*" } });
 const editorData = {};
 
 app.get('/api/editorState', function (req, res) {
-  var questionId = req.url.substring(req.url.search('=')+1,req.url.search('&'));
+  var questionId = req.url.substring(req.url.search('=') + 1, req.url.search('&'));
   var meetingId = req.url.split('&meetingId=')[1];
   var latestState = {
     "value": null,
   }
-  var currentData =  editorData["meetings"];
 
-  if(currentData==undefined){
+  if (editorData["meetings"] == undefined) {
     const newMeeting = new Array();
-    newMeeting.push({meetingId:meetingId,questions:[{
-      questionId:"1",
-      value:""
-    },
-    {
-      questionId:"2",
-      value:""
-    },{
-      questionId:"3",
-      value:""
-    }]})
-    currentData = newMeeting;
-    editorData["meetings"] = currentData;
-    latestState.value = "";
-  }
-  else if(!currentData.find((meeting) => {
-    if (meeting.meetingId == meetingId) {
-      return true;
-    }})){
-      const newMeeting = currentData;
-      newMeeting.push({meetingId:meetingId,questions:[{
-        questionId:"1",
-        value:""
+    newMeeting.push({
+      meetingId: meetingId, questions: [{
+        questionId: "1",
+        value: ""
       },
       {
-        questionId:"2",
-        value:""
-      },{
-        questionId:"3",
-        value:""
-      }]})
-      currentData = newMeeting;
-      editorData["meetings"] = currentData;
-      latestState.value = "";
+        questionId: "2",
+        value: ""
+      },
+      {
+        questionId: "3",
+        value: ""
+      }]
+    })
+    editorData["meetings"] = newMeeting;
   }
-  else{
-    currentData.find((meeting) => {
+  else if (!editorData["meetings"].find((meeting) => {
+    if (meeting.meetingId == meetingId) {
+      return true;
+    }
+  })) {
+    editorData["meetings"].push({
+      meetingId: meetingId, questions: [{
+        questionId: "1",
+        value: ""
+      },
+      {
+        questionId: "2",
+        value: ""
+      },
+      {
+        questionId: "3",
+        value: ""
+      }]
+    })
+  }
+  else {
+    editorData["meetings"].find((meeting) => {
       if (meeting.meetingId == meetingId) {
-         meeting.questions.find(question=>{
-            if(question.questionId == questionId){
-              latestState.value = question.value;
-            }
-          })
+        meeting.questions.find(question => {
+          if (question.questionId == questionId) {
+            latestState.value = question.value;
+          }
+        })
       }
     });
   }
@@ -69,27 +69,25 @@ app.get('/api/editorState', function (req, res) {
 });
 
 io.on("connection", (socket) => {
-    socket.on("message", (message,questionId,meetingId) => {
-      let currentData = editorData["meetings"];
-      let currentMeeting;
-      let updateindex;
-      currentData.find((meeting,meetingIndex) => {
-        if (meeting.meetingId == meetingId) {
-          updateindex = meetingIndex;
-          currentMeeting = meeting;
-            meeting.questions.find((question,index)=>{
-              if(question.questionId == questionId){
-                currentMeeting.questions[index].value =message;
-              }
-            })
-        }
-      });
-      currentData[updateindex] = currentMeeting;
-      editorData["meetings"] = currentData;
-      io.emit("message", message)
-    })
-  });
+  socket.on("message", (message, questionId, meetingId) => {
+    let currentMeeting;
+    let updateindex;
+    editorData["meetings"].find((meeting, meetingIndex) => {
+      if (meeting.meetingId == meetingId) {
+        updateindex = meetingIndex;
+        currentMeeting = meeting;
+        meeting.questions.find((question, index) => {
+          if (question.questionId == questionId) {
+            currentMeeting.questions[index].value = message;
+          }
+        })
+      }
+    });
+    editorData["meetings"][updateindex] = currentMeeting;
+    io.emit("message", message)
+  })
+});
 
 server.listen(PORT, () => {
-    console.log(`Server listening on http://localhost:${PORT}`);
+  console.log(`Server listening on http://localhost:${PORT}`);
 });
