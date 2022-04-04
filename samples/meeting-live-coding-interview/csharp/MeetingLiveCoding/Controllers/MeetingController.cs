106e827aa45c786402a7149b1416078145bc5130
Prithvi-MSFT <v-pritka@microsoft.com>
2022-04-04 13:58:28 +0530

    PR fixes

--- samples/meeting-live-coding-interview/csharp/MeetingLiveCoding/Controllers/MeetingController.cs
+++ samples/meeting-live-coding-interview/csharp/MeetingLiveCoding/Controllers/MeetingController.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using MeetingLiveCoding.Models;
+using System.Linq;
 
 namespace MeetingLiveCoding.Controllers
 {
@@ -33,10 +34,44 @@ namespace MeetingLiveCoding.Controllers
 
                 if (_meetingDetails == null)
                 {
-                    var meetingDetails = new MeetingDetails()
+                    MeetingDetails meetingDetails = CreateNewMeeting(meetingId);
+
+                    _meetingDetails.Add(meetingDetails);
+                    latestValue.Value = null;
+                }
+
+                else
+                {
+                    var data = _meetingDetails.Find(e => e.MeetingId == meetingId);
+                    if (data == null)
                     {
-                        MeetingId = meetingId,
-                        Questions = new List<Question>
+                        MeetingDetails meetingDetails = CreateNewMeeting(meetingId);
+
+                        latestValue.Value = null;
+                        _meetingDetails.Add(meetingDetails);
+                    }
+
+                    else
+                    {
+                        var questionData = data.Questions.FirstOrDefault(item => item.QuestionId == questionId);
+                        latestValue.Value = questionData.Value;
+                    }
+                }
+
+                    return Ok(latestValue);
+                }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private static MeetingDetails CreateNewMeeting(string meetingId)
+        {
+            return new MeetingDetails()
+            {
+                MeetingId = meetingId,
+                Questions = new List<Question>
                         {
                            new Question()
                            {
@@ -54,58 +89,7 @@ namespace MeetingLiveCoding.Controllers
                                Value = ""
                            }
                         }
-                    };
-
-                    _meetingDetails.Add(meetingDetails);
-                    latestValue.Value = null;
-                }
-
-                else
-                {
-                    var data = _meetingDetails.Find(e => e.MeetingId == meetingId);
-                    if (data == null)
-                    {
-                        var meetingDetails = new MeetingDetails()
-                        {
-                            MeetingId = meetingId,
-                            Questions = new List<Question>
-                        {
-                            new Question()
-                            {
-                                QuestionId = "1",
-                                Value = ""
-                            },
-                            new Question()
-                            {
-                                QuestionId = "2",
-                                Value = ""
-                            },
-                            new Question()
-                            {
-                                QuestionId = "3",
-                                Value = ""
-                            }
-                        }
-                        };
-
-                        latestValue.Value = null;
-                        _meetingDetails.Add(meetingDetails);
-                    }
-
-                    else
-                    {
-                        var meetingIndex = _meetingDetails.FindIndex(e => e.MeetingId == meetingId);
-                        var questionIndex = _meetingDetails[meetingIndex].Questions.FindIndex(e => e.QuestionId == questionId);
-                        latestValue.Value = _meetingDetails[meetingIndex].Questions[questionIndex].Value;
-                    }
-                }
-
-                    return Ok(latestValue);
-                }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
+            };
         }
 
         /// <summary>
@@ -119,10 +103,12 @@ namespace MeetingLiveCoding.Controllers
         {
             try
             {
-                var data = _meetingDetails.Find(e => e.MeetingId == meetingData.MeetingId);
-                var meetingIndex = _meetingDetails.FindIndex(e => e.MeetingId == meetingData.MeetingId);
-                var questionIndex = _meetingDetails[meetingIndex].Questions.FindIndex(e => e.QuestionId == meetingData.QuestionId);
-                _meetingDetails[meetingIndex].Questions[questionIndex].Value = meetingData.Description;
+                var data = _meetingDetails.FirstOrDefault(e => e.MeetingId == meetingData.MeetingId);
+                if (data != null)
+                {
+                    var questionData = data.Questions.FirstOrDefault(item => item.QuestionId == meetingData.QuestionId);
+                    questionData.Value = meetingData.Description;
+                }
             }
             catch (Exception ex)
             {
