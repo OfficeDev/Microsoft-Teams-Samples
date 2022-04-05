25225ebcd42b0fa33029bb0d2d74a7c8508f9d12
Eric Scherlinger <35633680+ericsche@users.noreply.github.com>
2022-04-05 12:49:00 +0200

    Update index.tsx
    Updating code to work with the latest version of teams fluentUI.
    https://fluentsite.z22.web.core.windows.net/quick-start

--- samples/tab-personal-sso-quickstart/ts/src/index.tsx
+++ samples/tab-personal-sso-quickstart/ts/src/index.tsx
@@ -5,10 +5,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
-import { Provider, themes } from '@fluentui/react-northstar' //https://fluentsite.z22.web.core.windows.net/quick-start
+import { Provider, teamsTheme  } from '@fluentui/react-northstar' //https://fluentsite.z22.web.core.windows.net/quick-start
 
 ReactDOM.render(
-    <Provider theme={themes.teams}>
+    <Provider theme={teamsTheme}>
         <App />
     </Provider>, document.getElementById('root')
 );
