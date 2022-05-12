98e8bfe96a3da0e6a5c9c10a8488e44e72e34312
Piyush Singh <65072631+singhpiyush998@users.noreply.github.com>
2022-05-12 17:46:33 +0530

    Update app.js

--- samples/app-hello-world/nodejs/src/app.js
+++ samples/app-hello-world/nodejs/src/app.js
@@ -3,18 +3,60 @@
 
 import path from 'path';
 import restify from 'restify';
-import { adapter, EchoBot } from './bot';
-import tabs from './tabs';
-import MessageExtension from './message-extension';
+import { adapter, EchoBot } from './bot.js';
+import tabs from './tabs.js';
+import MessageExtension from './message-extension.js';
+import { fileURLToPath } from 'url';
+import dotenv from 'dotenv';
 
 // See https://aka.ms/bot-services to learn more about the different parts of a bot.
 import { ActivityTypes } from 'botbuilder';
 
 // Read botFilePath and botFileSecret from .env file.
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const ENV_FILE = path.join(__dirname, '.env');
-require('dotenv').config({ path: ENV_FILE });
+dotenv.config({ path: ENV_FILE });
 
 //Create HTTP server.
+const server = restify.createServer({
+    formatters: {
+        'text/html': function (req, res, body) {
+            return body;
+        },
+    },
+});
+
+server.get(
+    '/*',
+    restify.plugins.serveStatic({
+        directory: __dirname + '/static',
+    })
+);
+
+server.listen(process.env.port || process.env.PORT || 3333, function () {
+    console.log(`\n${server.name} listening to ${server.url}`);
+});
+
+// Adding tabs to our app. This will setup routes to various views
+tabs(server);
+
+// Adding a bot to our app
+const bot = new EchoBot();
+
+// Adding a messaging extension to our app
+const messageExtension = new MessageExtension();
+
+// Listen for incoming requests.
+server.post('/api/messages', (req, res) => {
+    adapter.processActivity(req, res, async (context) => {
+        if (context.activity.type === ActivityTypes.Invoke)
+            await messageExtension.run(context);
+        else await bot.run(context);
+    });
+});
+
 const server = restify.createServer({
     formatters: {
         'text/html': function (req, res, body) {
