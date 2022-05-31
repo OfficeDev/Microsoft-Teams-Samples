380e161749bfb2158fc6701bc0bc39cd8d9941df
Scott Perham <scott.perham@microsoft.com>
2022-05-31 14:29:41 +0100

    Added node version of the azure AD consent bot

---
+++ samples/bot-consent-azuread/nodejs/src/app.js
@@ -0,0 +1,66 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+const path = require('path');
+const restify = require('restify');
+const EchoBot = require('./bot');
+const { BotFrameworkAdapter } = require('botbuilder');
+
+// Read botFilePath and botFileSecret from .env file.
+const ENV_FILE = path.join(__dirname, '.env');
+require('dotenv').config({ path: ENV_FILE });
+
+//Create HTTP server.
+const server = restify.createServer({
+    formatters: {
+        'text/html': function (req, res, body) {
+            return body;
+        },
+    },
+});
+
+server.get(
+    '/*',
+    restify.plugins.serveStatic({
+        directory: __dirname + '/static',
+    })
+);
+
+server.listen(process.env.port || process.env.PORT || 3333, function () {
+    console.log(`\n${server.name} listening to ${server.url}`);
+});
+
+// Create adapter.
+// See https://aka.ms/about-bot-adapter to learn more about adapters.
+const adapter = new BotFrameworkAdapter({
+    appId: process.env.MicrosoftAppId,
+    appPassword: process.env.MicrosoftAppPassword,
+});
+
+adapter.onTurnError = async (context, error) => {
+    const errorMsg = error.message
+        ? error.message
+        : `Oops. Something went wrong!`;
+    // This check writes out errors to console log .vs. app insights.
+    // NOTE: In production environment, you should consider logging this to Azure
+    //       application insights.
+    console.error(`\n [onTurnError] unhandled error: ${error}`);
+
+    // Clear out state
+    await conversationState.delete(context);
+    // Send a message to the user
+    await context.sendActivity(errorMsg);
+
+    // Note: Since this Messaging Extension does not have the messageTeamMembers permission
+    // in the manifest, the bot will not be allowed to message users.
+};
+
+// Adding a bot to our app
+const bot = new EchoBot();
+
+// Listen for incoming requests.
+server.post('/api/messages', (req, res) => {
+    adapter.processActivity(req, res, async (context) => {
+        await bot.run(context);
+    });
+});
