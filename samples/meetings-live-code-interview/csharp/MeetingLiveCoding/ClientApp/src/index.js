43b6624e126cbd51dc0d145bd70b51ce55210e3f
Prithvi-MSFT <v-pritka@microsoft.com>
2022-07-01 11:26:10 +0530

    added live share sdk for c# sample

--- samples/meetings-live-code-interview/csharp/MeetingLiveCoding/ClientApp/src/index.js
+++ samples/meetings-live-code-interview/csharp/MeetingLiveCoding/ClientApp/src/index.js
@@ -1,16 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { Provider, teamsTheme } from '@fluentui/react-northstar'
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider theme={teamsTheme}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
