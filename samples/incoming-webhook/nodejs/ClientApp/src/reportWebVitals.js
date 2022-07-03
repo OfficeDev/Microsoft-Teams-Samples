e04ee733da0432ce7e6951b9ac6392176a10b865
Prithvi-MSFT <87962396+Prithvi-MSFT@users.noreply.github.com>
2022-07-03 21:50:03 +0530

    Changed the live-coding sample to use the live share sdk nodejs (#381)
    * Changed the live-coding sample to use the live share sdk
    
    * Update README.md

--- samples/incoming-webhook/nodejs/ClientApp/src/reportWebVitals.js
+++ samples/incoming-webhook/nodejs/ClientApp/src/reportWebVitals.js
@@ -5,7 +5,7 @@ const reportWebVitals = onPerfEntry => {
       getFID(onPerfEntry);
       getFCP(onPerfEntry);
       getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
+         getTTFB(onPerfEntry);
     });
   }
 };
