2c05773330f25b8ba0235baf2544ec8f92d97cc1
Hunaid Hanfee-MSFT <64194049+HunaidHanfee-MSFT@users.noreply.github.com>
2022-08-02 20:43:04 +0530

    Graph Team Tag CRUD - NodeJs (#416)
    * Sample completed
    
    * Update README.md
    
    * Update README.md
    
    * PR comment fix.

---
+++ samples/graph-teams-tag/nodejs/controller/index.js
@@ -0,0 +1,14 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+const teamTagController = require('./team-tag-controller');
+const express = require('express');
+const router = express.Router();
+
+// Routes for the API calls.
+router.post('/', teamTagController.createTeamTag);
+router.get('/list', teamTagController.listTeamTagAsync);
+router.patch('/update', teamTagController.updateTeamTagAsync);
+router.get('/members', teamTagController.getTeamworkTagMembersAsync);
+  
+module.exports = router;
