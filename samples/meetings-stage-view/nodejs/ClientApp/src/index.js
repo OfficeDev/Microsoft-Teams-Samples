ab1a1d111b07f27a287dd46425395764cd93c36b
Keshav Sharma <v-keshsharma@microsoft.com>
2022-08-12 13:01:39 +0530

    nodeJs code updated

--- samples/meetings-stage-view/nodejs/ClientApp/src/index.js
+++ samples/meetings-stage-view/nodejs/ClientApp/src/index.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import { AppRoute } from './router/router';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <AppRoute />
-  </React.StrictMode>
+ReactDOM.render(
+    <React.StrictMode>
+        <AppRoute />
+    </React.StrictMode>, document.getElementById('root')
 );
