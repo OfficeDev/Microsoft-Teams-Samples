78292f57b5092555279f848b93d6b3dda1f13a9d
Hunaid Hanfee <v-huhanf@microsoft.com>
2022-08-19 10:57:47 +0530

    env config

---
+++ samples/graph-meeting-notification/nodejs/helper/decryption-helper.js
@@ -0,0 +1,13 @@
+const { X509Certificate } = require('crypto');
+let pki = require('node-forge').pki;
+
+class DecryptionHelper {
+    static async getDecryptedContent(encryptedContent) {
+        var decryptedSymmetricKey = null;
+
+        require('node-forge').pki.crea
+        var cert = new X509Certificate();
+
+        cert.checkPrivateKey
+    }
+}
