4ea217b2ee3af713a284fafa079bb7374f5b1b39
Eoin O'Brien <eoobrien@microsoft.com>
2022-09-13 20:23:37 +0100

    Meeting Signing: Prevent Multiple Graph Consents

--- samples/meetings-share-to-stage-signing/csharp/Source/MeetingSigning.Web/ClientApp/src/api/fetchClient.ts
+++ samples/meetings-share-to-stage-signing/csharp/Source/MeetingSigning.Web/ClientApp/src/api/fetchClient.ts
@@ -1,10 +1,13 @@
 import { merge } from 'lodash';
-import { ApiErrorResponse, ErrorCode } from 'models/ApiErrorResponse';
+import { ApiErrorResponse } from 'models/ApiErrorResponse';
 import * as microsoftTeams from '@microsoft/teams-js';
 
 // This function is for callers where authentication is required.
 // It makes a fetch client call with an AAD token.
-export async function authFetch<T>(urlPath: string, init?: RequestInit) {
+export async function authFetch<T>(
+  urlPath: string,
+  init?: RequestInit,
+) {
   const token = await microsoftTeams.authentication
     .getAuthToken()
     .catch((err) => {
@@ -18,31 +21,20 @@ export async function authFetch<T>(urlPath: string, init?: RequestInit) {
       'Content-Type': 'application/json;charset=UTF-8',
     },
   });
+
   return (await fetchClient(urlPath, mergedInit)) as T;
 }
 
-async function fetchClient(urlPath: string, mergedInit: RequestInit): Promise<any> {
+async function fetchClient(
+  urlPath: string,
+  mergedInit: RequestInit,
+): Promise<any> {
   const response = await fetch(`/${urlPath}`, mergedInit);
 
   if (!response.ok) {
     const errorJson: ApiErrorResponse = await response.json();
     if (!errorJson) {
       throw new Error('Error fetching data.');
-    }
-
-    if (errorJson.ErrorCode === ErrorCode.AuthConsentRequired) {
-      try {
-        await microsoftTeams.authentication.authenticate({
-          url: `${window.location.origin}/auth-start`,
-          width: 600,
-          height: 535,
-        });
-
-        console.log('Consent provided.');
-        return await fetchClient(urlPath, mergedInit);
-      } catch (error) {
-        console.error("Failed to get consent: '" + error + "'");
-      }
     }
 
     throw new Error(`${errorJson.ErrorCode}: ${errorJson.Message}`);
