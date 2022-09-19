34aa87d10b4a1b240451f29b543e3c1610e4f9ca
Keshav Sharma <v-keshsharma@microsoft.com>
2022-09-19 11:57:01 +0530

    code commit to update version and change in AdapterWithErrorHanlder

--- samples/bot-conversation-sso-quickstart/csharp_dotnetcore/BotConversationSsoQuickstart/Program.cs
+++ samples/bot-conversation-sso-quickstart/csharp_dotnetcore/BotConversationSsoQuickstart/Program.cs
@@ -2,28 +2,62 @@
 // Licensed under the MIT License.
 
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.BotBuilderSamples;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Integration.AspNet.Core;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.Bot.Connector.Authentication;
 
-namespace Microsoft.BotBuilderSamples
+var builder = Microsoft.AspNetCore.Builder.WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers().AddNewtonsoftJson();
+
+builder.Services.AddSingleton<BotFrameworkAuthentication, ConfigurationBotFrameworkAuthentication>();
+
+// Create the Bot Framework Adapter with error handling enabled.
+builder.Services.AddSingleton<IBotFrameworkHttpAdapter, AdapterWithErrorHandler>();
+
+// Create the storage we'll be using for User and Conversation state. (Memory is great for testing purposes.)
+builder.Services.AddSingleton<IStorage, MemoryStorage>();
+
+// Create the User state. (Used in this bot's Dialog implementation.)
+builder.Services.AddSingleton<UserState>();
+
+// Create the Conversation state. (Used by the Dialog system itself.)
+builder.Services.AddSingleton<ConversationState>();
+
+// The Dialog that will be run by the bot.
+builder.Services.AddSingleton<MainDialog>();
+
+// Dialog Manager handles initiating the Dialog Stack, saving state, etc.
+builder.Services.AddSingleton<DialogManager>();
+
+// Register the Token Exchange Helper, for processing TokenExchangeOperation Invoke Activities 
+builder.Services.AddSingleton<TokenExchangeHelper>();
+
+// Create the bot as a transient. In this case the ASP Controller is expecting an IBot.
+builder.Services.AddTransient<IBot, TeamsBot<MainDialog>>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.l
+if (!app.Environment.IsDevelopment())
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.ConfigureLogging((logging) =>
-                    {
-                        logging.AddDebug();
-                        logging.AddConsole();
-                    });
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+    app.UseHsts();
 }
+else
+{
+    app.UseDeveloperExceptionPage();
+}
+app.UseDefaultFiles()
+               .UseStaticFiles()
+               .UseRouting()
+               .UseAuthorization()
+               .UseEndpoints(endpoints =>
+               {
+                   endpoints.MapControllers();
+               });
+app.Run();
