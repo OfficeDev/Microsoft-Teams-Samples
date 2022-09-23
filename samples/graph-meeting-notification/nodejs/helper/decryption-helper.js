aeab04eeee7f06f866c8fa37ed0beee28c208455
Prithvi-MSFT <v-prkamble@microsoft.com>
2022-09-24 00:31:08 +0530

    graph meeting notification nodejs sample

---
+++ samples/graph-meeting-notification/nodejs/helper/decryption-helper.js
@@ -0,0 +1,45 @@
+
+const certHelper = require('./certHelper');
+
+class DecryptionHelper {
+    static async getDecryptedContent(notification) {
+
+        if (notification) {
+            
+            var payLoad = this.processEncryptedNotification(notification);
+            return payLoad;
+            
+        }
+    }
+
+    /**
+    * Processes an encrypted notification
+    * @param  {object} notification - The notification containing encrypted content
+    */
+    static processEncryptedNotification(notification) {
+        // Decrypt the symmetric key sent by Microsoft Graph
+        const symmetricKey = certHelper.decryptSymmetricKey(
+            notification[0].encryptedContent.dataKey,
+            process.env.PRIVATE_KEY_PATH
+        );
+
+        // Validate the signature on the encrypted content
+        const isSignatureValid = certHelper.verifySignature(
+            notification[0].encryptedContent.dataSignature,
+            notification[0].encryptedContent.data,
+            symmetricKey
+        );
+
+        if (isSignatureValid) {
+            // Decrypt the payload
+            const decryptedPayload = certHelper.decryptPayload(
+                notification[0].encryptedContent.data,
+                symmetricKey
+            );
+
+            return JSON.parse(decryptedPayload);
+        }
+    }
+}
+
+exports.DecryptionHelper = DecryptionHelper;
