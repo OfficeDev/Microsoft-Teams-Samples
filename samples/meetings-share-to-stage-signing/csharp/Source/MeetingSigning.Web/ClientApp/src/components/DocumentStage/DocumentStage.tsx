2699de3386955ce243862a44690ec1e814c65954
Eoin O'Brien <eoobrien@microsoft.com>
2022-10-11 16:37:42 -0400

    Add Live Share to Meeting Signing

--- samples/meetings-share-to-stage-signing/csharp/Source/MeetingSigning.Web/ClientApp/src/components/DocumentStage/DocumentStage.tsx
+++ samples/meetings-share-to-stage-signing/csharp/Source/MeetingSigning.Web/ClientApp/src/components/DocumentStage/DocumentStage.tsx
@@ -1,12 +1,25 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useRef, useState } from 'react';
 import { Flex, Loader } from '@fluentui/react-northstar';
+import { PresenceState } from '@microsoft/live-share';
 import { useParams } from 'react-router-dom';
-import { useDefaultColorScheme } from 'hooks';
-import { useAADId } from 'utils/TeamsProvider/hooks';
+import {
+  useDefaultColorScheme,
+  useCursorLocationDom,
+  useCursorLocationsLiveShare,
+  useLiveShare,
+  useScrollOffsetDom,
+  useScrollOffsetLiveShare,
+  useTakeControl,
+} from 'hooks';
+import { useTeamsContext } from 'utils/TeamsProvider/hooks';
 import { getDocument } from 'api/documentApi';
 import { DocumentChooser } from 'components/Documents';
+import { LiveSharePage } from 'components/LiveSharePage';
+import { CursorsRenderer } from 'components/Cursor';
+import { StageControls } from 'components/StageControls';
 import { Document } from 'models';
 import { useQuery } from 'react-query';
+import styles from './DocumentStage.module.css';
 
 /**
  * A component that calls the `getDocument` API, get's the document and
@@ -15,9 +28,59 @@ import { useQuery } from 'react-query';
  * @returns A component with a Document rendered on the stage
  */
 export function DocumentStage() {
+  const teamsContext = useTeamsContext();
   const params = useParams();
   const documentId: string = params.documentId ?? 'unknown';
   const pollingInterval = 2000;
+  var documentStageRef = useRef<HTMLDivElement>(null!);
+
+  const { position, setPosition } = useScrollOffsetDom(
+    documentStageRef.current,
+  );
+  const { cursorLocation } = useCursorLocationDom(documentStageRef.current);
+  const {
+    scrollOffsetEvent,
+    cursorLocationsEvent,
+    takeControlState,
+    container,
+    audience,
+  } = useLiveShare();
+
+  const {
+    takeControlStarted,
+    localUserInControl,
+    localUserCanTakeControl,
+    takeControl,
+    clearControl,
+  } = useTakeControl(takeControlState, teamsContext?.user, audience);
+
+  const {
+    scrollOffsetStarted,
+    followSuspended,
+    endSuspension,
+    sendScrollOffset,
+  } = useScrollOffsetLiveShare(
+    scrollOffsetEvent,
+    setPosition,
+    takeControlState,
+    localUserInControl,
+    teamsContext?.user?.id,
+  );
+
+  const { cursorLocationsStarted, sendCursorLocation } =
+    useCursorLocationsLiveShare(
+      cursorLocationsEvent,
+      teamsContext?.user?.displayName ?? teamsContext?.user?.userPrincipalName,
+      teamsContext?.user?.id,
+    );
+
+  const started = useMemo(() => {
+    return [
+      takeControlStarted,
+      scrollOffsetStarted,
+      cursorLocationsStarted,
+    ].every((value) => value === true);
+  }, [takeControlStarted, scrollOffsetStarted, cursorLocationsStarted]);
 
   // We are using https://react-query.tanstack.com/ for handling the calls to our APIs.
   // Here when the documentId changes, React Query will fetch the document from the API.
@@ -30,7 +93,6 @@ export function DocumentStage() {
 
   const colorScheme = useDefaultColorScheme();
   const stageInlineStyles = { background: colorScheme.background };
-  const loggedInAADId = useAADId();
 
   const [showLoader, setShowLoader] = useState<boolean>(true);
   const showLoaderTimeout = 5000;
@@ -41,27 +103,73 @@ export function DocumentStage() {
     }, showLoaderTimeout);
 
     return () => clearTimeout(timer);
-  }, []);
+  }, [setShowLoader]);
+
+  useEffect(() => {
+    // When position is changed, we need to send the new scrollOffset to the other users via Live Share.
+    sendScrollOffset(position);
+  }, [position, sendScrollOffset]);
+
+  useEffect(() => {
+    sendCursorLocation(cursorLocation);
+  }, [cursorLocation, sendCursorLocation]);
 
   return (
     <>
-      <Flex styles={stageInlineStyles}>
-        {error &&
-          ((showLoader && <Loader />) || (
-            <h1>Error loading document: {error.message ?? error}</h1>
-          ))}
-        {data && (
-          <>
-            <DocumentChooser
-              documentId={data.id}
-              documentType={data.documentType}
-              loggedInAadId={loggedInAADId}
-              signatures={data.signatures}
-              clickable
-            />
-          </>
-        )}
-      </Flex>
+      <LiveSharePage
+        context={teamsContext}
+        container={container}
+        started={started}
+      >
+        <Flex
+          className={styles.stageControlsDiv}
+          hAlign="center"
+          vAlign="center"
+        >
+          <StageControls
+            localUserInControl={localUserInControl}
+            localUserCanTakeControl={localUserCanTakeControl}
+            userInControl={takeControlState?.data?.userId !== undefined}
+            nameOfUserInControl={takeControlState?.data?.displayName}
+            followSuspended={followSuspended}
+            endSuspension={endSuspension}
+            takeControl={takeControl}
+            clearControl={clearControl}
+          />
+        </Flex>
+        <Flex styles={stageInlineStyles}>
+          {error &&
+            ((showLoader && <Loader />) || (
+              <h1>Error loading document: {error.message ?? error}</h1>
+            ))}
+          {data && (
+            <div className={styles.documentChooser} ref={documentStageRef}>
+              <DocumentChooser
+                documentId={data.id}
+                documentType={data.documentType}
+                // You should not use user information from the context if you need to prove a user's identity.
+                // Here, we are using it control the UI to highlight a user's signature box, so we feel comfortable using it.
+                loggedInAadId={teamsContext?.user?.id ?? ''}
+                signatures={data.signatures}
+                clickable
+              />
+              {cursorLocationsEvent && (
+                <CursorsRenderer
+                  cursors={cursorLocationsEvent
+                    .toArray()
+                    .filter(
+                      (p) =>
+                        p.state === PresenceState.online &&
+                        p.data !== undefined &&
+                        p.userId !== teamsContext?.user?.id,
+                    )}
+                  parentBoundingBox={documentStageRef.current?.getBoundingClientRect()}
+                />
+              )}
+            </div>
+          )}
+        </Flex>
+      </LiveSharePage>
     </>
   );
 }
