0bda12eab99664e67ec6bde7779eccb78924d7c7
Eoin O'Brien <eoobrien@microsoft.com>
2022-10-26 18:07:03 -0400

    Restructure directory, move some code to domain factories

---
+++ samples/bot-calling-meeting/csharp/Source/CallingMediaBot.Domain/Factories/IAdaptiveCardFactory.cs
@@ -0,0 +1,10 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+
+namespace CallingMediaBot.Domain.Factories;
+
+using Microsoft.Bot.Schema;
+
+public interface IAdaptiveCardFactory
+{
+    Attachment CreateWelcomeCard();
+}
