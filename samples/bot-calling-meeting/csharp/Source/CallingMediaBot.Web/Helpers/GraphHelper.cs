ee164bdd7499589aef69a916b077f4882fde9f18
Eoin O'Brien <eoobrien@microsoft.com>
2022-11-02 21:35:22 -0400

    remove double lines

--- samples/bot-calling-meeting/csharp/Source/CallingMediaBot.Web/Helpers/GraphHelper.cs
+++ samples/bot-calling-meeting/csharp/Source/CallingMediaBot.Web/Helpers/GraphHelper.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-
 namespace CallingMediaBot.Web.Helpers;
 using System;
 using System.Collections.Generic;
