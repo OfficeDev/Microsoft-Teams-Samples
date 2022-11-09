628607afd87233fd0eac8eee034ac9609a940c96
Keshav Sharma <v-keshsharma@microsoft.com>
2022-11-09 14:42:48 +0530

    commit for tab-sso and tab-personal-sso-quickstart

--- samples/tab-personal-sso-quickstart/csharp_dotnetcore/Program.cs
+++ samples/tab-personal-sso-quickstart/csharp_dotnetcore/Program.cs
@@ -2,23 +2,66 @@
 // Copyright (c) Microsoft. All Rights Reserved.
 // </copyright>
 
-namespace TeamsAuthSSO
+
+using System;
+using Microsoft.AspNetCore.Authentication.AzureAD.UI;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using TeamsTabSSO.Helper;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddHttpClient("WebClient", client => client.Timeout = TimeSpan.FromSeconds(600));
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddAuthentication(options =>
 {
-    using Microsoft.AspNetCore.Hosting;
-    using Microsoft.Extensions.Hosting;
-
-    public class Program
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+    .AddJwtBearer(options =>
     {
-        public static void Main(string[] args)
+        var azureAdOptions = new AzureADOptions();
+        builder.Configuration.Bind("AzureAd", azureAdOptions);
+        options.Authority = $"{azureAdOptions.Instance}{azureAdOptions.TenantId}/v2.0";
+        options.TokenValidationParameters = new TokenValidationParameters
         {
-            CreateHostBuilder(args).Build().Run();
-        }
+            ValidAudiences = SSOAuthHelper.GetValidAudiences(builder.Configuration),
+            ValidIssuers = SSOAuthHelper.GetValidIssuers(builder.Configuration),
+            AudienceValidator = SSOAuthHelper.AudienceValidator
+        };
+    });
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
 }
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+});
+app.Run();
--- samples/tab-sso/csharp/TeamsTabSSO/Program.cs
+++ samples/tab-sso/csharp/TeamsTabSSO/Program.cs
@@ -1,24 +1,59 @@
-// <copyright file="Program.cs" company="Microsoft">
-// Copyright (c) Microsoft. All Rights Reserved.
-// </copyright>
+using Microsoft.AspNetCore.Authentication.AzureAD.UI;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using TeamsTabSSO.Helper;
 
-namespace TeamsAuthSSO
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddHttpClient("WebClient", client => client.Timeout = TimeSpan.FromSeconds(600));
+builder.Services.AddHttpContextAccessor();
+builder.Services.AddAuthentication(options =>
 {
-    using Microsoft.AspNetCore.Hosting;
-    using Microsoft.Extensions.Hosting;
-
-    public class Program
+    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+    .AddJwtBearer(options =>
     {
-        public static void Main(string[] args)
+        var azureAdOptions = new AzureADOptions();
+        builder.Configuration.Bind("AzureAd", azureAdOptions);
+        options.Authority = $"{azureAdOptions.Instance}{azureAdOptions.TenantId}/v2.0";
+        options.TokenValidationParameters = new TokenValidationParameters
         {
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+            ValidAudiences = SSOAuthHelper.GetValidAudiences(builder.Configuration),
+            ValidIssuers = SSOAuthHelper.GetValidIssuers(builder.Configuration),
+            AudienceValidator = SSOAuthHelper.AudienceValidator
+        };
+    });
+var app = builder.Build();
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
 }
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllerRoute(
+        name: "default",
+        pattern: "{controller=Home}/{action=Index}/{id?}");
+});
+app.Run();
