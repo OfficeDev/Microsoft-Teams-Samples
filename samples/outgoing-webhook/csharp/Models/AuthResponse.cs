43313e0520cc74bfe27a05cdd36cdc05cad78b16
ChetanSharma-MSFT <59052547+ChetanSharma-msft@users.noreply.github.com>
2022-12-04 18:03:54 +0530

    Migrate samples from .Net Framework to .Net 6.0 (#608)
    * Migrated OutGoing sample from .Net Framework to .Net 6.0
    
    * Update Readme
    
    * Readme Update
    
    * Migrate samples from .Net Framework to .Net 6.0  - app-complete-sample
    
    * Update Readme - app-complete-sample
    
    * Update Readme - app-complete-sample
    
    Co-authored-by: Harikrishnan Rajandiran <v-hrajandira@microsoft.com>

---
+++ samples/outgoing-webhook/csharp/Models/AuthResponse.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+
+namespace WebhookSampleBot.Models
+{
+    /// <summary>
+    /// Encapsulates auth results.
+    /// </summary>
+    public class AuthResponse
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="AuthResponse"/> class.
+        /// </summary>
+        /// <param name="authSuccessful">if set to <c>true</c> then [authentication was successful].</param>
+        /// <param name="errorMessage">The error message.</param>
+        public AuthResponse(bool authSuccessful, string errorMessage)
+        {
+            this.AuthSuccessful = authSuccessful;
+            this.ErrorMessage = errorMessage;
+        }
+
+        /// <summary>
+        /// Gets a value indicating whether [authentication successful].
+        /// </summary>
+        /// <value>
+        /// <c>true</c> if [authentication successful]; otherwise, <c>false</c>.
+        /// </value>
+        public bool AuthSuccessful { get; private set; }
+
+        /// <summary>
+        /// Gets the error message.
+        /// </summary>
+        /// <value>
+        /// The error message.
+        /// </value>
+        public string ErrorMessage { get; private set; }
+    }
+}
