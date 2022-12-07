173f91877ac7abfe6f4268fc654e879410fc0842
Mohammed Furquan <v-mfurquan@microsoft.com>
2022-12-07 12:05:11 +0530

    External PR Comments Fixed

--- samples/app-cache-meetings/nodejs/src/components/app-cache-tab.tsx
+++ samples/app-cache-meetings/nodejs/src/components/app-cache-tab.tsx
@@ -11,26 +11,24 @@ import * as microsoftTeams from "@microsoft/teams-js";
 /// function logitem
 /// </summary>
 function logItem(action: string, actionColor: string, message: string) {
-  const newItem =
-    "<span style='font-weight:bold;color:" +
-    actionColor +
-    "'>" +
-    action +
-    "</span> " +
-    message +
-    "</br>";
-  return newItem;
+    const newItem =
+        "<span style='font-weight:bold;color:" +
+        actionColor +
+        "'>" +
+        action +
+        "</span> " +
+        message +
+        "</br>";
+    return newItem;
 }
 
 /// </summary>
 /// In beforeUnloadHandler using setItems and readyToUnload callback function
 /// </summary>
 const beforeUnloadHandler = (
-  readyToUnload: () => void) => {
-  console.log("sending readyToUnload to TEAMS");
-  readyToUnload();
-
-  return true;
+    readyToUnload: () => void) => {
+    readyToUnload();
+    return true;
 };
 
 /// </summary>
@@ -48,65 +46,54 @@ const loadHandler = (
 };
 
 const AppCacheTab = () => {
-  const [items, setItems] = useState<string[]>([]);
-  const [title, setTitle] = useState("App Cache Testing Sample");
-  const [initState] = useState(true);
+    const [items, setItems] = useState<string[]>([]);
+    const [title] = useState("App Cache Testing Sample");
+    const [initState] = useState(true);
 
-  React.useEffect(() => {
-    if (!initState) {
-      return;
-    }
-
-    microsoftTeams.app.initialize().then(() => {
-      microsoftTeams.app.getContext().then((context) => {
-
-        const newItem = logItem("Success", "green", "Loaded Teams context");
-        setItems((Items) => [...Items, newItem]);
-        setTitle(title);
-
-        const newLogItem = logItem("FrameContext", "orange", "Frame context is " + context.page.frameContext);
-        setItems((Items) => [...Items, newLogItem]);
-
-        if (context.page.frameContext === "sidePanel") {
-          microsoftTeams.teamsCore.registerBeforeUnloadHandler((readyToUnload) => {
-            const result = beforeUnloadHandler(readyToUnload);
-            return result;
-          });
-
-          microsoftTeams.teamsCore.registerOnLoadHandler((data) => {
-            loadHandler(setItems, data);
-          });
-
-          const newItem = logItem("Handlers", "orange", "Registered load and before unload handlers. Ready for app caching.");
-          setItems((Items) => [...Items, newItem]);
+    React.useEffect(() => {
+        if (!initState) {
+            return;
         }
 
-        else {
-          let newItem = logItem("ERROR", "red", "could not get context");
-          setItems((Items) => [...Items, newItem]);
-        }
+        microsoftTeams.app.initialize().then(() => {
 
-      });
-    });
+            try {
+                microsoftTeams.teamsCore.registerBeforeUnloadHandler((readyToUnload) => {
+                    const result = beforeUnloadHandler(readyToUnload);
+                    return result;
+                });
 
-    return () => {
-      console.log("useEffect cleanup - Tab");
-    };
+                microsoftTeams.teamsCore.registerOnLoadHandler((data) => {
+                    loadHandler(setItems, data);
+                    console.log(data.contentUrl, data.entityId);
+                });
 
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [initState]);
+                const newItem = logItem("Handlers", "orange", "Registered load and before unload handlers. Ready for app caching.");
+                setItems((Items) => [...Items, newItem]);
 
-  const jsx = initState ? (
-    <div>
-      <h3>{title}</h3>
-      {items.map((item) => {
-        return <div dangerouslySetInnerHTML={{ __html: item }} />;
-      })}
-    </div>
-  ) : (
-    <div style={{ color: "white" }}>loading</div>
-  );
-  return jsx;
+            }
+            catch (error) {
+                console.log(error, "could not registered handlers");
+            }
+        });
+
+        return () => {
+            console.log("useEffect cleanup - Tab");
+        };
+
+    }, [initState]);
+
+    const jsx = initState ? (
+        <div>
+            <h3>{title}</h3>
+            {items.map((item) => {
+                return <div dangerouslySetInnerHTML={{ __html: item }} />;
+            })}
+        </div>
+    ) : (
+        <div style={{ color: "white" }}>loading</div>
+    );
+    return jsx;
 };
 
 export default AppCacheTab;
