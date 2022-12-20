b7c2111abad93b646efdd1c428dc9ba6ebedd4a2
Eoin O'Brien <eoobrien@microsoft.com>
2022-12-20 11:59:04 -0500

    add comments

--- samples/bot-calling-meeting/csharp/Source/CallingBotSample/Extensions/NotificationEventArgsExtenstions.cs
+++ samples/bot-calling-meeting/csharp/Source/CallingBotSample/Extensions/NotificationEventArgsExtenstions.cs
@@ -6,8 +6,16 @@ using Microsoft.Graph.Communications.Core.Notifications;
 
 namespace CallingMeetingBot.Extensions
 {
+    /// <summary>
+    /// Extensions for NotificationEventArgs
+    /// </summary>
     public static class NotificationEventArgsExtensions
     {
+        /// <summary>
+        /// Checks if an incoming notification is about participants changing
+        /// </summary>
+        /// <param name="args">The notification</param>
+        /// <returns>Whether it is a participant notification</returns>
         public static bool IsParticipantsNotification(this NotificationEventArgs args) =>
             args.ChangeType == ChangeType.Updated &&
                 args.Notification.ResourceUrl.Contains("/participants");
