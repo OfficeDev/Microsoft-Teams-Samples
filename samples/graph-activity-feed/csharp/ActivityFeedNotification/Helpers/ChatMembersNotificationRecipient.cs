d37a73c7ae7d8786ba8289588f88f9703e34b6b8
Pawan Koshti <v-pakoshti@microsoft.com>
2022-12-22 10:55:07 +0530

    Activity Feed notification Change and fix

---
+++ samples/graph-activity-feed/csharp/ActivityFeedNotification/Helpers/ChatMembersNotificationRecipient.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+
+namespace Microsoft.Graph
+{
+    //
+    // Summary:
+    //     The type ChatMembersNotificationRecipient.
+    [JsonObject(MemberSerialization = MemberSerialization.OptIn)]
+    public class ChatMembersNotificationRecipient : TeamworkNotificationRecipient
+    {
+        //
+        // Summary:
+        //     Gets or sets chatId.
+        [JsonProperty(NullValueHandling = NullValueHandling.Ignore, PropertyName = "chatId", Required = Required.Default)]
+        public string ChatId { get; set; }
+
+        //
+        // Summary:
+        //     Initializes a new instance of the Microsoft.Graph.chatMembersNotificationRecipient
+        //     class.
+        public ChatMembersNotificationRecipient()
+        {
+            base.ODataType = "microsoft.graph.chatMembersNotificationRecipient";
+        }
+    }
+}
