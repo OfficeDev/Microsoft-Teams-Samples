733746ca8e9ecd961fa07adb57d93e00aec7ec93
Prithvi-MSFT <v-prkamble@microsoft.com>
2023-01-01 18:12:12 +0530

    Converted auth samples to not use impicit grant flow

--- samples/app-complete-auth/csharp/AppCompleteAuth/Pages/auth-end.cshtml.cs
+++ samples/app-complete-auth/csharp/AppCompleteAuth/Pages/auth-end.cshtml.cs
@@ -1,11 +1,22 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
 
 namespace AppCompleteAuth.Pages
 {
     public class authEnd : PageModel
     {
+        private readonly IConfiguration _configuration;
+
+        public string ClientId { get; set; }
+
+        public authEnd(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         public void OnGet()
-        { 
+        {
+            ClientId = _configuration["MicrosoftAppId"];
         }
     }
 }
