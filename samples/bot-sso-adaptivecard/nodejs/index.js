a941543b62ce74c31c2a2400f2a39f1506d94f10
Mohammed Furquan <v-mfurquan@microsoft.com>
2023-01-22 09:45:43 +0530

    Comments Fixed

--- samples/bot-sso-adaptivecard/nodejs/index.js
+++ samples/bot-sso-adaptivecard/nodejs/index.js
@@ -26,7 +26,7 @@ const adapter = new BotFrameworkAdapter({
 adapter.onTurnError = async (context, error) => {
     // This check writes out errors to console log .vs. app insights.
     // NOTE: In production environment, you should consider logging this to Azure
-    //       application insights. See https://aka.ms/bottelemetry for telemetry 
+    //       application insights. See https://aka.ms/bottelemetry for telemetry
     //       configuration instructions.
     // Uncomment below commented line for local debugging.
     // await context.sendActivity("Sorry, it looks like something went wrong. Exception Caught:" error);
