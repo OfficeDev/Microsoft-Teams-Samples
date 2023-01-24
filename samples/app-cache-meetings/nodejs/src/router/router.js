b62da4499a5a704769277373e68958ee15c00eaf
Mohammed Furquan <v-mfurquan@microsoft.com>
2023-01-24 12:13:17 +0530

    Default page added In App Cache Meetings.

--- samples/app-cache-meetings/nodejs/src/router/router.js
+++ samples/app-cache-meetings/nodejs/src/router/router.js
@@ -11,12 +11,14 @@ import {
 } from 'react-router-dom';
 import Configure from "../components/configure";
 import AppCacheTab from "../components/app-cache-tab";
+import Index from "../components/index";
 
 export const AppRoute = () => {
     return (
         <React.Fragment>
             <BrowserRouter>
                 <Routes>
+                    <Route path="/" element={<Index />} />
                     <Route path="/configure" element={<Configure />}/>
                     <Route path="/appCacheTab" element={<AppCacheTab />}/>
                 </Routes>
