4e901ec2f12d1a0d30150f14bfe562fb2abfb259
Pawan Koshti <v-pakoshti@microsoft.com>
2023-04-19 13:53:22 +0530

    Upgrade teams version and up and running configurable tab

--- samples/app-hello-world/nodejs/src/static/scripts/teamsapp.js
+++ samples/app-hello-world/nodejs/src/static/scripts/teamsapp.js
@@ -1,8 +1,8 @@
-(function () {
+(async function () {
     'use strict';
 
     // Call the initialize API first
-    microsoftTeams.initialize();
+    await microsoftTeams.app.initialize();
 
     // Check the initial theme user chose and respect it
     microsoftTeams.getContext(function (context) {
@@ -10,42 +10,32 @@
             setTheme(context.theme);
         }
     });
-
+    
     // Handle theme changes
     microsoftTeams.registerOnThemeChangeHandler(function (theme) {
         setTheme(theme);
     });
 
-    // Save configuration changes
-    microsoftTeams.settings.registerOnSaveHandler(function (saveEvent) {
-        // Let the Microsoft Teams platform know what you want to load based on
-        // what the user configured on this page
-        microsoftTeams.settings.setSettings({
-            contentUrl: createTabUrl(), // Mandatory parameter
-            entityId: createTabUrl(), // Mandatory parameter
-        });
 
-        // Tells Microsoft Teams platform that we are done saving our settings. Microsoft Teams waits
-        // for the app to call this API before it dismisses the dialog. If the wait times out, you will
-        // see an error indicating that the configuration settings could not be saved.
-        saveEvent.notifySuccess();
+    microsoftTeams.pages.config.registerOnSaveHandler((saveEvent) => {
+        const configPromise = microsoftTeams.pages.config.setConfig({
+            websiteUrl: createTabUrl(),
+            contentUrl: createTabUrl(),
+            entityId: createTabUrl(),
+        });
+        configPromise.
+            then((result) => { saveEvent.notifySuccess() }).
+            catch((error) => { saveEvent.notifyFailure("failure message") });
     });
 
     // Logic to let the user configure what they want to see in the tab being loaded
-    document.addEventListener('DOMContentLoaded', function () {
-        var tabChoice = document.getElementById('tabChoice');
-        if (tabChoice) {
-            tabChoice.onchange = function () {
-                var selectedTab = this[this.selectedIndex].value;
-
-                // This API tells Microsoft Teams to enable the 'Save' button. Since Microsoft Teams always assumes
-                // an initial invalid state, without this call the 'Save' button will never be enabled.
-                microsoftTeams.settings.setValidityState(
-                    selectedTab === 'first' || selectedTab === 'second'
-                );
-            };
-        }
-    });
+    var tabChoice = document.getElementById('tabChoice');
+    tabChoice.onchange = function () {
+        var selectedTab = tabChoice[tabChoice.selectedIndex].value;
+        microsoftTeams.pages.config.setValidityState(
+            selectedTab === 'first' || selectedTab === 'second'
+        );
+    }
 
     // Set the desired theme
     function setTheme(theme) {
