f16a1985bd19c475211ccc6051db98753dea9b9f
Pawan Koshti <v-pakoshti@microsoft.com>
2023-04-25 12:33:48 +0530

    App issues fixes

--- samples/bot-all-cards/nodejs/Readme.md
+++ samples/bot-all-cards/nodejs/Readme.md
@@ -121,7 +121,8 @@ Run ngrok - point to port 3978
    - From the lower left corner, choose Upload a custom App
    - Go to your project directory, the ./AppPackage folder, select the zip folder, and choose Open.
    - Select Add in the pop-up dialog box. Your app is uploaded to Teams.
-
+   
+**Note**: If you are facing any issue in your app, please uncomment [this](https://github.com/OfficeDev/Microsoft-Teams-Samples/blob/main/samples/bot-all-cards/nodejs/index.js#L32) line and put your debugger for local debug.
 
 ## Running the sample
 
--- samples/tab-stage-view/csharp/TabInStageView/Startup.cs
+++ samples/tab-stage-view/csharp/TabInStageView/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Bot.Builder;
 using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using Microsoft.Bot.Connector.Authentication;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using TabInStageView.Bots;
@@ -28,6 +29,9 @@ namespace TabInStageView
             services.AddControllers().AddNewtonsoftJson();
 
             services.AddRazorPages();
+
+            // Create the Bot Framework Authentication to be used with the Bot Adapter.
+            services.AddSingleton<BotFrameworkAuthentication, ConfigurationBotFrameworkAuthentication>();
 
             // Create the Bot Framework Adapter with error handling enabled.
             services.AddSingleton<IBotFrameworkHttpAdapter, AdapterWithErrorHandler>();
