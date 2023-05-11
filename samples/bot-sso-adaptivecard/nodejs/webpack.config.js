778d6b31a2c251640b8e4913f5522010058b9762
Harikrishnan Rajandiran <v-hrajandira@microsoft.com>
2023-05-11 14:32:32 +0530

    Updating webpack.config

--- samples/bot-sso-adaptivecard/nodejs/webpack.config.js
+++ samples/bot-sso-adaptivecard/nodejs/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
-  mode: 'production', //add this line here
+  mode: 'production', 
   entry: './index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
