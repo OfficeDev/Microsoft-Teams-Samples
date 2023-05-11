ee17fc9368ae3279e564e7e3c6dbf4f86232ff8b
Harikrishnan Rajandiran <v-hrajandira@microsoft.com>
2023-05-11 15:01:38 +0530

    Implement webpack - bot all card sample

---
+++ samples/bot-all-cards/nodejs/webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+
+module.exports = {
+  mode: 'production', 
+  entry: './index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  target: 'node', // use require() & use NodeJs CommonJS style
+  externals: [nodeExternals()], // in order to ignore all modules in node_modules folder
+  externalsPresets: {
+      node: true // in order to ignore built-in modules like path, fs, etc. 
+  },
+};
