8dfc66f39fa3d3ca198f11f1da619d01e8bd40ea
Prithvi-MSFT <v-prkamble@microsoft.com>
2023-07-02 00:46:33 +0530

    bot-formatting-cards sample

--- samples/bot-formatting-cards/nodejs/index.js
+++ samples/bot-formatting-cards/nodejs/index.js
@@ -3,25 +3,20 @@
 
 // index.js is used to setup and configure your bot
 
-// Import required pckages
+// Import required packages
 const path = require('path');
 const express = require('express');
-
-// Read variables from .env file.
-const ENV_FILE = path.join(__dirname, '.env');
-require('dotenv').config({ path: ENV_FILE });
-const PORT = process.env.PORT || 3978;
-const server = express();
-server.use(express.json());
-server.use(express.urlencoded({
-    extended: true
-}));
 
 // Import required bot services.
 // See https://aka.ms/bot-services to learn more about the different parts of a bot.
 const { BotFrameworkAdapter } = require('botbuilder');
 
+// Import bot definitions
 const { BotFormattingCards } = require('./bots/botFormattingCards');
+
+// Read botFilePath and botFileSecret from .env file.
+const ENV_FILE = path.join(__dirname, '.env');
+require('dotenv').config({ path: ENV_FILE });
 
 // Create adapter.
 // See https://aka.ms/about-bot-adapter to learn more about adapters.
@@ -33,36 +28,35 @@ const adapter = new BotFrameworkAdapter({
 adapter.onTurnError = async (context, error) => {
     // This check writes out errors to console log .vs. app insights.
     // NOTE: In production environment, you should consider logging this to Azure
-    //       application insights. See https://aka.ms/bottelemetry for telemetry
-    //       configuration instructions.
-
-    // Uncomment below commented line for local debugging.
-    // await context.sendActivity(`Sorry, it looks like something went wrong. Exception Caught: ${error}`);
+    //       application insights.
+    console.error(`\n [onTurnError] unhandled error: ${ error }`);
 
     // Send a trace activity, which will be displayed in Bot Framework Emulator
     await context.sendTraceActivity(
         'OnTurnError Trace',
-        `${error}`,
+        `${ error }`,
         'https://www.botframework.com/schemas/error',
         'TurnError'
     );
+
+     // Uncomment below commented line for local debugging.
+     // await context.sendActivity(`Sorry, it looks like something went wrong. Exception Caught: ${error}`);    
 };
 
-// Create the bot that will handle incoming messages.
-const bot = new BotFormattingCards();
+// Create bot handlers
+const botActivityHandler = new BotFormattingCards();
 
-server.listen(PORT, () => {
-    console.log(`Server listening on http://localhost:${PORT}`);
-});
+// Create HTTP server.
+const server = express();
+const port = process.env.port || process.env.PORT || 3978;
+server.listen(port, () => 
+    console.log(`\Bot/ME service listening at http://localhost:${port}`)
+);
 
-server.use("/Images", express.static(path.resolve(__dirname, 'Images')));
-
-server.get('*', (req, res) => {
-    res.json({ error: 'Route not found' });
-});
-
+// Listen for incoming requests.
 server.post('/api/messages', (req, res) => {
     adapter.processActivity(req, res, async (context) => {
-        await bot.run(context);
+        // Process bot activity
+        await botActivityHandler.run(context);
     });
 });
