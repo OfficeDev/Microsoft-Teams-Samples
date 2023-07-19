04e2b81a16921f74bc0b19a042df470672c81e0b
Mohammed Furquan <v-mfurquan@microsoft.com>
2023-07-19 15:06:55 +0530

    Removed Commented code

--- samples/bot-sequential-flow-adaptive-cards/nodejs/server/index.js
+++ samples/bot-sequential-flow-adaptive-cards/nodejs/server/index.js
@@ -4,14 +4,8 @@ const cors = require('cors');
 const ENV_FILE = path.join(__dirname, '../.env');
 require('dotenv').config({ path: ENV_FILE });
 
-//const PORT = process.env.PORT || 3978;
 const server = express();
 
-// server.use(cors());
-// server.use(express.json());
-// server.use(express.urlencoded({
-//     extended: true
-// }));
 
 server.use('/api', require('./api'));
 
