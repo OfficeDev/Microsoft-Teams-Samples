b1a50b451c9ef43a6ed87bdbe7c4cf150da9cdfa
Harikrishnan Rajandiran <v-hrajandira@microsoft.com>
2023-07-26 12:03:36 +0530

    Add Teamsfx toolkit migration nodejs samples

--- samples/msgext-search-quickstart/js/index.js
+++ samples/msgext-search-quickstart/js/index.js
@@ -51,7 +51,7 @@ const botActivityHandler = new BotActivityHandler();
 const server = express();
 const port = process.env.port || process.env.PORT || 3978;
 server.listen(port, () => 
-    console.log(`\Bot/ME service listening at http://localhost:${port}`)
+    console.log(`Service listening at http://localhost:${port}`)
 );
 
 // Listen for incoming requests.
