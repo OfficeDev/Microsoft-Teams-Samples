03cea3477098be8d62247d493f22d955fa238b66
JhontSouth <jhonatan.sandoval@southworks.com>
2023-07-28 11:35:21 -0500

    add teams batch operations bot

---
+++ samples/bot-batch-operations/csharp/Bots/TeamsBatchOperationsBot.cs
@@ -0,0 +1,340 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using AdaptiveCards.Templating;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Teams;
+using Microsoft.Bot.Schema;
+using Microsoft.Bot.Schema.Teams;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.BotBuilderSamples.Bots
+{
+    public class TeamsBatchOperationsBot : TeamsActivityHandler
+    {
+        public TeamsBatchOperationsBot(IConfiguration config)
+        {
+        }
+
+        protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            turnContext.Activity.RemoveRecipientMention();
+            if (turnContext.Activity.Text == null)
+            {
+                await SendDataToBatchOperations(turnContext, cancellationToken);
+            }
+            else
+            {
+                var text = turnContext.Activity.Text.Trim().ToLower();
+                if (text.Contains("listusers"))
+                    await MessageListOfUsersInputAsync(turnContext, cancellationToken);               
+                else if (text.Contains("tenant"))
+                    await MessageAllUsersInTenantAsync(turnContext, cancellationToken);
+                else if (text.Contains("team"))
+                    await MessageAllUsersInTeamInputAsync(turnContext, cancellationToken);
+                else if (text.Contains("listchannels"))
+                    await MessageListOfChannelsInputAsync(turnContext, cancellationToken);
+                else if (text.Contains("state"))
+                    await GetOperationStateInputAsync(turnContext, cancellationToken);
+                else if (text.Contains("failed"))
+                    await GetFailedEntriesInputAsync(turnContext, cancellationToken);
+                else if (text.Contains("cancel"))
+                    await CancelOperationInputAsync(turnContext, cancellationToken);
+                else
+                    await CardActivityAsync(turnContext, cancellationToken);
+            }
+        }
+
+        protected override async Task OnTeamsMembersAddedAsync(IList<TeamsChannelAccount> membersAdded, TeamInfo teamInfo, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        {
+            foreach (var teamMember in membersAdded)
+            {
+                if (teamMember.Id != turnContext.Activity.Recipient.Id && turnContext.Activity.Conversation.ConversationType != "personal")
+                {
+                    await turnContext.SendActivityAsync(MessageFactory.Text($"Welcome to the team {teamMember.GivenName} {teamMember.Surname}."), cancellationToken);
+                }
+            }
+        }
+
+        protected override async Task OnInstallationUpdateActivityAsync(ITurnContext<IInstallationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        {
+            if (turnContext.Activity.Conversation.ConversationType == "channel")
+            {
+                await turnContext.SendActivityAsync($"Welcome to Microsoft Teams batch operations demo bot. This bot is configured in {turnContext.Activity.Conversation.Name}");
+            }
+            else
+            {
+                await turnContext.SendActivityAsync("Welcome to Microsoft Teams batch operations demo bot.");
+            }
+        }
+
+        private async Task CardActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+
+            var card = new HeroCard
+            {
+                Buttons = new List<CardAction>
+                        {                          
+                            new CardAction
+                            {
+                                Type = ActionTypes.MessageBack,
+                                Title = "Message list of users",
+                                Text = "listUsers"
+                            },
+                            new CardAction
+                            {
+                                Type = ActionTypes.MessageBack,
+                                Title = "Message all users in tenant",
+                                Text = "tenant"
+                            },
+                            new CardAction
+                            {
+                                Type = ActionTypes.MessageBack,
+                                Title = "Message all users in team",
+                                Text = "team"
+                            },
+                            new CardAction
+                            {
+                                Type = ActionTypes.MessageBack,
+                                Title = "Message list of channels",
+                                Text = "listChannels"
+                            },
+                            new CardAction
+                            {
+                                Type = ActionTypes.MessageBack,
+                                Title = "Get Operation state",
+                                Text = "state"
+                            },
+                            new CardAction
+                            {
+                                Type = ActionTypes.MessageBack,
+                                Title = "Get failed entries",
+                                Text = "failed"
+                            }
+                            ,new CardAction
+                            {
+                                Type = ActionTypes.MessageBack,
+                                Title = "Cancel operation",
+                                Text = "cancel"
+                            }
+                        }
+            };
+
+
+            await SendWelcomeCard(turnContext, card, cancellationToken);
+
+        }
+
+        private async Task MessageListOfUsersInputAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            await turnContext.SendActivityAsync(MessageFactory.Attachment(GetInputCard(turnContext, "MultipleInputCard.json", "user-id" )), cancellationToken);
+        }
+
+        private async Task MessageListOfUsersAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var membersList = new List<object>();
+            var tenantId = turnContext.Activity.Conversation.TenantId;
+
+            for (int i = 1; i <= 5; i++)
+            {
+                var userId = GetId(turnContext.Activity.Value, $"user-id{i}");
+                membersList.Add(new { Id = userId });
+            }
+
+            var message = MessageFactory.Text("Hello user! You are part of the batch.");
+            var operationId = await TeamsInfo.SendMessageToListOfUsersAsync(turnContext, message, membersList, tenantId, cancellationToken);
+
+            await turnContext.SendActivityAsync(MessageFactory.Text($"All messages have been sent. OperationId: {operationId}"), cancellationToken);
+        }
+
+        private async Task MessageListOfChannelsInputAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            await turnContext.SendActivityAsync(MessageFactory.Attachment(GetInputCard(turnContext, "MultipleInputCard.json", "channel-id")), cancellationToken);
+        }
+
+        private async Task MessageListOfChannelsAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var membersList = new List<object>();
+            var tenantId = turnContext.Activity.Conversation.TenantId;
+
+            for (int i = 1; i <= 5; i++)
+            {
+                var channelId = GetId(turnContext.Activity.Value, $"channel-id{i}");
+                membersList.Add(new { Id = channelId });
+            }
+
+            var message = MessageFactory.Text($"Hello channel user! You are part of the batch.");
+            var operationId = await TeamsInfo.SendMessageToListOfChannelsAsync(turnContext, message, membersList, tenantId, cancellationToken);
+
+            await turnContext.SendActivityAsync(MessageFactory.Text($"All messages have been sent. OperationId: {operationId}"), cancellationToken);
+        }
+
+        private async Task MessageAllUsersInTenantAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+
+            var tenantId = turnContext.Activity.Conversation.TenantId;
+
+            var message = MessageFactory.Text("Hello user! You received this tenant message from batch.");
+
+            var operationId = await TeamsInfo.SendMessageToAllUsersInTenantAsync(turnContext, message, tenantId, cancellationToken);
+
+            await turnContext.SendActivityAsync(MessageFactory.Text($"All messages have been sent. OperationId: {operationId}"), cancellationToken);
+        }
+
+        private async Task MessageAllUsersInTeamInputAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            await turnContext.SendActivityAsync(MessageFactory.Attachment(GetInputCard(turnContext, "InputCard.json", "team-id")), cancellationToken);
+        }
+        private async Task MessageAllUsersInTeamAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var teamId = GetId(turnContext.Activity.Value, "team-id");
+            var tenantId = turnContext.Activity.Conversation.TenantId;
+
+            var message = MessageFactory.Text("Hello user! You received this team message from batch.");
+            var operationId = await TeamsInfo.SendMessageToAllUsersInTeamAsync(turnContext, message, teamId, tenantId, cancellationToken);
+
+            await turnContext.SendActivityAsync(MessageFactory.Text($"All messages have been sent. OperationId: {operationId}"), cancellationToken);
+        }
+
+        private async Task GetOperationStateInputAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            await turnContext.SendActivityAsync(MessageFactory.Attachment(GetInputCard(turnContext, "InputCard.json", "state-operationId")), cancellationToken);
+        }
+
+        private async Task GetOperationStateAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var operationId = GetId(turnContext.Activity.Value, "state-operationId");
+            var operationState = await TeamsInfo.GetOperationStateAsync(turnContext, operationId, cancellationToken);
+            var statusResponses = "";
+
+            foreach (var statusResponse in operationState.StatusMap)
+            {
+                statusResponses += statusResponse.Key + ": " + statusResponse.Value + ", ";
+            }
+
+            var response = $"The operation was {operationState.State} with the status responses: {statusResponses} and total entries count: {operationState.TotalEntriesCount}";
+
+            await turnContext.SendActivityAsync(MessageFactory.Text(response), cancellationToken);
+        }
+
+        private async Task GetFailedEntriesInputAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            await turnContext.SendActivityAsync(MessageFactory.Attachment(GetInputCard(turnContext, "InputCard.json", "entries-operationId")), cancellationToken);
+        }
+
+        private async Task GetFailedEntriesAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var operationId = GetId(turnContext.Activity.Value, "entries-operationId");
+            string continuationToken = null;
+            List<BatchFailedEntry> failedEntries = new List<BatchFailedEntry>();
+            var message = $"This is the list of failed entries for the operation {operationId}";
+
+            do
+            {
+                var currentPage = await TeamsInfo.GetPagedFailedEntriesAsync(turnContext, operationId, continuationToken, cancellationToken);
+                continuationToken = currentPage.ContinuationToken;
+                failedEntries = failedEntries.Concat(currentPage.FailedEntries).ToList();
+            }
+            while (continuationToken != null);
+
+            foreach (var entry in failedEntries)
+            {
+                message += $"\n id: {entry.Id}, error: {entry.Error} \n\n";
+            }
+
+            await turnContext.SendActivityAsync(MessageFactory.Text(message), cancellationToken);
+        }
+
+        private async Task CancelOperationInputAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            await turnContext.SendActivityAsync(MessageFactory.Attachment(GetInputCard(turnContext, "InputCard.json", "cancel-operationId")), cancellationToken);
+        }
+
+        private async Task CancelOperationAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var operationId = GetId(turnContext.Activity.Value, "cancel-operationId");
+            var message = $"The operation with Id: {operationId} ";
+
+            try
+            {
+                await TeamsInfo.CancelOperationAsync(turnContext, operationId, cancellationToken);
+                message += "has been canceled";
+            }
+            catch (Exception ex)
+            {
+                message += $"couldn't be canceled. ex: {ex.Message}";
+            }
+
+            await turnContext.SendActivityAsync(MessageFactory.Text(message), cancellationToken);
+        }
+
+        private Attachment GetInputCard(ITurnContext<IMessageActivity> turnContext, string resourceName, string operationName)
+        {
+            string[] filepath = { ".", "Resources", resourceName };
+
+            var adaptiveCardJson = File.ReadAllText(Path.Combine(filepath));
+            if (operationName == "team-id")
+                adaptiveCardJson = adaptiveCardJson.Replace("the operation", "the team");
+            adaptiveCardJson = adaptiveCardJson.Replace("\"inputid", $"\"{operationName}");
+            
+            AdaptiveCardTemplate template = new AdaptiveCardTemplate(adaptiveCardJson);
+            var payloadData = new
+            {
+                createdBy = turnContext.Activity.From.Name
+            };
+
+            //"Expand" the template - this generates the final Adaptive Card payload
+            var cardJsonstring = template.Expand(payloadData);
+            var adaptiveCardAttachment = new Attachment()
+            {
+                ContentType = "application/vnd.microsoft.card.adaptive",
+                Content = JsonConvert.DeserializeObject(cardJsonstring),
+            };
+
+            return adaptiveCardAttachment;
+        }
+
+        private async Task SendDataToBatchOperations(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var operation = turnContext.Activity.Value.ToString();
+
+            if (operation.Contains("user"))
+                await MessageListOfUsersAsync(turnContext, cancellationToken);
+            else if (operation.Contains("channel"))
+                await MessageListOfChannelsAsync(turnContext, cancellationToken);
+            else if (operation.Contains("team"))
+                await MessageAllUsersInTeamAsync(turnContext, cancellationToken);
+            else if (operation.Contains("state"))
+                await GetOperationStateAsync(turnContext, cancellationToken);
+            else if (operation.Contains("entries"))
+                await GetFailedEntriesAsync(turnContext, cancellationToken);
+            else if (operation.Contains("cancel"))
+                await CancelOperationAsync(turnContext, cancellationToken);
+        }
+       
+        private string GetId(object source, string propName)
+        {
+            var jobject = source as JObject;
+            var jtoken = jobject?[propName];
+
+            return jtoken?.Value<string>().Trim();
+        }
+
+        private static async Task SendWelcomeCard(ITurnContext<IMessageActivity> turnContext, HeroCard card, CancellationToken cancellationToken)
+        {
+            card.Title = "Welcome!";
+
+            var activity = MessageFactory.Attachment(card.ToAttachment());
+
+            await turnContext.SendActivityAsync(activity, cancellationToken);
+        }
+    }
+}
---
+++ samples/bot-batch-operations/csharp/README.md
@@ -0,0 +1,143 @@
+---
+page_type: sample
+description: This sample app demonstrates use of different batch operations as part of the Microsoft Teams APIs.
+products:
+- office-teams
+languages:
+- csharp
+extensions:
+ contentType: samples
+ createdDate: "10/17/2019 13:38:25 PM"
+urlFragment: officedev-microsoft-teams-samples-bot-batch-operations-csharp
+---
+
+# Teams Batch Operations Bot
+
+Bot Framework v4 Batch Operations Bot sample for Teams.
+
+This bot has been created using [Bot Framework](https://dev.botframework.com). This sample demonstrates the different batch operations you can make from your bot.
+
+## Included Features
+* Bots
+* Adaptive Cards
+* Batch API calls
+
+## Prerequisites
+
+- Microsoft Teams is installed and you have an account
+- [.NET SDK](https://dotnet.microsoft.com/download) version 6.0
+- [ngrok](https://ngrok.com/) or equivalent tunnelling solution
+
+## Setup
+
+> Note these instructions are for running the sample on your local machine, the tunnelling solution is required because
+the Teams service needs to call into the bot.
+
+1) Run ngrok - point to port 3978
+
+    ```bash
+    ngrok http 3978 --host-header="localhost:3978"
+    ```
+
+1) Setup for Bot
+
+   In Azure portal, create a [Azure Bot resource](https://docs.microsoft.com/azure/bot-service/bot-service-quickstart-registration).
+    - For bot handle, create a name.
+    - Select "Use existing app registration" (Create the app registration in Azure Active Directory beforehand.)
+    - __*If you don't have an Azure account*__ create an [Azure free account here](https://azure.microsoft.com/free/)
+    
+   In the new Azure Bot resource in the Portal, 
+    - Ensure that you've [enabled the Teams Channel](https://learn.microsoft.com/azure/bot-service/channel-connect-teams?view=azure-bot-service-4.0)
+    - In Settings/Configuration/Messaging endpoint, enter the current `https` URL you were given by running ngrok. Append with the path `/api/messages`
+
+1) Clone the repository
+
+    ```bash
+    git clone https://github.com/OfficeDev/Microsoft-Teams-Samples.git
+    ```
+
+1) If you are using Visual Studio
+   - Launch Visual Studio
+   - File -> Open -> Project/Solution
+   - Navigate to `samples/bot-batch-operations/csharp` folder
+   - Select `TeamsBatchOperationsBot.csproj` or `TeamsBatchOperationsBot.sln`file
+
+1) Update the `appsettings.json` configuration for the bot to use the MicrosoftAppId, MicrosoftAppPassword, MicrosoftAppTenantId generated in Step 2 (App Registration creation). (Note the App Password is referred to as the "client secret" in the azure portal and you can always create a new client secret anytime.)
+    - Also, set MicrosoftAppType in the `appsettings.json`. (**Allowed values are: MultiTenant(default), SingleTenant, UserAssignedMSI**)
+
+1) Run your bot, either from Visual Studio with `F5` or using `dotnet run` in the appropriate folder.
+
+1) __*This step is specific to Teams.*__
+    - **Edit** the `manifest.json` contained in the  `TeamsAppManifest` folder to replace your Microsoft App Id (that was created when you registered your bot earlier) *everywhere* you see the place holder string `<<YOUR-MICROSOFT-APP-ID>>` (depending on the scenario the Microsoft App Id may occur multiple times in the `manifest.json`)
+    - **Edit** the `manifest.json` for `validDomains` with base Url domain. E.g. if you are using ngrok it would be `https://1234.ngrok-free.app` then your domain-name will be `1234.ngrok-free.app`.
+    - **Zip** up the contents of the `TeamsAppManifest` folder to create a `manifest.zip` (Make sure that zip file does not contains any subfolder otherwise you will get error while uploading your .zip package)
+    - **Upload** the `manifest.zip` to Teams (In Teams Apps/Manage your apps click "Upload an app". Browse to and Open the .zip file. At the next dialog, click the Add button.)
+    - Add the app to personal/team/groupChat scope (Supported scopes)
+
+**Note**: If you are facing any issue in your app, please uncomment [this](https://github.com/OfficeDev/Microsoft-Teams-Samples/blob/main/samples/bot-batch-operations/csharp/AdapterWithErrorHandler.cs#L25) line and put your debugger for local debug.
+
+## Running the sample
+
+You can interact with this bot in Teams by sending it a message, or selecting a command from the command list. The bot will respond to the following strings.
+
+1. **Show Welcome**
+  - **Result:** The bot will send the welcome card for you to interact with
+  - **Valid Scopes:** personal, group chat, team chat
+    
+![image](Images/WelcomeCard.png)
+
+2. **Message a List of Users**
+- **Result:** The bot will send a message to each corresponding user from the list of user IDs provided in the dialog and return the operation ID.
+- **Valid Scopes:** personal, group chat, team chat
+
+![image](Images/MessageAListOfUsers.png)
+
+3. **Send message to all users in a tenant**
+- **Result:** The bot will send a 1-on-1 message to each user in the current tenant and return the operation ID
+- **Valid Scopes:** personal, group chat, team chat
+
+![image](Images/MessageAllUsersInTenant.png)
+
+4. **Message All Users in a Team:**
+- **Result:** The bot will send a 1-on-1 message to each user in the team corresponding to the ID provided in the dialog and return the operation ID
+- **Valid Scopes:** personal, group chat, team chat
+
+![image](Images/MessageAllUsersInTeam.png)
+
+5. **Message A List of Channels**
+- **Result:** The bot will send a message to each corresponding channel from the list of channel IDs provided in the dialog (by default the current channel) abd return the operation ID.
+- **Valid Scopes:** personal, group chat, team chat
+
+![image](Images/MessageAListOfChannels.png)
+
+6. **Get Operation State**
+- **Result:** The bot will open a dialog and ask for the operation ID and return if the corresponding operation is Ongoing, Completed, or Failed
+- **Valid Scopes:** personal, group chat, team chat
+
+![image](Images/GetOperationState.png)
+
+7. **Get Failed Entries Paginated**
+- **Result:** The bot will show a list of the failed entries, and provide the continuation token to see multiple pages
+- **Valid Scopes:** personal, group chat, team chat
+
+![image](Images/GetFailedEntries.png)
+
+8. **Cancel Operation**
+- **Result:** The bot will open a dialog and ask for the operation ID and cancel the corresponding operation.
+- **Valid Scopes:** personal, group chat, team chat
+
+![image](Images/CancelOperation.png)
+
+You can select an option from the command list by typing ```@TeamsBatchOperationsBot``` into the compose message area and ```What can I do?``` text above the compose area.
+
+## Deploy the bot to Azure
+
+To learn more about deploying a bot to Azure, see [Deploy your bot to Azure](https://aka.ms/azuredeployment) for a complete list of deployment instructions.
+
+## Further reading
+
+- [Bot Framework Documentation](https://docs.botframework.com)
+- [Bot Basics](https://docs.microsoft.com/azure/bot-service/bot-builder-basics?view=azure-bot-service-4.0)
+- [Azure Bot Service Introduction](https://docs.microsoft.com/azure/bot-service/bot-service-overview-introduction?view=azure-bot-service-4.0)
+- [Azure Bot Service Documentation](https://docs.microsoft.com/azure/bot-service/?view=azure-bot-service-4.0)
+- [Messages in bot conversations](https://learn.microsoft.com/microsoftteams/platform/bots/how-to/conversations/conversation-messages?tabs=dotnet)
