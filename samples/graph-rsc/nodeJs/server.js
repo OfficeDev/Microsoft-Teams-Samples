a17eb5d47d155b8813840ac5e8aa7e768f10be63
Prithvi-MSFT <v-prkamble@microsoft.com>
2023-10-03 10:13:55 +0530

    changed api calls to use access token at server side

--- samples/graph-rsc/nodeJs/server.js
+++ samples/graph-rsc/nodeJs/server.js
@@ -17,6 +17,9 @@ app.set('views', __dirname);
 const ENV_FILE = path.join(__dirname, '.env');
 require('dotenv').config({ path: ENV_FILE });
 
+var token;
+var recipientId;
+
 app.use('/', indexRouter);
 
 app.get('/configure', function (req, res) {
@@ -31,11 +34,83 @@ app.get('/rscdemo', function (req, res) {
 });
 
 app.get('/sendNotification', function (req, res) {
-  var tenantId = process.env.TenantId
-  auth.getAccessToken(tenantId).then(async function (token) {
-    res.render('./views/sendNotification', { token: JSON.stringify(token) });
-  });
+  var tenantId = process.env.TenantId;
+    res.render('./views/sendNotification');
 });
+
+app.post('/sendFeedNotification', function (req, res) {
+    recipientId = req.data.reciepientUserId;
+    token = auth.getAccessToken(tenantId).then(async function (token) {
+    await getInstalledAppList(token, recipientId);
+  })
+});
+
+// Get installed app id.
+function getAppId(appList) {
+  var list = appList;
+  var i;
+  for (i = 0; i < list.length; i++) {
+    if (list[i].teamsAppDefinition['displayName'] == "RSC-GraphAPI NodeJs") {
+      return list[i].id;
+    }
+  }
+}
+
+// Fetch the list of installed apps for user
+async function getInstalledAppList(accessToken, reciepientUserId) {
+  $.ajax({
+    url: "https://graph.microsoft.com/v1.0/users/" + reciepientUserId + "/teamwork/installedApps/?$expand=teamsAppDefinition",
+    type: "GET",
+    beforeSend: function (request) {
+      request.setRequestHeader("Authorization", "Bearer " + accessToken);
+    },
+    success: function (result) {
+      var appId = getAppId(result.value);
+      sendActivityFeedNotification(token, reciepientUserId, appId)
+    },
+    error: function (xhr, textStatus, errorThrown) {
+      console.log("textStatus: " + textStatus + ", errorThrown:" + errorThrown);
+    },
+  });
+}
+
+// Send activity feed notification to user
+async function sendActivityFeedNotification(accessToken, reciepientUserId, appId) {
+
+  var postData = {
+    topic: {
+      source: "entityUrl",
+      value: `https://graph.microsoft.com/beta/users/${reciepientUserId}/teamwork/installedApps/${appId}`
+    },
+    activityType: "taskCreated",
+    previewText: {
+      content: "New Task Created"
+    },
+    templateParameters: [
+      {
+        name: "taskName",
+        value: "test"
+      }
+    ]
+  };
+
+  $.ajax({
+    url: `https://graph.microsoft.com/beta/users/${reciepientUserId}/teamwork/sendActivityNotification`,
+    type: "POST",
+    contentType: 'application/json',
+    data: JSON.stringify(postData),
+    beforeSend: function (request) {
+      request.setRequestHeader("Authorization", "Bearer " + accessToken);
+    },
+    success: function (profile) {
+      console.log(profile);
+    },
+    error: function (xhr, textStatus, errorThrown) {
+      console.log("textStatus: " + textStatus + ", errorThrown:" + errorThrown);
+    },
+  });
+}
+
 
 app.listen(3978 || 3978, function () {
   console.log('app listening on port 3978!');
