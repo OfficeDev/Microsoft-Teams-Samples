531ee35a86f4fe22343cd12e2547f414afdff2c9
Mohammed Furquan <v-mfurquan@microsoft.com>
2023-10-10 13:37:16 +0530

    Meeting autdio State - Theme handler

--- samples/meetings-audio-state/nodejs/src/components/ToggleAudioCall.jsx
+++ samples/meetings-audio-state/nodejs/src/components/ToggleAudioCall.jsx
@@ -1,58 +1,85 @@
-import React, { Component } from "react";
-import { Flex, FlexItem, Button } from "@fluentui/react-northstar";
+import React, { useState } from "react";
+import { Button } from "@fluentui/react-northstar";
 import * as microsoftTeams from "@microsoft/teams-js";
 import "../style/style.css";
 
-class ToggleAudioCall extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            errorCode: "",
-            result: ""
-        }
-    }
+const ToggleAudioCall = () => {
+    let app = microsoftTeams.app;
+    app.initialize();
 
-    componentDidMount() {
-        microsoftTeams.app.initialize();
-    }
+    const [appTheme, setAppTheme] = useState("");
+    const [result, setResult] = useState(false);
 
-    callback = (errcode, bln) => {
+    const callback = (errcode, bln) => {
         if (errcode) {
-            this.setState({ errorCode: JSON.stringify(errcode) })
+            setResult(JSON.stringify(errcode))
         }
         else {
-            this.setState({ result: JSON.stringify(bln) })
+            setResult(JSON.stringify(bln))
         }
     }
+
     /// <summary>
     /// This method getIncomingClientAudioState returns the current state of client audio.
     /// The incoming audio is muted if the result is true and unmuted if the result is false.
     /// </summary>
-    ClientAudioState = () => {
-        microsoftTeams.meeting.getIncomingClientAudioState(this.callback);
+    const ClientAudioState = () => {
+        microsoftTeams.meeting.getIncomingClientAudioState(callback);
     }
 
     /// <summary>
     /// This method toggleIncomingClientAudio which toggles mute/unmute to client audio.
     /// Setting for the meeting user from mute to unmute or vice-versa.
     /// </summary>
-    toggleState = () => {
-        microsoftTeams.meeting.toggleIncomingClientAudio(this.callback);
+    const toggleState = () => {
+        console.log("callback---->", callback);
+        microsoftTeams.meeting.toggleIncomingClientAudio(callback);
     }
 
-    render() {
-        return (
-            <Flex>
-                <FlexItem push>
-                    <div className="tag-container">
-                        <h3>Mute/Unmute Audio Call </h3>
-                        <Button primary content="Mute/Un-Mute" onClick={this.toggleState} />
-                        <li className="break"> Mute State : <b>{this.state.result}</b></li>
-                    </div>
-                </FlexItem>
-            </Flex>
-        )
-    }
+    React.useEffect(() => {
+        app.initialize().then(app.getContext).then((context) => {
+            app.notifySuccess();
+
+            // Applying default theme from app context property
+            let defaultTheme = context.app.theme;
+
+            switch (defaultTheme) {
+                case 'dark':
+                    setAppTheme("theme-dark");
+                    break;
+                default:
+                    setAppTheme('theme-light');
+            }
+
+            // Handle app theme when 'Teams' theme changes
+            microsoftTeams.app.registerOnThemeChangeHandler(function (theme) {
+                switch (theme) {
+                    case 'dark':
+                        setAppTheme('theme-dark');
+                        break;
+                    case 'default':
+                        setAppTheme('theme-light');
+                        break;
+                    case 'contrast':
+                        setAppTheme('theme-contrast');
+                        break;
+                    default:
+                        return setAppTheme('theme-light');
+                }
+            });
+        }).catch(function (error) {
+            console.log(error, "Could not register handlers.");
+        });
+
+    }, [])
+
+    return (
+        <div className={appTheme}>
+            <h3>Mute/Unmute Audio Call </h3>
+            <Button primary content="Mute/Un-Mute" onClick={toggleState} />
+            <li className="break"> Mute State : <b>{result}</b></li>
+        </div>
+    );
 }
 
 export default ToggleAudioCall
