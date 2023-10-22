588dccf081e6c9d329ce362ab3dc39397837a1cc
Mohammed Furquan <v-mfurquan@microsoft.com>
2023-10-23 00:30:37 +0530

    Sentiment Analysis Csharp

---
+++ samples/msgext-ai-sentiment-analysis/csharp/MEAISentimentAnalysis/Pages/SentimentModel.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MEAISentimentAnalysis.Pages
+{
+    public class SentimentModel: PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
