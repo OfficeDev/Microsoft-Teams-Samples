3d80a19fe37f5f350814e68b70bac83a9a934933
ChetanSharma-MSFT <59052547+ChetanSharma-msft@users.noreply.github.com>
2023-10-25 18:04:44 +0530

    Auth fix

--- samples/msgext-search-plugin/nodejs/index.js
+++ samples/msgext-search-plugin/nodejs/index.js
@@ -28,7 +28,8 @@ const { TeamsMessagingExtensionsSearchAuthConfigBot } = require('./bots/teamsMes
 const credentialsFactory = new ConfigurationServiceClientCredentialFactory({
   MicrosoftAppId: process.env.MicrosoftAppId,
   MicrosoftAppPassword: process.env.MicrosoftAppPassword,
-  MicrosoftAppType: "MultiTenant"
+  MicrosoftAppType: "SingleTenant",
+  MicrosoftAppTenantId: process.env.TenantId
 });
 
 const botFrameworkAuthentication = new ConfigurationBotFrameworkAuthentication(
