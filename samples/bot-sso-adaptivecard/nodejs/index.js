b1491d9bec3737ae6d4043d2db31288801c09194
Harikrishnan Rajandiran <v-hrajandira@microsoft.com>
2023-11-16 17:21:11 +0530

    Convert from BotFrameworkAdapter to CloudAdapter[bot-sso-adaptivecard] Sample

--- samples/bot-sso-adaptivecard/nodejs/index.js
+++ samples/bot-sso-adaptivecard/nodejs/index.js
@@ -3,34 +3,35 @@
 
 // index.js is used to setup and configure your bot
 
-// Import required packages
+// Import required pckages
 const path = require('path');
-const express = require('express');
-
-// Import required bot services.
-// See https://aka.ms/bot-services to learn more about the different parts of a bot.
-const { BotFrameworkAdapter } = require('botbuilder');
-
-// Import bot definitions
-
-const { BotSSOAdativeCard } = require('./bots/botSSOAdaptiveCard');
 
 // Read botFilePath and botFileSecret from .env file.
 const ENV_FILE = path.join(__dirname, '.env');
 require('dotenv').config({ path: ENV_FILE });
 
+const restify = require('restify');
+
+// Import required bot services.
+// See https://aka.ms/bot-services to learn more about the different parts of a bot.
+const {
+    CloudAdapter,
+    ConfigurationBotFrameworkAuthentication
+} = require('botbuilder');
+
+const { BotSSOAdativeCard } = require('./bots/botSSOAdaptiveCard');
+
+const botFrameworkAuthentication = new ConfigurationBotFrameworkAuthentication(process.env);
+
 // Create adapter.
-// See https://aka.ms/about-bot-adapter to learn more about adapters.
-const adapter = new BotFrameworkAdapter({
-    appId: process.env.MicrosoftAppId,
-    appPassword: process.env.MicrosoftAppPassword,
-    connectionName :process.env.ConnectionName
-});
+// See https://aka.ms/about-bot-adapter to learn more about how bots work.
+const adapter = new CloudAdapter(botFrameworkAuthentication);
 
 adapter.onTurnError = async (context, error) => {
     // This check writes out errors to console log .vs. app insights.
     // NOTE: In production environment, you should consider logging this to Azure
-    //       application insights.
+    //       application insights. See https://aka.ms/bottelemetry for telemetry
+    //       configuration instructions.
     console.error(`\n [onTurnError] unhandled error: ${ error }`);
 
     // Send a trace activity, which will be displayed in Bot Framework Emulator
@@ -41,31 +42,24 @@ adapter.onTurnError = async (context, error) => {
         'TurnError'
     );
 
-     // Uncomment below commented line for local debugging.
-     // await context.sendActivity(`Sorry, it looks like something went wrong. Exception Caught: ${error}`);    
+    // Uncomment below commented line for local debugging.
+    // await context.sendActivity(`Sorry, it looks like something went wrong. Exception Caught: ${error}`);
+    
 };
 
-// Create bot handlers
-const botActivityHandler = new BotSSOAdativeCard();
+// Create the bot that will handle incoming messages.
+const bot = new BotSSOAdativeCard();
 
 // Create HTTP server.
-const server = express();
-const port = process.env.port || process.env.PORT || 3978;
+const server = restify.createServer();
+server.use(restify.plugins.bodyParser());
 
-server.listen(port, () => 
-    console.log(`\Bot/ME service listening at http://localhost:${port}`)
-);
-
-server.use("/Images", express.static(path.resolve(__dirname, 'Images')));
-
-server.get('*', (req, res) => {
-    res.json({ error: 'Route not found' });
+server.listen(process.env.port || process.env.PORT || 3978, function() {
+    console.log(`Server listening on http://localhost:${ server.url }`);
 });
 
 // Listen for incoming requests.
-server.post('/api/messages', (req, res) => {
-    adapter.processActivity(req, res, async (context) => {
-        // Process bot activity
-        await botActivityHandler.run(context);
-    });
+server.post('/api/messages', async (req, res) => {
+    // Route received a request to adapter for processing
+    await adapter.process(req, res, (context) => bot.run(context));
 });
