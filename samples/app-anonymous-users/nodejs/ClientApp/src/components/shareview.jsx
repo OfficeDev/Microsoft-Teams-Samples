c1339a0b554d461c735c93882392b6e2c0cfc3ab
Harikrishnan Rajandiran <v-hrajandira@microsoft.com>
2024-02-07 10:26:54 +0530

    Fixed app-anonymous-users nodejs sample issues

--- samples/app-anonymous-users/nodejs/ClientApp/src/components/shareview.jsx
+++ samples/app-anonymous-users/nodejs/ClientApp/src/components/shareview.jsx
@@ -118,7 +118,7 @@ const ShareView = () => {
     const facebookLogin = () => {
         fbAuthentication() // This method get a client-side token for Facebook
             .then((result) => {
-                return getFacebookProfileName(result.idToken); // This method get a face book user profile details.
+                return getFacebookProfileName(result); // This method get a face book user profile details.
             })
             .catch((error) => {
                 console.log(error);
@@ -129,9 +129,8 @@ const ShareView = () => {
     const fbAuthentication = () => {
         var facebookAppId = process.env.REACT_APP_FACEBOOK_APP_ID;
         let redirectUri = window.location.origin + "/facebook-auth-end";
-        let state = Math.random().toString(36).substring(2, 7);
-        localStorage.setItem("simple.state", state);
-
+        let state = "ValidateState";
+       
         return new Promise((resolve, reject) => {
             microsoftTeams.authentication.authenticate({
                 url: `https://www.facebook.com/v12.0/dialog/oauth?client_id=${facebookAppId}&redirect_uri=${redirectUri}&state=${state}`,
@@ -139,10 +138,10 @@ const ShareView = () => {
                 height: 535
             })
             .then((result) => {
-                let data = localStorage.getItem(result);
-                let tokenDetails = JSON.parse(data);
-                localStorage.removeItem(result);
-                resolve(tokenDetails);
+                //let data = localStorage.getItem(result);
+                //let tokenDetails = JSON.parse(data);
+                //localStorage.removeItem(result);
+                resolve(result);
             })
             .catch((reason) => {
                 reject(reason);
