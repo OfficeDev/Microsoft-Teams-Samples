f3f39fff4da6e96c9788168773183e7bdbee2369
Harikrishnan Rajandiran <v-hrajandira@microsoft.com>
2024-07-15 16:46:02 +0530

    msgext-ai-doc-compliance-checker folder name updated

---
+++ samples/msgext-ai-doc-compliance-checker/src/config.js
@@ -0,0 +1,12 @@
+const config = {
+  botId: process.env.BOT_ID,
+  botPassword: process.env.BOT_PASSWORD,
+  endpoint: process.env.END_POINT,
+  apiKey: process.env.API_KEY,
+  deploymentId: process.env.DEPLOYMENT_ID,
+  azure_Storage_Connection_String: process.env.AZURE_STORAGE_CONNECTION_STRING,
+  containerName: process.env.CONTAINER_NAME,
+  CheckListFileName: process.env.CHECKLIST_NAME
+};
+
+module.exports = config;
