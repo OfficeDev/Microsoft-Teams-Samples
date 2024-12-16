0d27692d5ea738f4a4909f69402cb3c48e240f57
ChetanSharma-MSFT <59052547+ChetanSharma-msft@users.noreply.github.com>
2024-12-17 03:34:50 +0530

    Coding standards changes

--- samples/app-hello-world/csharp/Microsoft.Teams.Samples.HelloWorld.Web/Controllers/BotController.cs
+++ samples/app-hello-world/csharp/Microsoft.Teams.Samples.HelloWorld.Web/Controllers/BotController.cs
@@ -1,36 +1,48 @@
+// <copyright file="BotController.cs" company="Microsoft Corporation">
 // Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-//
-// Generated with Bot Builder V4 SDK Template for Visual Studio EchoBot v4.6.2
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Bot.Builder;
-using Microsoft.Bot.Builder.Integration.AspNet.Core;
+// </copyright>
 
+// Generated with Bot Builder V4 SDK Template for Visual Studio EchoBot v4.6.2
 namespace Microsoft.Teams.Samples.HelloWorld.Web.Controllers
 {
-    // This ASP Controller is created to handle a request. Dependency Injection will provide the Adapter and IBot
-    // implementation at runtime. Multiple different IBot implementations running at different endpoints can be
-    // achieved by specifying a more specific type for the bot constructor argument.
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Bot.Builder;
+    using Microsoft.Bot.Builder.Integration.AspNet.Core;
+
+    /// <summary>
+    /// This ASP Controller is created to handle a request. Dependency Injection will provide the Adapter and IBot
+    /// implementation at runtime. Multiple different IBot implementations running at different endpoints can be
+    /// achieved by specifying a more specific type for the bot constructor argument.
+    /// </summary>
     [Route("api/messages")]
     [ApiController]
     public class BotController : ControllerBase
     {
-        private readonly IBotFrameworkHttpAdapter Adapter;
-        private readonly IBot Bot;
+        private readonly IBot bot;
+        private readonly IBotFrameworkHttpAdapter adapter;
 
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BotController"/> class.
+        /// </summary>
+        /// <param name="adapter">The bot framework HTTP adapter.</param>
+        /// <param name="bot">The bot instance.</param>
         public BotController(IBotFrameworkHttpAdapter adapter, IBot bot)
         {
-            Adapter = adapter;
-            Bot = bot;
+            this.adapter = adapter;
+            this.bot = bot;
         }
 
+        /// <summary>
+        /// Handles the HTTP POST request.
+        /// </summary>
+        /// <returns>A task that represents the work queued to execute.</returns>
         [HttpPost]
         public async Task PostAsync()
         {
             // Delegate the processing of the HTTP POST to the adapter.
             // The adapter will invoke the bot.
-            await Adapter.ProcessAsync(Request, Response, Bot);
+            await this.adapter.ProcessAsync(this.Request, this.Response, this.bot);
         }
     }
 }
