d602711fcb73b8b492e9c2b521a42b597cb4cfca
Harikrishnan Rajandiran <v-hrajandira@microsoft.com>
2025-02-25 11:25:53 +0530

    Implement Managed Identity Support and Toolkit Deployment Code - Nodejs Sample

--- samples/bot-message-reaction/nodejs/index.js
+++ samples/bot-message-reaction/nodejs/index.js
@@ -1,70 +1,71 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
 // index.js is used to setup and configure your bot
 
-// Import required pckages
-const path = require('path');
-
-// Read botFilePath and botFileSecret from .env file.
-const ENV_FILE = path.join(__dirname, '.env');
-require('dotenv').config({ path: ENV_FILE });
-
-const restify = require('restify');
+// Import required packages
+const express = require("express");
 
 // Import required bot services.
 // See https://aka.ms/bot-services to learn more about the different parts of a bot.
 const {
-    CloudAdapter,
-    MemoryStorage,
-    ConfigurationBotFrameworkAuthentication
-} = require('botbuilder');
+  CloudAdapter,
+  MemoryStorage,
+  ConfigurationServiceClientCredentialFactory,
+  ConfigurationBotFrameworkAuthentication,
+} = require("botbuilder");
 
 const { ActivityLog } = require('./activityLog');
-const { MessageReactionBot } = require('./bots/messageReactionBot');
-
-const botFrameworkAuthentication = new ConfigurationBotFrameworkAuthentication(process.env);
+const { TeamsBot } = require("./teamsBot");
+const config = require("./config");
 
 // Create adapter.
-// See https://aka.ms/about-bot-adapter to learn more about how bots work.
+// See https://aka.ms/about-bot-adapter to learn more about adapters.
+const credentialsFactory = new ConfigurationServiceClientCredentialFactory(config);
+
+const botFrameworkAuthentication = new ConfigurationBotFrameworkAuthentication(
+  {},
+  credentialsFactory
+);
+
 const adapter = new CloudAdapter(botFrameworkAuthentication);
 
 adapter.onTurnError = async (context, error) => {
-    // This check writes out errors to console log .vs. app insights.
-    // NOTE: In production environment, you should consider logging this to Azure
-    //       application insights. See https://aka.ms/bottelemetry for telemetry
-    //       configuration instructions.
-    console.error(`\n [onTurnError] unhandled error: ${ error }`);
+  // This check writes out errors to console log .vs. app insights.
+  // NOTE: In production environment, you should consider logging this to Azure
+  //       application insights. See https://aka.ms/bottelemetry for telemetry
+  //       configuration instructions.
+  console.error(`\n [onTurnError] unhandled error: ${error}`);
 
-    // Send a trace activity, which will be displayed in Bot Framework Emulator
-    await context.sendTraceActivity(
-        'OnTurnError Trace',
-        `${ error }`,
-        'https://www.botframework.com/schemas/error',
-        'TurnError'
-    );
-
-    // Uncomment below commented line for local debugging..
-    // await context.sendActivity(`Sorry, it looks like something went wrong. Exception Caught: ${error}`);
+  // Only send error message for user messages, not for other message types so the bot doesn't spam a channel or chat.
+  if (context.activity.type === "message") {
+    // Send a message to the user
+    await context.sendActivity(`The bot encountered an unhandled error:\n ${error.message}`);
+    await context.sendActivity("To continue to run this bot, please fix the bot source code.");
+  }
 };
 
-// Create storage and Activity Log used for tracking sent messages.
+// Create the bot that will handle incoming messages.
+// Create the main dialog.
 const memoryStorage = new MemoryStorage();
 const activityLog = new ActivityLog(memoryStorage);
+const bot = new TeamsBot(activityLog);
 
-// Create the bot that will handle incoming messages.
-const bot = new MessageReactionBot(activityLog);
+// Create express application.
+const expressApp = express();
+expressApp.use(express.json());
 
-// Create HTTP server.
-const server = restify.createServer();
-server.use(restify.plugins.bodyParser());
-
-server.listen(process.env.port || process.env.PORT || 3978, function() {
-    console.log(`\n${ server.name } listening to ${ server.url }`);
+const server = expressApp.listen(process.env.port || process.env.PORT || 3978, () => {
+  console.log(`\nBot Started, ${expressApp.name} listening to`, server.address());
 });
 
 // Listen for incoming requests.
-server.post('/api/messages', async (req, res) => {
-    // Route received a request to adapter for processing
-    await adapter.process(req, res, (context) => bot.run(context));
+expressApp.post("/api/messages", async (req, res) => {
+  await adapter.process(req, res, async (context) => {
+    await bot.run(context);
+  });
+});
+
+// Gracefully shutdown HTTP server
+["exit", "uncaughtException", "SIGINT", "SIGTERM", "SIGUSR1", "SIGUSR2"].forEach((event) => {
+  process.on(event, () => {
+    server.close();
+  });
 });
