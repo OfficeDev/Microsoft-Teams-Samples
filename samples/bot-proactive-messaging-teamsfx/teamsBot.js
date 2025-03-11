c3e3ff16064faf903697d3b7b2f78fdd420de447
Harikrishnan Rajandiran <v-hrajandira@microsoft.com>
2025-03-11 21:25:32 +0530

    Include code Copyright and comments

--- samples/bot-proactive-messaging-teamsfx/teamsBot.js
+++ samples/bot-proactive-messaging-teamsfx/teamsBot.js
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
 const { TeamsActivityHandler, TurnContext } = require("botbuilder");
 
 require('dotenv').config({ path: './env/.env.local' });
@@ -8,18 +11,21 @@ class TeamsBot extends TeamsActivityHandler {
     
     this.conversationReferences = conversationReferences;
 
+    // Capture new conversations
     this.onConversationUpdate(async (context, next) => {
       this.addConversationReference(context.activity);
 
       await next();
     });
 
+    // Handle incoming messages
     this.onMessage(async (context, next) => {
       this.addConversationReference(context.activity);
       await context.sendActivity(`You sent '${context.activity.text}'. Navigate to ${process.env.BOT_ENDPOINT}/api/notify to proactively message everyone who has previously messaged this bot.`);
       await next();
     });
 
+    // Greet new members
     this.onMembersAdded(async (context, next) => {
       const membersAdded = context.activity.membersAdded;
       for (let cnt = 0; cnt < membersAdded.length; cnt++) {
@@ -32,6 +38,7 @@ class TeamsBot extends TeamsActivityHandler {
     });
   }
 
+  // Store conversation references for proactive messaging
   addConversationReference(activity) {
     const conversationReference = TurnContext.getConversationReference(activity);
     this.conversationReferences[conversationReference.conversation.id] = conversationReference;
