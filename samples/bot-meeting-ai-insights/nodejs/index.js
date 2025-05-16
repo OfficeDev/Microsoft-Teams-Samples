e88d50d5a1f310ac2f1812901df5ee64dc856006
Ajay Jadhav (Tata Consultancy Services Limited) <v-ajayjadhav@microsoft.com>
2025-05-16 14:47:44 +0530

    Adding new sample bot-meeting-ai-insights nodejs sample

---
+++ samples/bot-meeting-ai-insights/nodejs/index.js
@@ -0,0 +1,69 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+// index.js is used to setup and configure your bot
+
+// Import required packages
+const path = require('path');
+const restify = require('restify');
+require('dotenv').config({ path: path.join(__dirname, '.env') });
+
+
+// Import required bot services.
+// See https://aka.ms/bot-services to learn more about the different parts of a bot.
+const {
+    CloudAdapter,
+    ConversationState,
+    MemoryStorage,
+    UserState,
+    ConfigurationBotFrameworkAuthentication
+} = require('botbuilder');
+
+const { TeamsBot } = require('./bots/teamsBot');
+const { MainDialog } = require('./dialogs/mainDialog');
+
+const botFrameworkAuthentication = new ConfigurationBotFrameworkAuthentication(process.env);
+
+// Create adapter.
+// See https://aka.ms/about-bot-adapter to learn more about how bots work.
+const adapter = new CloudAdapter(botFrameworkAuthentication);
+
+adapter.onTurnError = async (context, error) => {
+    const errorMsg = error.message || 'Oops. Something went wrong!';
+    console.error(`\n [onTurnError] unhandled error: ${error}`);
+
+    // Clear out state
+    await conversationState.delete(context);
+    // Send a message to the user
+    await context.sendActivity(errorMsg);
+
+    // Uncomment below line for local debugging.
+    // await context.sendActivity(`Sorry, it looks like something went wrong. Exception Caught: ${errorMsg}`);
+};
+
+// Define the state store for your bot.
+// See https://aka.ms/about-bot-state to learn more about using MemoryStorage.
+const memoryStorage = new MemoryStorage();
+
+// Create conversation and user state with in-memory storage provider.
+const conversationState = new ConversationState(memoryStorage);
+const userState = new UserState(memoryStorage);
+
+// Create the main dialog.
+const dialog = new MainDialog();
+// Create the bot that will handle incoming messages.
+const bot = new TeamsBot(conversationState, userState, dialog);
+
+// Create HTTP server.
+const server = restify.createServer();
+server.use(restify.plugins.bodyParser());
+
+server.listen(process.env.port || process.env.PORT || 3978, () => {
+    console.log(`Server listening on ${server.url}`);
+});
+
+// Listen for incoming requests.
+server.post('/api/messages', async (req, res) => {
+    // Route received a request to adapter for processing
+    await adapter.process(req, res, (context) => bot.run(context));
+});
