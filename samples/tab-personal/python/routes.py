a445f306ba18aae56eadb3f9faf64cec83602ac8
Jegadeesh V <v-jegadeeshv@microsoft.com>
2025-06-19 02:54:07 +0530

    Adding Tab - Personal sample

---
+++ samples/tab-personal/python/routes.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+import random
+from faker import Faker
+
+app = Flask(__name__)
+fake = Faker()
+
+def setup_routes(app):
+    @app.route('/PersonalTab')
+    def personal_tab():
+        return render_template('personalTab.html')
