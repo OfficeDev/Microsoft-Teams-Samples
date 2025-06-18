330fc400ba4b36daf9e474e55b2607e14bfa15ff
YugalPradhan31 <v-ypradhan@microsoft.com>
2025-06-18 15:54:07 +0530

    Added Python Sample tab-people-picker

---
+++ samples/tab-people-picker/python/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__, template_folder='templates')
+
+@app.route('/configure')
+def configure():
+    return render_template('configure.html')
+
+@app.route('/tab')
+def tab():
+    return render_template('tab.html')
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 3978))
+    print(f"Bot/ME service listening at http://localhost:{port}")
+    app.run(host='0.0.0.0', port=port)
