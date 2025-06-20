3c9f82f1925dbc13cef5f2fb33c125c0022bc81e
YugalPradhan31 <v-ypradhan@microsoft.com>
2025-06-20 17:46:05 +0530

    Added python sample tab-product-inspection

---
+++ samples/tab-product-inspection/python/static/js/viewProductDetails.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+    $("#productStatus").hide();
+});
+
+microsoftTeams.app.initialize().then(() => {
+    scanBarCode();
+});
+
+let productId;
+
+function scanBarCode() {
+    const config = {
+        timeOutIntervalInSec: 30
+    };
+
+    microsoftTeams.media.scanBarCode((error, decodedText) => {
+        if (error) {
+            const msg = error.message ? `${error.errorCode}: ${error.message}` : `ErrorCode: ${error.errorCode}`;
+            alert(msg);
+        } else if (decodedText) {
+            productId = decodedText;
+            window.location.href = `${window.location.origin}/productDetails?productId=${productId}`;
+        }
+    }, config);
+}
