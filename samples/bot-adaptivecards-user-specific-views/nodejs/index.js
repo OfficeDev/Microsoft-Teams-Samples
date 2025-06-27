e9cc2b80054b345dc102160805ad051d65e12815
Mohammed Furquan <v-mfurquan@microsoft.com>
2025-06-27 12:19:14 +0530

    Harsh - CSharp to Nodejs Conversion Bot Adaptivecard User Specific Views.

---
+++ samples/bot-adaptivecards-user-specific-views/nodejs/index.js
@@ -0,0 +1,69 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+// index.js is used to setup and configure your bot
+
+// Import required packages
+const path = require('path');
+const express = require('express');
+
+// Import required bot services.
+// See https://aka.ms/bot-services to learn more about the different parts of a bot.
+const { BotFrameworkAdapter } = require('botbuilder');
+
+// Import bot definitions
+const { UserSpecificAdaptiveCardActionBot } = require('./bots/adaptivecardActionsBot');
+
+// Read botFilePath and botFileSecret from .env file.
+const ENV_FILE = path.join(__dirname, '.env');
+require('dotenv').config({ path: ENV_FILE });
+
+// Create adapter.
+// See https://aka.ms/about-bot-adapter to learn more about adapters.
+const adapter = new BotFrameworkAdapter({
+    appId: process.env.MicrosoftAppId,
+    appPassword: process.env.MicrosoftAppPassword
+});
+
+// Handle errors that occur during bot processing
+adapter.onTurnError = async (context, error) => {
+    console.error(`\n [onTurnError] unhandled error: ${error}`);
+
+    // Send a trace activity, which will be displayed in Bot Framework Emulator
+    await context.sendTraceActivity(
+        'OnTurnError Trace',
+        `${error}`,
+        'https://www.botframework.com/schemas/error',
+        'TurnError'
+    );
+
+    // Uncomment below line for local debugging.
+    // await context.sendActivity(`Sorry, it looks like something went wrong. Exception Caught: ${error}`);
+};
+
+// Create bot handlers
+const botActivityHandler = new UserSpecificAdaptiveCardActionBot();
+
+// Create HTTP server.
+const server = express();
+const port = process.env.port || process.env.PORT || 3978;
+
+server.listen(port, () => 
+    console.log(`\Bot/ME service listening at http://localhost:${port}`)
+);
+
+// Serve static files from the Images directory
+server.use("/Images", express.static(path.resolve(__dirname, 'Images')));
+
+// Handle undefined routes
+server.get('*', (req, res) => {
+    res.json({ error: 'Route not found' });
+});
+
+// Listen for incoming requests.
+server.post('/api/messages', (req, res) => {
+    adapter.processActivity(req, res, async (context) => {
+        // Process bot activity
+        await botActivityHandler.run(context);
+    });
+});
