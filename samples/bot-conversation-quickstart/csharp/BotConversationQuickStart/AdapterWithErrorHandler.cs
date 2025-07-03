65acc6a8bcf1a2a741bd658d69c7d571336dc617
Mohammed Furquan <v-mfurquan@microsoft.com>
2025-07-03 15:43:32 +0530

    Added BotConversationQuickStart Sample to this PR

---
+++ samples/bot-conversation-quickstart/csharp/BotConversationQuickStart/AdapterWithErrorHandler.cs
@@ -0,0 +1,43 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using Microsoft.Bot.Builder.TraceExtensions;
+using Microsoft.Bot.Connector.Authentication;
+using Microsoft.Extensions.Logging;
+
+namespace BotConversationQuickStart
+{
+    public class AdapterWithErrorHandler : CloudAdapter
+    {
+        // Adapter class to handle incoming requests and integrate with the bot's logic pipeline.
+
+        public AdapterWithErrorHandler(BotFrameworkAuthentication auth, ILogger<IBotFrameworkHttpAdapter> logger)
+            : base(auth, logger)
+        {
+            /// <summary>
+            /// Handles unhandled exceptions that occur during a bot turn.
+            /// Logs the error using the provided logger and sends a trace activity
+            /// for debugging purposes in the Bot Framework Emulator.
+            /// </summary>
+            /// <param name="turnContext">The context for the current bot turn.</param>
+            /// <param name="exception">The exception that was thrown during the turn.</param>
+            /// <returns>A task that represents the asynchronous error handling operation.</returns>
+
+            OnTurnError = async (turnContext, exception) =>
+            {
+                // Log any leaked exception from the application.
+                // NOTE: In production environment, you should consider logging this to
+                // Azure Application Insights. Visit https://aka.ms/bottelemetry to see how
+                // to add telemetry capture to your bot.
+                logger.LogError(exception, $"[OnTurnError] unhandled error : {exception.Message}");
+
+                // Uncomment below commented line for local debugging.
+                //await turnContext.SendActivityAsync($"Sorry, it looks like something went wrong. Exception Caught: {exception.Message}");
+
+                // Send a trace activity, which will be displayed in the Bot Framework Emulator
+                await turnContext.TraceActivityAsync("OnTurnError Trace", exception.Message, "https://www.botframework.com/schemas/error", "TurnError");
+            };
+        }
+    }
+}
---
+++ samples/bot-conversation-quickstart/csharp/BotConversationQuickStart/Program.cs
@@ -0,0 +1,49 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using BotConversationQuickStart.Bots;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using Microsoft.Bot.Connector.Authentication;
+using BotConversationQuickStart;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers().AddNewtonsoftJson();
+
+// Create the Bot Framework Authentication to be used with the Bot Adapter.
+builder.Services.AddSingleton<BotFrameworkAuthentication, ConfigurationBotFrameworkAuthentication>();
+
+// Create the Bot Adapter with error handling enabled.
+builder.Services.AddSingleton<IBotFrameworkHttpAdapter, AdapterWithErrorHandler>();
+
+
+// Create the bot as a transient. In this case the ASP Controller is expecting an IBot.
+builder.Services.AddTransient<IBot, BotActivityHandler>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.l
+if (!app.Environment.IsDevelopment())
+{
+    app.UseHsts();
+}
+else
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseDefaultFiles()
+    .UseStaticFiles()
+    .UseRouting()
+    .UseAuthorization()
+    .UseEndpoints(endpoints =>
+    {
+        endpoints.MapControllers();
+    });
+
+app.Run();
