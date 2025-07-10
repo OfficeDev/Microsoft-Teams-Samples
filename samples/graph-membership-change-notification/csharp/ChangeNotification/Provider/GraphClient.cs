33c32adf2f6022f912cc03b9406071a2e8832be6
AjayJ12-MSFT <v-ajayjadhav@microsoft.com>
2025-07-10 17:36:01 +0530

    Updating the csharp sample

---
+++ samples/graph-membership-change-notification/csharp/ChangeNotification/Provider/GraphClient.cs
@@ -0,0 +1,49 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+namespace ChangeNotification.Provider
+{
+    using Azure.Identity;
+    using ChangeNotification.Model.Configuration;
+    using Microsoft.Extensions.Options;
+    using Microsoft.Graph;
+
+    public class GraphClient
+    {
+        /// <summary>
+        /// Stores the Bot configuration values.
+        /// </summary>
+        private readonly IOptions<ApplicationConfiguration> botSettings;
+
+        public GraphClient(IOptions<ApplicationConfiguration> botSettings)
+        {
+            this.botSettings = botSettings;
+        }
+
+        public GraphServiceClient GetGraphClientforApp()
+        {
+            // The client credentials flow requires that you request the
+            // /.default scope, and preconfigure your permissions on the
+            // app registration in Azure. An administrator must grant consent
+            // to those permissions beforehand.
+            var scopes = new[] { "https://graph.microsoft.com/.default" };
+
+            // using Azure.Identity;
+            var options = new TokenCredentialOptions
+            {
+                AuthorityHost = AzureAuthorityHosts.AzurePublicCloud
+            };
+
+            // https://docs.microsoft.com/dotnet/api/azure.identity.clientsecretcredential
+            var clientSecretCredential = new ClientSecretCredential(
+                this.botSettings.Value.MicrosoftAppTenantId,
+                this.botSettings.Value.MicrosoftAppId,
+                this.botSettings.Value.MicrosoftAppPassword,
+                options);
+
+            var graphClient = new GraphServiceClient(clientSecretCredential, scopes);
+
+            return graphClient;
+        }
+    }
+}
