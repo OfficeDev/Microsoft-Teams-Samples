c26984644d18857c41de2122d678a7ad042256cf
Yugal Pradhan (Tata Consultancy Services Limited) <v-ypradhan@microsoft.com>
2025-11-09 00:49:39 +0530

    Updated meetings-notification nodejs

---
+++ samples/meetings-notification/nodejs/config.js
@@ -0,0 +1,12 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+const config = {
+  MicrosoftAppId: process.env.CLIENT_ID,
+  MicrosoftAppType: process.env.BOT_TYPE,
+  MicrosoftAppTenantId: process.env.TENANT_ID,
+  MicrosoftAppPassword: process.env.CLIENT_PASSWORD,
+  BOT_ENDPOINT: process.env.BOT_ENDPOINT,
+};
+
+module.exports = config;
