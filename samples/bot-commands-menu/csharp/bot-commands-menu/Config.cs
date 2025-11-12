1fbb48761f027aaca08104de4fc443c50f9b1287
AjayJ12-MSFT <v-ajayjadhav@microsoft.com>
2025-11-12 12:54:43 +0530

    Updating bot-commands-menu sample to teams sdk

---
+++ samples/bot-commands-menu/csharp/bot-commands-menu/Config.cs
@@ -0,0 +1,15 @@
+namespace bot_commands_menu
+{
+    public class ConfigOptions
+    {
+        public TeamsConfigOptions Teams { get; set; }
+    }
+
+    public class TeamsConfigOptions
+    {
+        public string BotType { get; set; }
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+        public string TenantId { get; set; }
+    }
+}
