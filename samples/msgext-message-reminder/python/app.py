e30de00ffa92f369d31486d9a8b0980d5e3b55b7
Yugal Pradhan (Tata Consultancy Services Limited) <v-ypradhan@microsoft.com>
2025-12-04 19:14:24 +0530

    Reapply "Fixed PR comments"
    This reverts commit c68c927bcc18ee4a98ad469afd75960b39326bda.

--- samples/msgext-message-reminder/python/app.py
+++ samples/msgext-message-reminder/python/app.py
@@ -6,12 +6,10 @@ import os
 import traceback
 from datetime import datetime
 from http import HTTPStatus
-
 from aiohttp import web
 from aiohttp.web import Request, Response, json_response
 import aiohttp_jinja2
 import jinja2
-
 from botbuilder.core import (
     BotFrameworkAdapterSettings,
     BotFrameworkAdapter,
@@ -19,25 +17,20 @@ from botbuilder.core import (
 )
 from botbuilder.core.integration import aiohttp_error_middleware
 from botbuilder.schema import Activity, ActivityTypes
-
 from bots.bot import MsgextReminderBot
 from config import DefaultConfig
 
 CONFIG = DefaultConfig()
-
 APP = web.Application(middlewares=[aiohttp_error_middleware])
 aiohttp_jinja2.setup(APP, loader=jinja2.FileSystemLoader("views"))
-
 SETTINGS = BotFrameworkAdapterSettings(CONFIG.APP_ID, CONFIG.APP_PASSWORD)
 ADAPTER = BotFrameworkAdapter(SETTINGS)
 
 # Error handling
 async def on_error(context: TurnContext, error: Exception):
     traceback.print_exc()
-
     await context.send_activity("The bot encountered an error or bug.")
     await context.send_activity("Please fix the bot source code to continue.")
-
     if context.activity.channel_id == "emulator":
         trace_activity = Activity(
             label="TurnError",
@@ -58,10 +51,8 @@ async def messages(req: Request) -> Response:
         body = await req.json()
     else:
         return Response(status=HTTPStatus.UNSUPPORTED_MEDIA_TYPE)
-
     activity = Activity().deserialize(body)
     auth_header = req.headers.get("Authorization", "")
-
     response = await ADAPTER.process_activity(activity, auth_header, BOT.on_turn)
     if response:
         return json_response(data=response.body, status=response.status)
