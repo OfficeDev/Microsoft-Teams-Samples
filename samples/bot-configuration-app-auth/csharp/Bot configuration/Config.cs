2cccd7e6a89ff472f5cddde618c9d01a14eb9195
Yugal Pradhan (Tata Consultancy Services Limited) <v-ypradhan@microsoft.com>
2025-12-22 00:40:48 +0530

    Removed BotType

--- samples/bot-configuration-app-auth/csharp/Bot configuration/Config.cs
+++ samples/bot-configuration-app-auth/csharp/Bot configuration/Config.cs
@@ -10,7 +10,6 @@ namespace Bot_configuration
 
     public class TeamsConfigOptions
     {
-        public string BotType { get; set; }
         public string ClientId { get; set; }
         public string ClientSecret { get; set; }
         public string TenantId { get; set; }
