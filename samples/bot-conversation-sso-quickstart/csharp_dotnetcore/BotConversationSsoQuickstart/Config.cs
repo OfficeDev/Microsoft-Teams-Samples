d606571cec5b628bf175f907f80980b1708972a8
AjayJ12-MSFT <v-ajayjadhav@microsoft.com>
2025-12-26 14:42:52 +0530

    Updating bot-conversation-sso-quickstart sample to use Teams SDK

---
+++ samples/bot-conversation-sso-quickstart/csharp_dotnetcore/BotConversationSsoQuickstart/Config.cs
@@ -0,0 +1,16 @@
+namespace BotConversationSsoQuickstart
+{
+    public class ConfigOptions
+    {
+        public TeamsConfigOptions Teams { get; set; }
+    }
+
+    public class TeamsConfigOptions
+    {
+        public string BotType { get; set; }
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+        public string TenantId { get; set; }
+        public string ConnectionName { get; set; }
+    }
+}
--- samples/bot-conversation-sso-quickstart/csharp_dotnetcore/BotConversationSsoQuickstart/Program.cs
+++ samples/bot-conversation-sso-quickstart/csharp_dotnetcore/BotConversationSsoQuickstart/Program.cs
@@ -1,60 +1,121 @@
-﻿// <copyright file="Program.cs" company="Microsoft">
-// Copyright (c) Microsoft. All rights reserved.
-// </copyright>
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
 
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Bot.Builder;
-using Microsoft.Bot.Builder.Integration.AspNet.Core;
-using Microsoft.Bot.Connector.Authentication;
-using Microsoft.BotBuilderSamples;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
+using BotConversationSsoQuickstart;
+using Microsoft.Teams.Apps;
+using Microsoft.Teams.Apps.Activities;
+using Microsoft.Teams.Apps.Events;
+using Microsoft.Teams.Apps.Extensions;
+using Microsoft.Teams.Common.Logging;
+using Microsoft.Teams.Extensions.Graph;
+using Microsoft.Teams.Plugins.AspNetCore.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
+var config = builder.Configuration.Get<ConfigOptions>();
 
-builder.Services.AddHttpClient().AddControllers().AddNewtonsoftJson();
 
-// Create the Bot Framework Adapter with error handling enabled.
-builder.Services.AddSingleton<IBotFrameworkHttpAdapter, AdapterWithErrorHandler>();
+var appBuilder = App.Builder()
+    .AddOAuth(config.Teams.ConnectionName);
 
-// Create the Bot Framework Authentication to be used with the Bot Adapter.
-builder.Services.AddSingleton<BotFrameworkAuthentication, ConfigurationBotFrameworkAuthentication>();
-
-// Create the storage we'll be using for User and Conversation state. (Memory is great for testing purposes.)
-builder.Services.AddSingleton<IStorage, MemoryStorage>();
-
-// Create the User state. (Used in this bot's Dialog implementation.)
-builder.Services.AddSingleton<UserState>();
-
-// Create the Conversation state. (Used by the Dialog system itself.)
-builder.Services.AddSingleton<ConversationState>();
-
-// The Dialog that will be run by the bot.
-builder.Services.AddSingleton<MainDialog>();
-
-// Create the bot as a transient. In this case the ASP Controller is expecting an IBot.
-builder.Services.AddTransient<IBot, TeamsBot<MainDialog>>();
+builder.AddTeams(appBuilder);
 
 var app = builder.Build();
+var teams = app.UseTeams();
+var token = "";
 
-// Configure the HTTP request pipeline.l
-if (!app.Environment.IsDevelopment())
-{
-    app.UseHsts();
-}
-else
-{
-    app.UseDeveloperExceptionPage();
-}
 
-app.UseDefaultFiles()
-                .UseStaticFiles()
-                .UseRouting()
-                .UseAuthorization()
-                .UseEndpoints(endpoints =>
-                {
-                    endpoints.MapControllers();
-                });
+teams.Use(async context =>
+{
+    var start = DateTime.UtcNow;
+    try
+    {
+        await context.Next();
+    }
+    catch
+    {
+        context.Log.Error("error occurred during activity processing");
+    }
+    context.Log.Debug($"request took {(DateTime.UtcNow - start).TotalMilliseconds}ms");
+});
+
+teams.OnMembersAdded(async context =>
+{
+    await context.Send("Welcome to AuthenticationBot. Type '/signin' to get logged in. Type '/signout' to sign out.");
+});
+
+
+
+teams.OnMessage("/signout", async context =>
+{
+    if (!context.IsSignedIn)
+    {
+        await context.Send("you are not signed in!");
+        return;
+    }
+
+    await context.SignOut();
+    await context.Send("You have been signed out!");
+});
+
+teams.OnMessage("/signin", async context =>
+{
+    if (!context.IsSignedIn)
+    {
+        await context.SignIn();
+
+        return;
+    }
+
+    var me = await context.GetUserGraphClient().Me.GetAsync();
+    await context.Send($"User '{me!.DisplayName}' is already signed in!");
+});
+
+teams.OnSignIn(async (_, @event) =>
+{
+    token = @event.Token.Token;
+    var context = @event.Context;
+
+    var graphClient = context.GetUserGraphClient();
+    var me = await graphClient.Me.GetAsync();
+
+    // Fetch and send profile photo
+    try
+    {
+        var photoStream = await graphClient.Me.Photo.Content.GetAsync();
+
+        if (photoStream != null)
+        {
+            using var memoryStream = new MemoryStream();
+            await photoStream.CopyToAsync(memoryStream);
+            var photoBytes = memoryStream.ToArray();
+            var base64Photo = Convert.ToBase64String(photoBytes);
+
+            var photoHtml = $"<img src=\"data:image/jpeg;base64,{base64Photo}\" alt=\"Profile Photo\" />";
+            await context.Send(photoHtml);
+        }
+    }
+    catch
+    {
+        await context.Send("Could not retrieve profile photo.");
+    }
+
+    await context.Send($"You have logged in as \"{me!.DisplayName}\". Would you like to view your token, type 'Yes' to view else type 'No' to cancel:");
+});
+
+teams.OnMessage(async context =>
+{
+    var command = context.Activity.Text.Trim().ToLowerInvariant();
+    if (command.Equals("yes"))
+    {
+        await context.Send(token);
+        await context.Send("Thank You!");
+        return;
+    }
+    else
+    {
+        await context.Send("Thank You!");
+        return;
+    }
+});
 
 app.Run();
