fd7e2542d5a7cec59cd35d62d5779c398aaa864e
Mohammed Furquan <v-mfurquan@microsoft.com>
2026-02-23 05:50:38 +0530

    Removed Bot Conv SSO QuickStart From the PR

--- samples/bot-conversation-sso-quickstart/nodejs/config.ts
+++
@@ -1,9 +0,0 @@
-const config = {
-  MicrosoftAppId: process.env.CLIENT_ID,
-  MicrosoftAppType: process.env.BOT_TYPE,
-  MicrosoftAppTenantId: process.env.TENANT_ID,
-  MicrosoftAppPassword: process.env.CLIENT_PASSWORD,
-  connectionName: process.env.CONNECTION_NAME || 'graph',
-};
-
-export default config;
--- samples/bot-conversation-sso-quickstart/nodejs/index.ts
+++
@@ -1,7 +0,0 @@
-import app from "./app";
-
-// Start the application
-(async () => {
-  await app.start();
-  console.log(`\nBot started, app listening to`, process.env.PORT || process.env.port || 3978);
-})();
