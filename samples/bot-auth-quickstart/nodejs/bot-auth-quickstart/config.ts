d276a129bfe5e3feb77bb8e98a343bc990b21b57
Mohammed Furquan <v-mfurquan@microsoft.com>
2026-03-04 05:45:18 +0530

    Updated

--- samples/bot-auth-quickstart/nodejs/bot-auth-quickstart/config.ts
+++ samples/bot-auth-quickstart/nodejs/bot-auth-quickstart/config.ts
@@ -2,7 +2,7 @@ const config = {
   MicrosoftAppId: process.env.CLIENT_ID,
   MicrosoftAppType: process.env.BOT_TYPE,
   MicrosoftAppTenantId: process.env.TENANT_ID,
-  MicrosoftAppPassword: process.env.CLIENT_PASSWORD,
+  MicrosoftAppPassword: process.env.CLIENT_SECRET,
   connectionName: process.env.CONNECTION_NAME || 'oauthbotsetting',
   teamsAppId: process.env.TEAMS_APP_ID,
 };
