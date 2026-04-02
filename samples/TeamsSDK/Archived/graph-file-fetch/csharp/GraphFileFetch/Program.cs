8c44ef2bb107170987e90ac2a24e7ef6ef0fb774
Yugal Pradhan (Tata Consultancy Services Limited) <v-ypradhan@microsoft.com>
2026-04-02 17:13:10 +0530

    Moved samples to Archived

---
+++ samples/TeamsSDK/Archived/graph-file-fetch/csharp/GraphFileFetch/Program.cs
@@ -0,0 +1,41 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using Microsoft.Extensions.DependencyInjection;
+
+using GraphFileFetch;
+using GraphFileFetch.Bots;
+using GraphFileFetch.Dialogs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHttpClient();
+builder.Services.AddControllers().AddNewtonsoftJson();
+
+// Create the storage we'll be using for User and Conversation state. (Memory is great for testing purposes.)
+builder.Services.AddSingleton<IStorage, MemoryStorage>();
+
+// Create the Conversation state. (Used by the Dialog system itself.)
+builder.Services.AddSingleton<ConversationState>();
+
+// The Dialog that will be run by the bot.
+builder.Services.AddSingleton<MainDialog>();
+
+// Create the Bot Framework Adapter with error handling enabled.
+builder.Services.AddSingleton<IBotFrameworkHttpAdapter, AdapterWithErrorHandler>();
+
+// Create the bot as a transient. In this case the ASP Controller is expecting an IBot.
+builder.Services.AddTransient<IBot, AuthBot<MainDialog>>();
+
+var app = builder.Build();
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+app.UseWebSockets();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
