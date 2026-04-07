284508fedca9708927fd371eb2dbceb87ec76314
Yugal Pradhan (Tata Consultancy Services Limited) <v-ypradhan@microsoft.com>
2026-04-07 17:08:35 +0530

    Moved tab-app-cookie to archive

--- samples/TeamsSDK/Archived/tab-app-cookie/js/server.js
+++ samples/TeamsSDK/Archived/tab-app-cookie/js/server.js
@@ -1,13 +1,12 @@
 // Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
 // See LICENSE in the project root for license information.
 
-var express = require("express");
-var cookieParser = require("cookie-parser");
-var serveStatic = require("serve-static");
+const express = require("express");
+const cookieParser = require("cookie-parser");
 
-var app = express();
+const app = express();
 app.use(cookieParser());
-app.use(serveStatic(__dirname + "/public"));
+app.use(express.static(__dirname + "/public"));
 
 app.get("/cookies/samesite/set", (req, res) => {
   res.cookie("3pcookie", "value", { sameSite: "none", secure: true });
@@ -32,7 +31,7 @@ app.get("/cookies/partitioned/set", (req, res) => {
   res.end();
 });
 
-var port = process.env.port || 3000;
-app.listen(port, function () {
-  console.log("Listening on http://localhost:" + port);
+const port = process.env.port || 3000;
+app.listen(port, () => {
+  console.log(`Listening on http://localhost:${port}`);
 });
