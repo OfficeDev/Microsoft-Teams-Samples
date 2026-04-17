25fa0bd85eb1d61f6b891fcf8e91fa2012a0b366
Mohammed Furquan <v-mfurquan@microsoft.com>
2026-04-17 11:15:44 +0530

    Added 10th Sample

---
+++ samples/TeamsSDK/Archived/meetings-app-icon-badging/csharp/AppIconBadgingInMeetings/Models/ActionBase.cs
@@ -0,0 +1,23 @@
+﻿// <copyright file="ActionBase.cs" company="Microsoft">
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+// </copyright>
+
+namespace AppIconBadgingInMeetings.Models
+{
+    /// <summary>
+    /// Model entity for identifying action type of card and extracting user's choiceset.
+    /// </summary>
+    public class ActionBase
+    {
+        /// <summary>
+        /// Gets or sets the action type of the card.
+        /// </summary>
+        public string Type { get; set; }
+
+        /// <summary>
+        /// Gets or sets the user's choiceset.
+        /// </summary>
+        public string Choice { get; set; }
+    }
+}
