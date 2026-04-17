7902a75d2700fd8a04541b34cdc19eb7e6d55623
Mohammed Furquan <v-mfurquan@microsoft.com>
2026-04-17 16:42:59 +0530

    Added CSharp Sample

---
+++ samples/TeamsSDK/Archived/tab-staggered-permission/csharp/StaggeredPermission/Controllers/TabController.cs
@@ -0,0 +1,131 @@
+﻿using StaggeredPermission.helper;
+using StaggeredPermission.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StaggeredPermission.Controllers
+{
+    public class TabController : Controller
+    {
+        public readonly IConfiguration _configuration;
+
+        public TabController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        // Get user's profile photo.
+        [HttpPost]
+        [Route("GetUserPhoto")]
+        public async Task<JsonResult> GetUserPhoto(string idToken)
+        {
+            try
+            {
+                var bearerToken = await AuthHelper.GetAccessTokenOnBehalfUserAsync(_configuration, idToken);
+                var client = new SimpleGraphClient(bearerToken);
+                var photo = await client.GetPhotoAsync();
+
+                var userInfo = new UserData()
+                {
+                    Photo = photo
+                };
+
+                var jsonString = JsonConvert.SerializeObject(userInfo);
+                return Json(jsonString);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return null;
+            }
+        }
+
+        // Get user's mails.
+        [HttpPost]
+        [Route("GetUserMails")]
+        public async Task<JsonResult> GetUserMails(string idToken)
+        {
+            try
+            {
+                var bearerToken = await AuthHelper.GetAccessTokenOnBehalfUserAsync(_configuration, idToken);
+                var emails = new List<UserEmail>();
+                var client = new SimpleGraphClient(bearerToken);
+
+                var mails = await client.GetMailsAsync();
+
+                foreach (var mail in mails)
+                {
+                    if (mail.Sender != null &&
+                        mail.ToRecipients?.Any() == true &&
+                        !string.IsNullOrEmpty(mail.Subject))
+                    {
+                        var userEntity = new UserEmail
+                        {
+                            FromMail = mail.Sender.EmailAddress?.Address,
+                            ToMail = mail.ToRecipients.FirstOrDefault()?.EmailAddress?.Address,
+                            Subject = mail.Subject,
+                            Time = mail.SentDateTime?.ToString()
+                        };
+
+                        emails.Add(userEntity);
+                    }
+                }
+
+                var userInfo = new UserData()
+                {
+                    Details = emails
+                };
+
+                var jsonString = JsonConvert.SerializeObject(userInfo);
+                return Json(jsonString);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return null;
+            }
+        }
+
+        // Get user details based on jwt id token.
+        [HttpPost]
+        [Route("decodeToken")]
+        public JsonResult DecodeToken(string accessToken)
+        {
+            try
+            {
+                object nameObj = null, emailObj = null;
+                var handler = new JwtSecurityTokenHandler();
+                var decodedValue = handler.ReadJwtToken(accessToken);
+                if (decodedValue.Payload.ContainsKey("name"))
+                {
+                    decodedValue.Payload.TryGetValue("name", out nameObj);
+                }
+
+                if (decodedValue.Payload.ContainsKey("preferred_username"))
+                {
+                    decodedValue.Payload.TryGetValue("preferred_username", out emailObj);
+                }
+
+                var userInfo = new UserData()
+                {
+                    Name = nameObj.ToString(),
+                    Email = emailObj.ToString()
+                };
+
+                var jsonString = JsonConvert.SerializeObject(userInfo);
+                return Json(jsonString);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+                return null;
+            }
+        }
+    }
+}
---
+++ samples/TeamsSDK/Archived/tab-staggered-permission/csharp/StaggeredPermission/helper/AuthHelper.cs
@@ -0,0 +1,121 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Identity.Client;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StaggeredPermission.helper
+{
+    public class AuthHelper
+    {
+        /// <summary>
+        /// Retrieve Valid Audiences.
+        /// </summary>
+        /// <param name="configuration">IConfiguration instance.</param>
+        /// <returns>Valid Audiences.</returns>
+        public static IEnumerable<string> GetValidAudiences(IConfiguration configuration)
+        {
+            var clientId = configuration["AzureAd:ClientId"];
+            var applicationIdUri = configuration["AzureAd:ApplicationIdURI"];
+            var validAudiences = new List<string> { clientId, applicationIdUri.ToLower() };
+            return validAudiences;
+        }
+        /// <summary>
+        /// Retrieve Valid Issuers.
+        /// </summary>
+        /// <param name="configuration">IConfiguration instance.</param>
+        /// <returns>Valid Issuers.</returns>
+        public static IEnumerable<string> GetValidIssuers(IConfiguration configuration)
+        {
+            var tenantId = configuration["AzureAd:TenantId"];
+            var validIssuers = GetSettings(configuration);
+            validIssuers = validIssuers.Select(validIssuer => validIssuer.Replace("tenantId", tenantId));
+            return validIssuers;
+        }
+
+        /// <summary>
+        /// Audience Validator.
+        /// </summary>
+        /// <param name="tokenAudiences">Token audiences.</param>
+        /// <param name="securityToken">Security token.</param>
+        /// <param name="validationParameters">Validation parameters.</param>
+        /// <returns>Audience validator status.</returns>
+        public static bool AudienceValidator(
+            IEnumerable<string> tokenAudiences,
+            SecurityToken securityToken,
+            TokenValidationParameters validationParameters)
+        {
+            if (tokenAudiences == null || tokenAudiences.Count() == 0)
+            {
+                throw new ApplicationException("No audience defined in token!");
+            }
+
+            var validAudiences = validationParameters.ValidAudiences;
+
+            if (validAudiences == null || validAudiences.Count() == 0)
+            {
+                throw new ApplicationException("No valid audiences defined in validationParameters!");
+            }
+
+            foreach (var tokenAudience in tokenAudiences)
+            {
+                if (validAudiences.Any(validAudience => validAudience.Equals(tokenAudience, StringComparison.OrdinalIgnoreCase)))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Get token using on-behalf-of flow.
+        /// </summary>
+        /// <param name="configuration">IConfiguration instance.</param>
+        /// <param name="idToken">The id token from the client.</param>
+        /// <returns>App access token on behalf of user.</returns>
+        public static async Task<string> GetAccessTokenOnBehalfUserAsync(IConfiguration configuration, string idToken)
+        {
+            var tenantId = configuration["AzureAd:TenantId"];
+            IConfidentialClientApplication app = ConfidentialClientApplicationBuilder.Create(configuration["AzureAd:ClientId"])
+                                                .WithClientSecret(configuration["AzureAd:AppSecret"])
+                                                .WithAuthority($"https://login.microsoftonline.com/{tenantId}")
+                                                .Build();
+
+            try
+            {
+                var assert = new UserAssertion(idToken);
+                var scopes = new List<string> { "https://graph.microsoft.com/User.Read" };
+                var responseToken = await app.AcquireTokenOnBehalfOf(scopes, assert).ExecuteAsync();
+
+                return responseToken.AccessToken;
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+
+        /// <summary>
+        /// Retrieve Settings.
+        /// </summary>
+        /// <param name="configuration">IConfiguration instance.</param>
+        /// <returns>Configuration settings for valid issuers.</returns>
+        private static IEnumerable<string> GetSettings(IConfiguration configuration)
+        {
+            var configurationSettingsValue = configuration["AzureAd:ValidIssuers"];
+            var settings = configurationSettingsValue
+                ?.Split(new char[] { ';', ',' }, StringSplitOptions.RemoveEmptyEntries)
+                ?.Select(p => p.Trim());
+
+            if (settings == null)
+            {
+                throw new ApplicationException($"AzureAd:ValidIssuers does not contain a valid value in the configuration file.");
+            }
+
+            return settings;
+        }
+    }
+}
