eaf559ecb3b63bbd325bf7ce2613fa91ed3b1260
Yugal Pradhan (Tata Consultancy Services Limited) <v-ypradhan@microsoft.com>
2026-05-20 16:03:55 +0530

    Moved 3 samples to archived folder

---
+++ samples/TeamsSDK/Archived/graph-change-notification/csharp/ChangeNotification/Program.cs
@@ -0,0 +1,40 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using ChangeNotification.Bots;
+using ChangeNotification.Dialogs;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using Microsoft.Bot.Connector.Authentication;
+using Microsoft.Bot.Schema;
+using System.Collections.Concurrent;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Logging.AddDebug();
+builder.Logging.AddConsole();
+
+builder.Services.AddControllers().AddNewtonsoftJson();
+builder.Services.AddSingleton<BotFrameworkAuthentication, ConfigurationBotFrameworkAuthentication>();
+builder.Services.AddSingleton<CloudAdapter, AdapterWithErrorHandler>();
+builder.Services.AddSingleton<IStorage, MemoryStorage>();
+builder.Services.AddSingleton<UserState>();
+builder.Services.AddSingleton<ConversationState>();
+builder.Services.AddSingleton<ConcurrentDictionary<string, ConversationReference>>();
+builder.Services.AddSingleton<MainDialog>();
+builder.Services.AddTransient<IBot, ChangeNotificationBot<MainDialog>>();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
---
+++ samples/TeamsSDK/Archived/msgext-message-reminder/csharp/MessagingExtensionReminder/Helper/ScheduleTaskReminder.cs
@@ -0,0 +1,22 @@
+﻿// <copyright file="ScheduleTaskReminder.cs" company="Microsoft">
+// Copyright (c) Microsoft. All rights reserved.
+// </copyright>
+
+using Quartz;
+
+namespace MessagingExtensionReminder
+{
+    public class ScheduleTaskReminder : IJob
+    {
+        static HttpClient client = new HttpClient();
+        
+        // Method to execute scheduled job.
+        public async Task Execute(IJobExecutionContext context)
+        {
+            JobDataMap dataMap = context.JobDetail.JobDataMap;
+            string baseUrl = dataMap.GetString("baseUrl");
+
+            await client.GetAsync(baseUrl + "/api/task");
+        }
+    }
+}
