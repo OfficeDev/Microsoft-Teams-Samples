9619aaa2bb0d6d9d7b6aa34aadef60bcafe1e850
Mohammed Furquan (Tata Consultancy Services Limi) <v-mfurquan@microsoft.com>
2026-06-08 12:18:49 +0530

    PR Fixes

---
+++ samples/TeamsSDK/Archived/meetings-live-caption/csharp/MeetingLiveCaption/Models/LiveCaption.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace MeetingLiveCaption.Models
+{
+    public class LiveCaption
+    {
+        public string CaptionText { get; set; }
+        public string CartUrl { get; set; }
+    }
+}
